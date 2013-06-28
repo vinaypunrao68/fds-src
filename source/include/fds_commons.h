9ba89f3799d72ae863316c6ba05a6a8c0d5850e0
vinaypunrao <vinay@formationds.com>
2013-06-28 06:22:59 +0000

    Added UDP version of xport protocol handling to stor_mgr

--- source/include/fds_commons.h
+++ source/include/fds_commons.h
@@ -2,6 +2,7 @@
 
 #define __FDS_COMMONS
 #include <sys/types.h>
+#include <stdbool.h>
 
 typedef unsigned char doid_t[20];
 #define SET_DOID_INV(doid) memet(doid, 0xff, 20)
@@ -23,15 +24,22 @@ static __inline__  is_doid_inv(doid_t doid) {
 
 typedef unsigned int volid_t;
 #define INV_VOL_ID (unsigned int)(-1)
-
+#if 0
 typedef short int16_t;
 typedef unsigned short uint16_t;
 typedef int int32_t;
 typedef unsigned int uint32_t;
 // typedef long long int64_t;
 typedef unsigned long long uint64_t;
+#endif
 
-#define UINT16_MAX (unsigned short) 0xffff
-#define UINT32_MAX (unsigned int) 0xffffffff
+// FDS definitions of variable types
+typedef unsigned int fds_uint32_t;
+typedef int          fds_int32_t; 
+typedef unsigned short fds_uint16_t;  
+typedef short  fds_int16_t;
+typedef char     fds_char_t;
+typedef bool fds_bool_t;
+typedef unsigned long long fds_uint64_t;
 
 #endif
--- source/stor_mgr/stor_mgr.c
+++ source/stor_mgr/stor_mgr.c
@@ -1,8 +1,9 @@
 #include <stor_mgr.h>
 
 fds_bool_t  stor_mgr_stopping = FALSE;
-
-
+#define FDS_XPORT_PROTO_TCP 1
+#define FDS_XPORT_PROTO_UDP 2
+#define FDSP_MAX_MSG_LEN (4*1024 + 128)
 fds_stor_mgr_init() 
 {
 // Create all data structures 
@@ -54,73 +55,96 @@ int sock = * ((int *)sock_ptr);
 /*------------------------------------------------------------------------------------- 
  * Storage Mgr main  processor : Listen on the socket and spawn or assign thread from a pool
  -------------------------------------------------------------------------------------*/
-void fds_stor_mgr_main(void)
+void fds_stor_mgr_main(int xport_protocol)
 {
     int sockfd, newsockfd, portno, clilen, err=0;
     pthread_t pthr;
     struct sockaddr_in serv_addr, cli_addr;
     int  n;
+    char buffer[4096+128];
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-    {
-        perror("ERROR opening socket");
-        exit(1);
-    }
-
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = FDS_STOR_MGR_LISTEN_PORT;
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
- 
-    /* Now bind the host address using bind() call.*/
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-                          sizeof(serv_addr)) < 0)
-    {
-         perror("ERROR on binding");
-         exit(1);
-    }
-
-    /* Now start listening for the clients, here process will
+    if (xport_protocol == FDS_XPORT_PROTO_TCP) {
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) 
+        {
+            perror("ERROR opening socket");
+            exit(1);
+        }
+    
+        bzero((char *) &serv_addr, sizeof(serv_addr));
+        portno = FDS_STOR_MGR_LISTEN_PORT;
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = INADDR_ANY;
+        serv_addr.sin_port = htons(portno);
+     
+        /* Now bind the host address using bind() call.*/
+        if (bind(sockfd, (struct sockaddr *) &serv_addr,
+                              sizeof(serv_addr)) < 0)
+        {
+             perror("ERROR on binding");
+             exit(1);
+        }
+    
+        /* Now start listening for the clients, here process will
      * go in sleep mode and will wait for the incoming connection
-     */
-    listen(sockfd, FDS_MAX_WAITING_CONNS);
-    clilen = sizeof(cli_addr);
-
-    /* Accept actual connection from the client */
-    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, 
-                                &clilen);
-    if (newsockfd < 0) 
-    {
-        perror("ERROR on accept");
-        exit(1);
-    }
-
-    while(!stor_mgr_stopping)
-    {
-        newsockfd = accept(sockfd, 
-                (struct sockaddr *) &cli_addr, &clilen);
-        if (newsockfd < 0)
+         */
+        listen(sockfd, FDS_MAX_WAITING_CONNS);
+        clilen = sizeof(cli_addr);
+        
+        /* Accept actual connection from the client */
+        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, 
+                                    &clilen);
+        if (newsockfd < 0) 
         {
             perror("ERROR on accept");
             exit(1);
         }
-        /* Enqueue into a processing thread's queue */
-        err = pthread_create(&pthr, NULL, &stor_mgr_req_processor, (void *)&newsockfd);
-        if (err < 0)
+        
+        while(!stor_mgr_stopping)
         {
-            perror("ERROR on pthread_create");
-	    exit(1);
-        }
-    } /* end of while */
+            newsockfd = accept(sockfd, 
+                    (struct sockaddr *) &cli_addr, &clilen);
+            if (newsockfd < 0)
+            {
+                perror("ERROR on accept");
+                exit(1);
+                }
+            /* Enqueue into a processing thread's queue */
+            err = pthread_create(&pthr, NULL, &stor_mgr_req_processor, (void *)&newsockfd);
+            if (err < 0)
+            {
+                    perror("ERROR on pthread_create");
+	        exit(1);
+            }
+        } /* end of while */
+   } else {
+      /* UDP based processing */
+      sockfd=socket(AF_INET,SOCK_DGRAM,0);
 
-    return; 
+      bzero((char *) &serv_addr, sizeof(serv_addr));
+      portno = FDS_STOR_MGR_LISTEN_PORT;
+      serv_addr.sin_family = AF_INET;
+      serv_addr.sin_addr.s_addr = INADDR_ANY;
+      serv_addr.sin_port = htons(portno);
+      bind(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
+
+      for (;;)
+      {
+         clilen = sizeof(cli_addr);
+         n = recvfrom(sockfd,buffer,FDSP_MAX_MSG_LEN,0,(struct sockaddr *)&cli_addr,&clilen);
+         printf("Received the FDSP msg:\n");
+         
+         /* Process the FDSP msg from DM or SH */ 
+         stor_mgr_proc_fdsp_msg((void *)buffer);
+      }
+   }
+    
+   return; 
 }
-
+    
 int main(int argc, void *argv)
 {
-  fds_stor_mgr_main();
+  fds_stor_mgr_main(FDS_XPORT_PROTO_UDP);
 }
 
 
