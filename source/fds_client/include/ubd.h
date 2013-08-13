daa61b9be2dfa6def5ef2e1e52b7e0b8fabff022
Subbu <subbu@formationds.com>
2013-08-13 11:13:34 -0700

    New header file to pass req between ubd and StorHvisor

---
+++ source/fds_client/include/ubd.h
@@ -0,0 +1,11 @@
+typedef double                     fbd_sector_t;
+
+struct fbd_request {
+  int                          op;
+  char                         *buf;
+  fbd_sector_t                 sec;
+  int                          secs;
+  void                         *req_data;
+};
+
+typedef struct fbd_request            fbd_request_t;
