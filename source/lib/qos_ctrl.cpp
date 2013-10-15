c6d1e6d8f411713a1b84e07772077728df90c991
Vinay Rao <vinay@formationds.com>
2013-10-15 12:14:28 -0700

    Fixed a few bugs in qos ctrl integration with HTB dispatcher

--- source/lib/qos_ctrl.cpp
+++ source/lib/qos_ctrl.cpp
@@ -5,25 +5,16 @@
 
 namespace fds {
 
-FDS_QoSControl::FDS_QoSControl(uint32_t _max_threads, 
+FDS_QoSControl::FDS_QoSControl() {
+}
+
+FDS_QoSControl::FDS_QoSControl(fds_uint32_t _max_threads, 
                                dispatchAlgoType algo, 
                                fds_log *log): qos_max_threads(_max_threads)  {
    threadPool = new fds_threadpool(qos_max_threads);
    dispatchAlgo = algo;
    qos_log = log;
    total_rate = 20000; //IOPS
-   switch(algo) { 
-    case FDS_DISPATCH_HIER_TOKEN_BUCKET :
-        dispatcher = (FDS_QoSDispatcher *)new QoSHTBDispatcher(this, qos_log, total_rate);
-        break;
-
-    case FDS_DISPATCH_WFQ : 
-        //dispatcher =(FDS_QoSDispatcher *) new QoSWFQDispatcher(this, total_rate);
-        break;
-
-     default :
-       break;
-   }
 }
 
 
@@ -38,13 +29,19 @@ fds_uint32_t FDS_QoSControl::waitForWorkers() {
 }
 
 
+void startDispatcher(FDS_QoSControl *qosctl) { 
+  if (qosctl && qosctl->dispatcher) {
+     qosctl->dispatcher->dispatchIOs();
+  }
+  
+}
 void FDS_QoSControl::runScheduler() { 
   
-  if (dispatcher) {
-     //threadPool->schedule(dispatcher->dispatchIOs, this);
-  }
+  threadPool->schedule(startDispatcher, this);
 }
 
+void   FDS_QoSControl::setQosDispatcher(dispatchAlgoType algo_type, FDS_QoSDispatcher *qosDispatcher) {
+}
 
 Error   FDS_QoSControl::registerVolume(fds_volid_t vol_uuid, FDS_VolumeQueue *volq) {
 Error err(ERR_OK);
@@ -59,18 +56,11 @@ Error err(ERR_OK);
    return err;
 }
 
-Error FDS_QoSControl::processIO(FDS_IOType *io) { 
-    
-Error err(ERR_OK);
-    if ( io->io_module == FDS_IOType::STOR_HV_IO) { 
-     if(io->io_type == FDS_IO_READ) {
-                //threadPool->schedule(StorHvisorProcIoRd, io);
-     } else if(io->io_type == FDS_IO_WRITE) {
-                //threadPool->schedule(StorHvisorProcIoWr, io);
-     } else
-                FDS_PLOG(qos_log)  << " Invalid op ";
-     }
+
+  Error FDS_QoSControl::enqueueIO(fds_volid_t volUUID, FDS_IOType *io) {
+    Error err(ERR_OK);
     return err;
-   };
+  }
 
 }
+
