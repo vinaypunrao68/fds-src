be746570a3e9671706a2a647780cc36609c78cfe
root <root@localhost.localdomain>
2013-10-21 11:04:45 -0700

    Another script to run pharos workloads assuming bring_up.py setup everything (OM, SM, DM, SH, policies and volumes)
    Once you run bring_up.py, suppose you have 2 volumes (/dev/fbd2 and
    /dev/fbd3), start two write workloads, each accessing its own volume
    
    >cd source/tools
    ./run-workloads -p "demo" -w "sample_workloads/write_N_outstand.sh:/deb/fbd1" \
    -w "sample_workloads/write_N_outstand.sh:/deb/fbd2"

--- source/tools/sample_workloads/write_N_outstand.sh
+++ source/tools/sample_workloads/write_N_outstand.sh
@@ -19,6 +19,7 @@ PHAROS_DIR=$(dirname $SCRIPTDIR)/pharos
 runtime=40           # length of experiment in seconds 
 blocksize=4          # in KB
 outstand_count=20    # number of outstanding IOs from workload generator 
+access=r            # 'r' is random and 's' is sequential
 
 tracename=$RESULT_DIR/$PREFIX"__"$NAME"_vol"$VOLID".stat" # note .stat extension otherwise plotting scrip will ignore it
 
@@ -41,7 +42,7 @@ fi
 mkdir -p $RESULT_DIR
 
 pushd .; cd $PHAROS_DIR
-./Pharos -k $blocksize -o $outstand_count -d w -t $tracename $DEVICE r $runtime $VOLID $PREFIX
+./Pharos -k $blocksize -o $outstand_count -d w -t $tracename $DEVICE $access $runtime $VOLID $PREFIX
 popd
 
 
