7f936b4d05465708c7ab500bfaaef1243790a1d1
rao <nbayyana@nbayyana-VirtualBox.(none)>
2013-11-13 11:40:01 -0800

    Fixed compliation errors from preivous pull

--- source/stor_mgr_ice/unit-test/sm_unit_test.cpp
+++ source/stor_mgr_ice/unit-test/sm_unit_test.cpp
@@ -78,8 +78,6 @@ class SmUnitTest {
                                    << " for volume offset " << putReq->volume_offset
                                    << " with object ID " << ioReq.pr_oid << " and data size "
                                    << putReq->data_obj_len;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
       req.req_complete();
     }
     void pr_get(ProbeRequest &req) {
@@ -88,20 +86,6 @@ class SmUnitTest {
     }
     void pr_delete(ProbeRequest &req) {
       req.req_complete();
-=======
-    }
-    void pr_get(ProbeRequest &req) {
-      std::cout << "Got a get print" << std::endl;
-    }
-    void pr_delete(ProbeRequest &req) {
->>>>>>> Stashed changes
-=======
-    }
-    void pr_get(ProbeRequest &req) {
-      std::cout << "Got a get print" << std::endl;
-    }
-    void pr_delete(ProbeRequest &req) {
->>>>>>> Stashed changes
     }
     void pr_verify_request(ProbeRequest &req) {
     }
