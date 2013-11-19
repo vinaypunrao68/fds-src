60b42c5c6caa3ea860da734f53ed79c15eb7c133
Vy Nguyen <vynguyen@bienhoa>
2013-11-18 16:09:53 -0800

    Forgot to check in 2 new files
    	new file:   am-engine/am-plugin.c
    	new file:   am-engine/am-plugin.h

---
+++ source/am-engine/am-plugin.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2013 Formation Data Systems, Inc.
+ */
+#ifndef INCLUDE_AM_ENGINE_AM_PLUGIN_H_
+#define INCLUDE_AM_ENGINE_AM_PLUGIN_H_
+
+#include <shared/fds_types.h>
+
+c_decls_begin
+
+extern int ngx_main(int argc, char const **argv);
+
+c_decls_end
+
+#endif /* INCLUDE_AM_ENGINE_AM_PLUGIN_H_ */
