de91c5984adc672474ee13d0df499f69c0f5061c
Anna Povzner <anna@formationds.com>
2013-11-21 02:21:04 +0000

    A small fix to checking volume exists by name (if need to get volid).

--- source/stor_hvisor_ice/StorHvNative.cpp
+++ source/stor_hvisor_ice/StorHvNative.cpp
@@ -83,8 +83,8 @@ void FDS_NativeAPI::DeleteBucket(BucketContext* bucketCtxt,
 {
     fds_volid_t ret_id; 
    // check the bucket is already attached. 
-   ret_id = storHvisor->vol_table->volumeExists(bucketCtxt->bucketName);
-   if (ret_id == invalid_vol_id) {
+   ret_id = storHvisor->vol_table->getVolumeUUID(bucketCtxt->bucketName);
+   if (ret_id != invalid_vol_id) {
        FDS_PLOG_SEV(storHvisor->GetLog(), fds::fds_log::error) << " S3 Bucket  Does not exsists  BucketID: " << ret_id;
 
      (handler)(FDSN_StatusErrorBucketNotExists,NULL,NULL);
