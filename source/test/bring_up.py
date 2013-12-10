ba3c162b9eaf51d91ee5c7074e2e058e70aa4038
anwleung <andrew@formationds.com>
2013-12-10 15:08:39 -0800

    Changes to bring up script to enable using a private key instead of SSH password.

--- source/test/bring_up.py
+++ source/test/bring_up.py
@@ -256,6 +256,7 @@ class TestBringUp():
     sshUser = None
     sshPswd = None
     sshPort = 22
+    sshPkey = None
 
     #
     # Relative path info on remote node
@@ -424,6 +425,8 @@ class TestBringUp():
                 self.setPath(self.config.get(section, "path"))
                 self.sshUser = self.config.get(section, "user")
                 self.sshPswd = self.config.get(section, "passwd")
+                if self.config.has_option(section, "privatekey") == True:
+                    self.sshPkey = self.config.get(section, "privatekey")
             elif re.match(self.sh_sec_prefix, section) != None:
                 self.loadClient(section, self.config.items(section))
             elif re.match(self.vol_sec_prefix, section) != None:
@@ -538,7 +541,11 @@ class TestBringUp():
             client = paramiko.SSHClient()
             client.load_system_host_keys()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(ipOver, self.sshPort, self.sshUser, self.sshPswd)
+            if self.sshPkey == None:
+                client.connect(ipOver, self.sshPort, self.sshUser, self.sshPswd)
+            else:
+                key = paramiko.RSAKey.from_private_key_file(self.sshPkey)
+                client.connect(ipOver, port=self.sshPort, username=self.sshUser, pkey=key)
 
             #
             # Run remote cmd
