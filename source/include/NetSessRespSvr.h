b84acc25f138b474a81dbfed1a2c139b9945322c
Anna Povzner <anna@formationds.com>
2014-01-14 13:00:28 -0800

    Added OMControlPathResp receiver class.

--- source/include/NetSessRespSvr.h
+++ source/include/NetSessRespSvr.h
@@ -35,147 +35,175 @@ using namespace fds;
 
 class fdspDataPathRespReceiver: public Runnable {
 public:
-  fdspDataPathRespReceiver(boost::shared_ptr<TProtocol> prot,
-                   boost::shared_ptr<FDSP_DataPathRespIf> hdlr)
-    : prot_(prot),
-      processor_(new FDSP_DataPathRespProcessor(hdlr))
-  {
-  }
-
-  void run() {
-    /* wait for connection to be established */
-    /* for now just busy wait */
-    while (!prot_->getTransport()->isOpen()) {
-      printf("fdspDataPathRespReceiver: waiting for transport...\n");
-      usleep(500);
+fdspDataPathRespReceiver(boost::shared_ptr<TProtocol>& prot,
+                         boost::shared_ptr<FDSP_DataPathRespIf>& hdlr)
+        : prot_(prot),
+            processor_(new FDSP_DataPathRespProcessor(hdlr)) {
     }
-
-    try {
-      for (;;) {
-        if (!processor_->process(prot_, prot_, NULL) ||
-            !prot_->getTransport()->peek()) {
-          break;
+    
+    void run() {
+        /* wait for connection to be established */
+        /* for now just busy wait */
+        while (!prot_->getTransport()->isOpen()) {
+            printf("fdspDataPathRespReceiver: waiting for transport...\n");
+            usleep(500);
         }
-      }
+        
+        try {
+            for (;;) {
+                if (!processor_->process(prot_, prot_, NULL) ||
+                    !prot_->getTransport()->peek()) {
+                    break;
+                }
+            }
+        }
+        catch (TException& tx) {
+            printf("fdspDataPathResponse Receiver exception");
+        }
     }
-    catch (TException& tx) {
-      printf("fdspDataPathResponse Receiver exception");
-    }
-  }
-
-
+    
+    
 public:
-  boost::shared_ptr<TProtocol> prot_;
-  boost::shared_ptr<TProcessor> processor_;
+    boost::shared_ptr<TProtocol> prot_;
+    boost::shared_ptr<TProcessor> processor_;
 };
 
 class fdspMetaDataPathRespReceiver: public Runnable {
 public:
-  fdspMetaDataPathRespReceiver(boost::shared_ptr<TProtocol> prot,
-                   boost::shared_ptr<FDSP_MetaDataPathRespIf> hdlr)
-    : prot_(prot),
-      processor_(new FDSP_MetaDataPathRespProcessor(hdlr))
-  {
-  }
-
-  void run() {
-    /* wait for connection to be established */
-    /* for now just busy wait */
-    while (!prot_->getTransport()->isOpen()) {
-      printf("fdspMetaDataPathRespReceiver: waiting for transport...\n");
-      usleep(500);
+fdspMetaDataPathRespReceiver(boost::shared_ptr<TProtocol>& prot,
+                             boost::shared_ptr<FDSP_MetaDataPathRespIf>& hdlr)
+        : prot_(prot),
+            processor_(new FDSP_MetaDataPathRespProcessor(hdlr)) {
     }
 
-    try {
-      for (;;) {
-        if (!processor_->process(prot_, prot_, NULL) ||
-            !prot_->getTransport()->peek()) {
-          break;
+    void run() {
+        /* wait for connection to be established */
+        /* for now just busy wait */
+        while (!prot_->getTransport()->isOpen()) {
+            printf("fdspMetaDataPathRespReceiver: waiting for transport...\n");
+            usleep(500);
         }
-      }
+        
+        try {
+            for (;;) {
+                if (!processor_->process(prot_, prot_, NULL) ||
+                    !prot_->getTransport()->peek()) {
+                    break;
+                }
+            }
+        }
+        catch (TException& tx) {
+            printf("fdspMetaDataPathResponse Receiver exception");
+        }
     }
-    catch (TException& tx) {
-      printf("fdspMetaDataPathResponse Receiver exception");
-    }
-  }
-
 
 public:
-  boost::shared_ptr<TProtocol> prot_;
-  boost::shared_ptr<TProcessor> processor_;
+    boost::shared_ptr<TProtocol> prot_;
+    boost::shared_ptr<TProcessor> processor_;
 };
 
 
 class fdspControlPathRespReceiver: public Runnable {
 public:
-  fdspControlPathRespReceiver(boost::shared_ptr<TProtocol> prot,
-                   boost::shared_ptr<FDSP_ControlPathRespIf> hdlr)
-    : prot_(prot),
-      processor_(new FDSP_ControlPathRespProcessor(hdlr))
-  {
-  }
-
-  void run() {
-    /* wait for connection to be established */
-    /* for now just busy wait */
-    while (!prot_->getTransport()->isOpen()) {
-      printf("fdspControlPathRespReceiver: waiting for transport...\n");
-      usleep(500);
+fdspControlPathRespReceiver(boost::shared_ptr<TProtocol>& prot,
+                            boost::shared_ptr<FDSP_ControlPathRespIf>& hdlr)
+        : prot_(prot),
+            processor_(new FDSP_ControlPathRespProcessor(hdlr)) {
     }
-
-    try {
-      for (;;) {
-        if (!processor_->process(prot_, prot_, NULL) ||
-            !prot_->getTransport()->peek()) {
-          break;
+    
+    void run() {
+        /* wait for connection to be established */
+        /* for now just busy wait */
+        while (!prot_->getTransport()->isOpen()) {
+            printf("fdspControlPathRespReceiver: waiting for transport...\n");
+            usleep(500);
         }
-      }
+        
+        try {
+            for (;;) {
+                if (!processor_->process(prot_, prot_, NULL) ||
+                    !prot_->getTransport()->peek()) {
+                    break;
+                }
+            }
+        }
+        catch (TException& tx) {
+            printf("fdspControlPathResponse Receiver exception");
+        }
     }
-    catch (TException& tx) {
-      printf("fdspControlPathResponse Receiver exception");
-    }
-  }
 
 public:
-  boost::shared_ptr<TProtocol> prot_;
-  boost::shared_ptr<TProcessor> processor_;
+    boost::shared_ptr<TProtocol> prot_;
+    boost::shared_ptr<TProcessor> processor_;
 };
 
-#if 0
 class fdspConfigPathRespReceiver: public Runnable {
 public:
-  fdspConfigPathRespReceiver(boost::shared_ptr<TProtocol> prot,
-                   boost::shared_ptr<FDSP_ConfigPathRespIf> hdlr)
-    : prot_(prot),
-      processor_(new fdspConfigPathRespProcessor(hdlr))
-  {
-  }
-
-  void run() {
-    /* wait for connection to be established */
-    /* for now just busy wait */
-    while (!prot_->getTransport()->isOpen()) {
-      printf("fdspConfigPathRespReceiver: waiting for transport...\n");
-      usleep(500);
+fdspConfigPathRespReceiver(boost::shared_ptr<TProtocol>& prot,
+                           boost::shared_ptr<FDSP_ConfigPathRespIf>& hdlr)
+        : prot_(prot),
+            processor_(new fdspConfigPathRespProcessor(hdlr)) {
     }
-
-    try {
-      for (;;) {
-        if (!processor_->process(prot_, prot_, NULL) ||
-            !prot_->getTransport()->peek()) {
-          break;
+    
+    void run() {
+        /* wait for connection to be established */
+        /* for now just busy wait */
+        while (!prot_->getTransport()->isOpen()) {
+            printf("fdspConfigPathRespReceiver: waiting for transport...\n");
+            usleep(500);
         }
-      }
+        
+        try {
+            for (;;) {
+                if (!processor_->process(prot_, prot_, NULL) ||
+                    !prot_->getTransport()->peek()) {
+                    break;
+                }
+            }
+        }
+        catch (TException& tx) {
+            printf("fdspConfigPathResponse Receiver exception");
+        }
     }
-    catch (TException& tx) {
-      printf("fdspConfigPathResponse Receiver exception");
-    }
-  }
 
 public:
-  boost::shared_ptr<TProtocol> prot_;
-  boost::shared_ptr<TProcessor> processor_;
+    boost::shared_ptr<TProtocol> prot_;
+    boost::shared_ptr<TProcessor> processor_;
 };
-#endif
+
+class fdspOMControlPathRespReceiver: public Runnable {
+public:
+fdspOMControlPathRespReceiver(boost::shared_ptr<TProtocol>& prot,
+                              boost::shared_ptr<FDSP_OMControlPathRespIf>& hdlr)
+        : prot_(prot),
+            processor_(new fdspOMControlPathRespProcessor(hdlr)) {
+    }
+    
+    void run() {
+        /* wait for connection to be established */
+        /* for now just busy wait */
+        while (!prot_->getTransport()->isOpen()) {
+            printf("fdspOMControlPathRespReceiver: waiting for transport...\n");
+            usleep(500);
+        }
+        
+        try {
+            for (;;) {
+                if (!processor_->process(prot_, prot_, NULL) ||
+                    !prot_->getTransport()->peek()) {
+                    break;
+                }
+            }
+        }
+        catch (TException& tx) {
+            printf("fdspOMControlPathResponse Receiver exception");
+        }
+    }
+
+public:
+    boost::shared_ptr<TProtocol> prot_;
+    boost::shared_ptr<TProcessor> processor_;
+};
+
 
 #endif
