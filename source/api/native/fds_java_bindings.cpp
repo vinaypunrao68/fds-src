6a77c6a700734debb826dc248f171869735b2c5f
Fabrice Medio <fabrice@formationds.com>
2014-02-19 15:02:13 -0800

    Java bindings: initialize storage hypervisor

--- source/api/native/fds_java_bindings.cpp
+++ source/api/native/fds_java_bindings.cpp
@@ -2,13 +2,14 @@
  * Copyright 2014 Formation Data Systems, Inc.
  */
 
-#include "../../stor-hvisor/StorHvisorCPP.h"
+#include <StorHvisorCPP.h>
 #include <util/fds_stat.h>
 #include <native_api.h>
 #include <fds_process.h>
+#include "./com_formationds_nativeapi_NativeApi.h"
 
 
-int main(int foo, char *bar[]) {
+JNIEXPORT void JNICALL Java_com_formationds_nativeapi_NativeApi_init(JNIEnv *env, jobject o) {
     int argc = 0;
     char *argv[] = {};
     fds::init_process_globals("am.log");
@@ -23,5 +24,9 @@ int main(int foo, char *bar[]) {
     fds::ModuleVector am_module(0, NULL, am_mod_vec);
 
     am_module.mod_execute();
-    // fds::gl_AMEngineS3.run_server(api);
+    // fds::gl_AMEngineS3.run_server(api);    
+}
+
+int main(int argc, char *argv[]) {
+    return 0;
 }
