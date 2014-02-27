c00372e5cfb6389d410ceb125393422ceb7871e1
Premkumar <prem@formationds.com>
2014-02-27 10:47:10 -0800

    redis cpp interface and connection pool

---
+++ source/unit-test/redis/redis_unit_test.cpp
@@ -0,0 +1,73 @@
+#define CATCH_CONFIG_MAIN
+#include "../catch/catch.hpp"
+
+#include <kvstore/redis.h>
+#include <iostream>
+#include <util/Log.h>
+
+using namespace redis;
+using namespace std;
+using namespace fds;
+
+TEST_CASE("invalid-host") {
+    try {
+        Redis* r= new Redis("xxxxx");
+        r->sendCommand("ping");
+    } catch(RedisException &e ) {
+        LOGWARN << e.what() ;
+    }
+}
+
+TEST_CASE ("hash") {
+    Redis* r= new Redis();
+    Reply reply = r->sendCommand("del testhash");
+    reply.dump();
+    reply = r->sendCommand("ping");
+    reply.dump();
+    reply = r->sendCommand("hmset testhash one 1 two 2 three 3");
+    reply.dump();
+        
+    std::vector<string> vec;
+    
+    reply = r->hgetall("testhash");
+    reply.dump();
+    
+    reply.toVector(vec);
+    
+    
+    REQUIRE (vec.size() == 6);
+    REQUIRE (vec[0] == "one");
+    REQUIRE (vec[5] == "3");
+    
+    delete r;
+}
+
+TEST_CASE ("get-set") {
+    for ( uint i=0; i<10 ; i++ ) {
+    Redis* r= new Redis();
+    Reply reply = r->set("test-name","fds");
+    REQUIRE (reply.getStatus() == "OK" );
+    reply = r->get("test-name");
+
+    REQUIRE ( reply.getString() == "fds" );
+    delete r;
+    }
+}
+
+TEST_CASE ("binary") {    
+    std::string s,h,s1;
+    s.append(1,10);
+    s.append(1,0);
+    s.append(1,3);
+    
+    Redis::encodeHex(s,h);
+    Redis* r= new Redis();
+    r->sendCommand("set test-binval %s",h.c_str());
+    Reply reply= r->get("test-binval");
+    Redis::decodeHex(reply.getString(),s1);
+    REQUIRE ( s1.length() == 3 );
+    REQUIRE ( s1[0] == 10 );
+    //REQUIRE ( s1.at(0) == 0 );
+    REQUIRE ( s1[2] == 3 );
+    delete r;
+}
