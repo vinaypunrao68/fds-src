83f67d0bca37f99e4232c4bbc6455c6e43961ec5
Fabrice Medio <fabrice@formationds.com>
2014-02-27 18:21:27 -0800

    [fm] Hook up maven build to Makefile tree

---
+++ source/orch_mgr/java/src/com/formationds/Main.java
@@ -0,0 +1,10 @@
+package com.formationds;
+
+/*
+ * Copyright 2014 Formation Data Systems, Inc.
+ */
+public class Main {
+        public void start() {
+            System.out.println("Hello, world!");
+        }
+}
