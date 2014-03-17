c8e12bcb21228a0263768eff8a42b012bcac101f
Premkumar <prem@formationds.com>
2014-03-17 12:30:31 -0700

    adding timeutils

---
+++ source/include/util/timeutils.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2013 Formation Data Systems, Inc.
+ */
+
+#ifndef SOURCE_UTIL_TIMEUTILS_H_
+#define SOURCE_UTIL_TIMEUTILS_H_
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <assert.h>
+#include "fds_types.h"
+
+namespace fds {
+    namespace util {
+
+        extern const boost::posix_time::ptime epoch;
+        fds_int64_t getTimeStampMillis() ;
+
+    } // namespace util
+} // namespace fds
+
+#endif // SOURCE_UTIL_TIMEUTILS_H_
