bb8d71a83d0d530ffc888304cc21e59c72ba5828
anwleung <andrew@formationds.com>
2014-03-26 06:33:18 -0700

    Fix style issue with types.

--- source/include/fds_types.h
+++ source/include/fds_types.h
@@ -349,13 +349,12 @@ struct hash<fds::ObjectID> {
 /*
  * NOTE!!! include only std typedefs here. Dont use any fds objects !!!!
  */
-#include <string>
 #include <vector>
 
 namespace fds {
     // new c++11 typedef convention - pretty cool !!!
-    using StringList  = std::vector<std::string> ;
-    using ConstString = const std::string& ;
+    using StringList  = std::vector<std::string>;
+    using ConstString = const std::string&;
 }  // namespace fds
 
 #endif  // SOURCE_INCLUDE_FDS_TYPES_H_
