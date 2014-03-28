205cde3c861d921fb6eda91596ed3847fa78fb0b
Vy Nguyen <vynguyen@bienhoa>
2014-03-28 10:28:47 -0700

    Add libparted lib package Fix bugs found during unit test. Split disk inventory to generic lib that could be shared and reused. Preliminary integration of libparted to handle disk partition.

--- source/platform/include/disk-partition.h
+++ source/platform/include/disk-partition.h
@@ -4,6 +4,23 @@
 #ifndef SOURCE_PLATFORM_INCLUDE_DISK_PARTITION_H_
 #define SOURCE_PLATFORM_INCLUDE_DISK_PARTITION_H_
 
+#include <vector>
+#include <disk.h>
+#include <parted/parted.h>
+
 namespace fds {
+class DiskPartition : public DiskObjIter
+{
+  public:
+    explicit DiskPartition(DiskObj::pointer disk);
+    virtual ~DiskPartition();
+
+    virtual bool dsk_iter_fn(DiskObj::pointer curr);
+
+  protected:
+    DiskObj::pointer          pe_disk;
+    PedDevice                *pe_dev;
+};
+
 }  // namespace fds
 #endif  // SOURCE_PLATFORM_INCLUDE_DISK_PARTITION_H_
