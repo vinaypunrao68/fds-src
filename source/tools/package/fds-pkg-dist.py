b201335a6e8e3870089186eb70da4d2b2113981f
Premkumar <prem@formationds.com>
2014-03-31 17:03:56 -0700

    not uploading test packages

--- source/tools/package/fds-pkg-dist.py
+++ source/tools/package/fds-pkg-dist.py
@@ -13,15 +13,21 @@ log.setLevel(logging.INFO)
 class PkgDist:
     def __init__(self):
         self.nameformat=re.compile("(.*)_([a-z0-9.-]+).deb")
+        self.testpkgformat= re.compile(".*T[0-9]+.deb")
         self.args = None
 
     def verify(self):
         if len(self.args.pkg) == 0:
             log.error("please specify a pkg")
             return False
+
         
         for pkg in self.args.pkg:
             filename = os.path.basename(pkg)
+
+            if self.testpkgformat.match(filename):
+                log.error('[%s] is a test pkg. Please create a release pkg to upload with fds-pkg-create -r', filename)
+                return False
 
             match = self.nameformat.match(filename)
             if not match:
@@ -56,9 +62,9 @@ class PkgDist:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Upload a a pkg to fds repo',formatter_class=argparse.ArgumentDefaultsHelpFormatter) 
     parser.add_argument('-r','--repo', default='coke.formationds.com:8081', help='Repository location')
-    parser.add_argument('-b','--branch', default='test', help='Branch' ,"upload to specific branch [test/stable]")
+    parser.add_argument('-b','--branch', default='test', help="upload to specific branch [test/stable]")
     parser.add_argument('-v','--verbose', action="store_true" , help = 'be more verbose')
-    parser.add_argument('-n','--dryrun', action="store_true" , 'just check , dont upload')
+    parser.add_argument('-n','--dryrun', action="store_true" , help = 'just check , dont upload')
     parser.add_argument('pkg', nargs=argparse.REMAINDER)
 
     args = parser.parse_args()
@@ -74,7 +80,7 @@ if __name__ == "__main__":
 
     dist.args = args
     if dist.verify():
-        if not dist.args.dryrun:
+        if dist.args.dryrun:
             log.info('not uploading as this is a dryrun')
         else:
             dist.process()
