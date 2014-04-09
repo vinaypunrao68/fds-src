092da0b33fe72b527e8f79be334e4203ceb281f2
Vy Nguyen <vynguyen@bienhoa>
2014-04-09 10:49:25 -0700

    fds-tool.py is cluster bring up/down/cleanup in real HW config to be used at Intel. Factor common code to fdslib library. Add parallel disk format to disk_type.py script.
    	modified:   platform/python/disk_type.py
    	modified:   test/fds-bringup.py
    	new file:   test/fds-tool.py
    	modified:   test/fdslib/BringUpCfg.py

--- source/test/fds-tool.py
+++ source/test/fds-tool.py
@@ -13,12 +13,6 @@ if __name__ == '__main__':
                       help = 'configuration file (e.g. sample.cfg)', metavar = 'FILE')
     parser.add_option('-v', '--verbose', action = 'store_true', dest = 'verbose',
                       help = 'enable verbosity')
-    parser.add_option('-p', '--package', action = 'store_true', dest = 'make_pkg',
-                      help = 'package fds tar ball')
-    parser.add_option('-i', '--install', action = 'store_true', dest = 'clus_inst',
-                      help = 'install fds package to remote machines')
-    parser.add_option('-m', '--manual', action = 'store_true', dest = 'manual',
-                      help = 'Only run OM and platform, admit nodes manually')
     parser.add_option('-u', '--up', action = 'store_true', dest = 'clus_up',
                       help = 'bring up cluster')
     parser.add_option('-d', '--down', action = 'store_true', dest = 'clus_down',
@@ -27,73 +21,35 @@ if __name__ == '__main__':
                       help = 'cleanup cluster')
     parser.add_option('-r', '--dryrun', action = 'store_true', dest = 'dryrun',
                       help = 'dry run, print commands only')
+    parser.add_option('-R', '--fds-root', dest = 'fds_root', default = '/fds',
+                      help = 'Specify fds-root directory')
 
     (options, args) = parser.parse_args()
 
-    # It's not important that we get fds-root here.
-    env = inst.FdsEnv('.')
-    pkg = inst.FdsPackage(env)
-    if options.make_pkg:
-        print "Make fds package from ", env.get_fds_source()
-        pkg.package_tar()
-        if options.config_file is None:
-            sys.exit(0)
-
+    env = inst.FdsEnv(options.fds_root)
     if options.config_file is None:
         print "You need to pass config file"
         sys.exit(1)
 
     cfg = fdscfg.FdsConfigRun(env, options.config_file, options.verbose)
 
-    # Get all the objects
+    # Get all the configuration
     nodes = cfg.rt_get_obj('cfg_nodes')
     ams   = cfg.rt_get_obj('cfg_am')
-    pols  = cfg.rt_get_obj('cfg_vol_pol')
-    vols  = cfg.rt_get_obj('cfg_volumes')
     steps = cfg.rt_get_obj('cfg_scenarios')
 
-    # Install package
-    if options.clus_inst:
-        for n in nodes:
-            n.nd_install_rmt_pkg()
-
-    # shutdown
+    # Shutdown
     if options.clus_down:
         for n in nodes:
             n.nd_cleanup_daemons()
 
-    # cleanup
+    # Cleanup
     if options.clus_clean:
         for n in nodes:
             n.nd_cleanup_node()
 
+    cfg.rt_fds_bootstrap()
+
     if options.clus_up is None:
         sys.exit(0)
 
-    # If we have scenarios to run steps, run them
-    if len(steps):
-        for sce in steps:
-            sce.start_scenario()
-        sys.exit(0)
-
-    # else run all defined sections by default
-    time.sleep(2)
-    cfg.rt_fds_bootstrap()
-
-    if options.manual:
-        print('You need to run fdscli --activate-nodes manually.')
-        sys.exit(0)
-
-    time.sleep(5)
-    cli = cfg.rt_get_obj('cfg_cli')
-    cli.run_cli('--activate-nodes abc -k 1 -e sm,dm')
-
-    for am in ams:
-        am.am_start_service()
-
-    time.sleep(5)
-    for p in pols:
-        p.policy_apply_cfg(cli)
-
-    for v in vols:
-        v.vol_apply_cfg(cli)
