060c42b8a590feb0f7412d85852ca675ec2200cd
Premkumar <prem@formationds.com>
2014-04-16 17:14:09 -0700

    fds-jdk-default pkg

---
+++ jdk/jdk.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export J2SDKDIR=/usr/lib/jvm/jdk-8-oracle-x64
+export J2REDIR=/usr/lib/jvm/jdk-8-oracle-x64/jre
+export PATH=$PATH:/usr/lib/jvm/jdk-8-oracle-x64/bin:/usr/lib/jvm/jdk-8-oracle-x64/db/bin:/usr/lib/jvm/jdk-8-oracle-x64/jre/bin
+export JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-x64
+export DERBY_HOME=/usr/lib/jvm/jdk-8-oracle-x64/db
