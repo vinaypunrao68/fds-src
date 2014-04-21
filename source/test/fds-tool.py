198e8fee4063ef93332e81599ba1613134eeeb5b
baop <baothaipham2001@gmail.com>
2014-04-21 16:06:21 -0700

    Wait completion on starting redis.

--- source/test/fds-tool.py
+++ source/test/fds-tool.py
@@ -86,8 +86,8 @@ if __name__ == '__main__':
 
     for n in nodes:
         print 'Running remote command for ' + n.nd_conf_dict['node-name']
-        n.nd_rmt_agent.ssh_exec('python -m disk_type -m')
-        n.nd_rmt_agent.ssh_exec('/fds/sbin/redis.sh start')
+        n.nd_rmt_agent.ssh_exec('python -m disk_type -m', wait_compl=True)
+        n.nd_rmt_agent.ssh_exec('/fds/sbin/redis.sh start', wait_compl=True, output=True)
 
     om = cfg.rt_om_node
     om_ip = om.nd_conf_dict['ip']
@@ -104,7 +104,7 @@ if __name__ == '__main__':
         else:
             cli.run_cli('--activate-nodes abc -k 1 -e sm')
         print "Waiting for node %s to come up" % n.nd_rmt_host
-    	time.sleep(30)
+    	time.sleep(3)
 
     time.sleep(4)
     for am in ams:
