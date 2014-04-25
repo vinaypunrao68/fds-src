bf2930179962d4aaa6afe6d599ab2d40268434e7
Brian Madden <brian@formationds.com>
2014-04-25 16:50:19 -0700

    Updated shared memory stuff

--- source/osdep/umod-linux/fds-shmem.cpp
+++ source/osdep/umod-linux/fds-shmem.cpp
@@ -5,61 +5,74 @@
 
 namespace fds {
 
-FdsShmem::FdsShmem(const char *name)
-    : sh_name(name), sh_addr(NULL), sh_size(0) {}
+FdsShmem::FdsShmem(const char *name, size_t size)
+    : sh_name(name), sh_addr(NULL), sh_size(size) {}
 
 
 FdsShmem::~FdsShmem() {}
 
-int
-FdsShmem::shm_alloc(size_t size)
+void *
+FdsShmem::shm_alloc()
 {
-    int fd = shm_open(sh_name, O_RDWR | O_CREAT, S_IRWXU);
-    sh_size = size;
+    int fd = shm_open(sh_name, O_RDWR|O_CREAT| O_EXCL, S_IRWXU);
 
     // fd should be 0
     if (fd == -1) {
-        return -1;
+        return NULL;
     }
 
     // Truncate the region
     if (ftruncate(fd, sh_size) == -1) {
-        return -1;
+        return NULL;
     }
 
     // Map the region
-    sh_addr = mmap(NULL, sh_size, PROT_WRITE, MAP_SHARED, fd, 0);
+    sh_addr = mmap(NULL, sh_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
     if (sh_addr == NULL) {
-        return -1;
+        return NULL;
     }
 
     // Close the file descriptor, we should no longer need it
     close(fd);
 
-    return 0;
+    return sh_addr;
 }
 
 void *
-FdsShmem::shm_get()
+FdsShmem::shm_attach()
 {
-    // Verify that the shm_alloc has already been called
+    int fd;
+    // If sh_addr is NULL then we need to try to open the shm segment
     if (sh_addr == NULL) {
-        return NULL;
+        fd = shm_open(sh_name, O_RDWR, S_IRUSR);
+        if (fd == -1) {
+            return NULL;
+        } else {
+            sh_addr = mmap(NULL, sh_size, PROT_READ, MAP_SHARED, fd, 0);
+        }
     }
+    // We should no longer need the file descriptor
+    close(fd);
 
     // Return a pointer to the shared region
     return sh_addr;
 }
 
 int
-FdsShmem::shm_unmap()
+FdsShmem::shm_detach()
 {
     // Verify that the shm is mapped first
     if (sh_addr == NULL) {
         return -1;
     }
 
-    // Unmap the shared region
     return munmap(sh_addr, sh_size);
 }
+
+int
+FdsShmem::shm_remove()
+{
+    return shm_unlink(sh_name);
+}
+
 }  // namespace fds
