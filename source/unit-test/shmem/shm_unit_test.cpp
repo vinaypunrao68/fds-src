f425f7259f4ca9ed273d04746518a1af8edffe5b
Brian Madden <brian@formationds.com>
2014-04-25 16:51:12 -0700

    Adding unit test for shared memory

---
+++ source/unit-test/shmem/shm_unit_test.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2014 by Formation Data Systems, Inc.
+ */
+#include <stdio.h>
+#include <platform/fds-shmem.h>
+#include <sys/mman.h>
+
+int main() {
+
+    // shm name
+    const char * n = "/test\0";
+
+    // Be sure that we're already unlinked
+    shm_unlink(n);
+
+    // Create a new shared mem segment
+    fds::FdsShmem a{n, 1024};
+    void *first = a.shm_alloc();
+
+    printf("Value of first[0] = %d\n", ((int*)first)[0]);
+    // Write to it
+    *(int *)first = 1;
+    printf("Value of first[0] = %d\n", ((int*)first)[0]);
+
+    // Create a second and attach
+    fds::FdsShmem b{n, 1024};
+    void *second = b.shm_attach();
+    
+    // verify that the contents are the same
+    assert( ((int*)first)[0] == ((int*)second)[0] );
+
+    // Try to write via second
+    //*(int *)second = 2;
+    
+    // Unmap and remap
+    b.shm_detach();
+     
+    assert(a.shm_remove() == 0);
+
+    return 0;
+}
