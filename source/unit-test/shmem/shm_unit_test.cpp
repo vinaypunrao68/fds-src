10ec5fdd05f25926cc49a8bbc12c6baf93618641
Brian Madden <brian@formationds.com>
2014-04-29 17:50:08 -0700

    Updated unit test to reflect new shared memory semantics.

--- source/unit-test/shmem/shm_unit_test.cpp
+++ source/unit-test/shmem/shm_unit_test.cpp
@@ -3,40 +3,33 @@
  */
 #include <stdio.h>
 #include <platform/fds-shmem.h>
-#include <sys/mman.h>
 
 int main() {
 
     // shm name
-    const char * n = "/test\0";
-
-    // Be sure that we're already unlinked
-    shm_unlink(n);
+    char *n = "abc\0";
 
     // Create a new shared mem segment
-    fds::FdsShmem a{n, 1024};
-    void *first = a.shm_alloc();
-
+    fds::FdsShmem a{n};
+    void *first = a.shm_alloc(1024);
+    assert(first != nullptr);
+    
     printf("Value of first[0] = %d\n", ((int*)first)[0]);
     // Write to it
-    *(int *)first = 1;
+    *(int *)first = 42;
     printf("Value of first[0] = %d\n", ((int*)first)[0]);
-
+    assert(((int*)first)[0] == 42);
+    
     // Create a second and attach
-    fds::FdsShmem b{n, 1024};
+    fds::FdsShmem b{n};
     void *second = b.shm_attach();
+    assert(second != nullptr);
+    
     
     // verify that the contents are the same
     assert( ((int*)first)[0] == ((int*)second)[0] );
 
-    // Try to write via second
-    //*(int *)second = 2;
-    
-    // Unmap and remap
-    a.shm_detach();
-    b.shm_detach();
-     
-    assert(a.shm_remove() == 0);
+    assert(a.shm_remove() == true);
 
     return 0;
 }
