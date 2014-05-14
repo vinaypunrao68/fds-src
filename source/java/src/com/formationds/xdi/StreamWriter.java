0ddf5ca0b5fd2a310cda06ec2094e37b72e8ab0c
Fabrice Medio <fmedio@gmail.com>
2014-05-14 14:05:43 -0700

    [jl/fm] Fix StreamWriter

--- source/java/src/com/formationds/xdi/StreamWriter.java
+++ source/java/src/com/formationds/xdi/StreamWriter.java
@@ -6,7 +6,6 @@ package com.formationds.xdi;
 import com.formationds.apis.AmService;
 import com.formationds.apis.ObjectOffset;
 import com.formationds.apis.TxDescriptor;
-import org.apache.commons.io.IOUtils;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -25,7 +24,8 @@ public class StreamWriter {
         this.am = am;
     }
 
-    public byte[] write(String domainName, String volumeName, String blobName, InputStream in, Map<String, String> metadata) throws Exception {
+    public byte[] write(String domainName, String volumeName, String blobName, InputStream input, Map<String, String> metadata) throws Exception {
+        InputStream in = new BufferedInputStream(input, objectSize);
         long objectOffset = 0;
         int lastBufSize = 0;
         MessageDigest md = MessageDigest.getInstance("MD5");
@@ -34,13 +34,12 @@ public class StreamWriter {
         TxDescriptor tx = am.startBlobTx(domainName, volumeName, blobName);
         byte[] buf = new byte[objectSize];
 
-        for (int read = IOUtils.read(in, buf); read != -1; read = in.read(buf)) {
+        for (int read = readFully(in, buf); read != -1; read = readFully(in, buf)) {
             md.update(buf, 0, read);
-            // Just create a fake tx id for now to make the compiler happy...
             am.updateBlob(domainName, volumeName, blobName, tx,
-                          ByteBuffer.wrap(buf, 0, read), read,
-                          new ObjectOffset(objectOffset),
-                          ByteBuffer.wrap(new byte[0]), false);
+                    ByteBuffer.wrap(buf, 0, read), read,
+                    new ObjectOffset(objectOffset),
+                    ByteBuffer.wrap(new byte[0]), false);
             lastBufSize = read;
             objectOffset++;
         }
@@ -50,13 +49,24 @@ public class StreamWriter {
             digest = md.digest();
             ByteBuffer byteBuffer = ByteBuffer.wrap(digest);
             am.updateBlob(domainName, volumeName, blobName, tx,
-                          ByteBuffer.wrap(buf), lastBufSize,
-                          new ObjectOffset(objectOffset - 1),
-                          byteBuffer, true);
+                    ByteBuffer.wrap(buf), lastBufSize,
+                    new ObjectOffset(objectOffset - 1),
+                    byteBuffer, true);
         }
 
         // am.updateMetadata(domainName, volumeName, blobName, tx, metadata);
         am.commitBlobTx(tx);
         return digest;
     }
+
+    public int readFully(InputStream inputStream, byte[] buf) throws IOException {
+        int bytesRead = 0;
+        while (bytesRead < buf.length) {
+            int read = inputStream.read(buf, bytesRead, buf.length - bytesRead);
+            if (read == -1) break;
+            bytesRead += read;
+        }
+        return bytesRead == 0 ? -1 : bytesRead;
+    }
+
 }
--- source/java/test/src/com/formationds/util/ChunkingInputStreamTest.java
+++ source/java/test/src/com/formationds/util/ChunkingInputStreamTest.java
@@ -1,9 +1,7 @@
 package com.formationds.util;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 
 import static org.junit.Assert.assertEquals;
@@ -13,17 +11,8 @@ public class ChunkingInputStreamTest {
     public void testChunking() throws Exception {
         ByteArrayInputStream bais = new ByteArrayInputStream(new byte[] {0, 1, 2, 3, 4, 5, 6, 7});
         ChunkingInputStream in = new ChunkingInputStream(bais, 2);
-        int read = in.read(new byte[8]);
+        byte[] buf = new byte[8];
+        int read = in.read(buf);
         assertEquals(2, read);
-    }
-
-    @Test
-    public void testJamesTheory() throws Exception {
-        ByteArrayInputStream bais = new ByteArrayInputStream(new byte[] {0, 1, 2, 3, 4, 5, 6, 7});
-        ChunkingInputStream chunky = new ChunkingInputStream(bais, 2);
-        BufferedInputStream in = new BufferedInputStream(chunky, 8);
-        int read = chunky.read(new byte[8]);
-        assertEquals(4, read);
-
     }
 }
