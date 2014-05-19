c854123bb98b3d4285201d3f473d37ce191d6253
Premkumar <prem@formationds.com>
2014-05-18 22:21:28 -0700

    new handler mechanism for stor-hvisor

---
+++ source/stor-hvisor/handler/getvolumemetadatahandler.cpp
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2014 Formation Data Systems, Inc.
+ */
+#include <string>
+#include "./handler.h"
+#include "../StorHvisorNet.h"
+
+namespace fds {
+
+Error GetVolumeMetaDataHandler::handleRequest(const std::string& volumeName, CallbackPtr cb) {
+    StorHvCtrl::BlobRequestHelper helper(storHvisor, volumeName);
+    LOGDEBUG << "volume: " << volumeName;
+
+    helper.setupVolumeInfo();
+    helper.blobReq = new GetVolumeMetaDataReq(helper.volId,
+                                              volumeName,
+                                              cb);
+
+    return helper.processRequest();
+}
+
+Error GetVolumeMetaDataHandler::handleResponse(fpi::FDSP_MsgHdrTypePtr& header,
+                                               fpi::FDSP_VolumeMetaDataPtr& volumeMeta) {
+    Error err(ERR_OK);
+    LOGDEBUG << "vol:" << header->glob_volume_id
+             << " txnid:" << header->req_cookie
+             << " blobCount: " << volumeMeta->blobCount
+             << " size:" << volumeMeta->size;
+
+    StorHvCtrl::TxnResponseHelper txnHelper(storHvisor, header->glob_volume_id, header->req_cookie);
+
+    // Return if err
+    if (header->result != FDSP_ERR_OK) {
+        LOGWARN << "error in response: " << header->result;
+        txnHelper.blobReq->cb->call(FDSN_StatusErrorUnknown);
+        return ERR_MAX;
+    }
+
+    GetVolumeMetaDataCallback::ptr cb =
+                SHARED_DYN_CAST(GetVolumeMetaDataCallback, txnHelper.blobReq->cb);
+
+    cb->volumeMetaData = *volumeMeta;
+    LOGDEBUG << "doing callback";
+    cb->call(FDSN_StatusOK);
+    return err;
+}
+
+Error GetVolumeMetaDataHandler::handleQueueItem(AmQosReq *qosReq) {
+    Error err(ERR_OK);
+
+    StorHvCtrl::TxnRequestHelper helper(storHvisor, qosReq->getBlobReqPtr());
+
+    if (!helper.isValidVolume()) {
+        LOGCRITICAL << "unable to get volume info for vol: " << helper.volId;
+        helper.blobReq->cb->call(FDSN_StatusErrorUnknown);
+        delete qosReq;
+        return ERR_DISK_READ_FAILED;
+    }
+
+    if (!helper.setupTxn()) {
+        LOGWARN << "unable to get txn for vol: " << helper.volId;
+        helper.blobReq->cb->call(FDSN_StatusErrorUnknown);
+        delete qosReq;
+        return ERR_DISK_READ_FAILED;
+    }
+
+    LOGDEBUG << "setup txn:" << helper.txnId
+             << " for vol:" << helper.volId;
+
+    helper.txn->op          = FDS_GET_VOLUME_METADATA;
+    helper.txn->io          = qosReq;
+
+    fpi::FDSP_MsgHdrTypePtr msgHdr(new FDSP_MsgHdrType);
+    storHvisor->InitDmMsgHdr(msgHdr);
+    msgHdr->glob_volume_id = helper.volId;
+    msgHdr->req_cookie = helper.txnId;
+    GetVolumeMetaDataReq* blobReq = TO_DERIVED(GetVolumeMetaDataReq, helper.blobReq);
+
+    fds_uint32_t ip, port;
+    helper.getPrimaryDM(ip, port);
+    METADATA_SESSION(session, storHvisor->rpcSessionTbl, ip, port);
+    auto client = session->getClient();
+    msgHdr->session_uuid = session->getSessionId();
+
+    boost::shared_ptr<std::string> volNamePtr(new std::string(blobReq->volumeName));
+
+    // Send async RPC request
+    try {
+        client->GetVolumeMetaData(msgHdr, volNamePtr);
+    } catch(att::TTransportException &e) {
+        LOGERROR << "error during network call: " << e.what();
+        err = ERR_NETWORK_TRANSPORT;
+    }
+    helper.scheduleTimer();
+    return err;
+}
+
+}  // namespace fds
