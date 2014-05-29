a82dcc07fbf6259084572f992d752d664eb5cdea
Fabrice Medio <fmedio@gmail.com>
2014-05-29 09:05:16 -0700

    Actually use volume name

--- source/java/src/com/formationds/om/CreateVolume.java
+++ source/java/src/com/formationds/om/CreateVolume.java
@@ -28,22 +28,23 @@ public class CreateVolume implements RequestHandler {
 
     @Override
     public Resource handle(Request request, Map<String, String> routeParameters) throws Exception {
-        JSONObject o = new JSONObject(IOUtils.toString(request.getInputStream()));
+        String source = IOUtils.toString(request.getInputStream());
+        JSONObject o = new JSONObject(source);
         String name = o.getString("name");
         int priority = o.getInt("priority");
         int sla = o.getInt("sla");
         int limit = o.getInt("limit");
         JSONObject connector = o.getJSONObject("data_connector");
         String type = connector.getString("type");
-        if ("block".equals(type)) {
+        if ("block".equals(type.toLowerCase())) {
             JSONObject attributes = connector.getJSONObject("attributes");
             int sizeUnits = attributes.getInt("size");
             long sizeInBytes = SizeUnit.valueOf(attributes.getString("unit")).totalBytes(sizeUnits);
             VolumeSettings volumeSettings = new VolumeSettings(1024 * 4, VolumeType.BLOCK);
             volumeSettings.setBlockDeviceSizeInBytes(sizeInBytes);
-            configApi.createVolume("", "", volumeSettings);
+            configApi.createVolume("", name, volumeSettings);
         } else {
-            configApi.createVolume("", "", new VolumeSettings(1024 * 1024 * 4, VolumeType.OBJECT));
+            configApi.createVolume("", name, new VolumeSettings(1024 * 1024 * 4, VolumeType.OBJECT));
         }
 
         Thread.sleep(200);
