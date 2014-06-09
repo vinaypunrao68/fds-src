b106c6a4440ad9a125fc98721de08a472b4bd063
Premkumar <prem@formationds.com>
2014-06-09 13:38:33 -0700

    updating to microsecs

--- source/util/timeutils.cpp
+++ source/util/timeutils.cpp
@@ -12,7 +12,7 @@ namespace fds { namespace util {
 const boost::posix_time::ptime epoch(boost::gregorian::date(1970,1,1));
 
 fds_int64_t getTimeStampMillis() {
-    boost::posix_time::time_duration diff = boost::posix_time::second_clock::universal_time()  - epoch;
+    boost::posix_time::time_duration diff = boost::posix_time::microsec_clock::universal_time()  - epoch;
     return diff.total_milliseconds();
 }
 
