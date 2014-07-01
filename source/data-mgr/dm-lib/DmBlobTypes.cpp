f37c74d1df23fe981f09b412ac9a7d24bdae6ac5
Anna Povzner <anna@formationds.com>
2014-07-01 13:55:02 -0700

    Added MetaDataList struct for list of blob metadata k-v pairs. BlobMetaDesc, MetaDataList serialize/deserialize methods.

--- source/data-mgr/dm-lib/DmBlobTypes.cpp
+++ source/data-mgr/dm-lib/DmBlobTypes.cpp
@@ -6,6 +6,89 @@
 
 namespace fds {
 
+//
+// Constructs emty metadata list
+//
+MetaDataList::MetaDataList() {
+}
+
+//
+// Constructs metadata list from fdsp metadata list
+//
+MetaDataList::MetaDataList(const fpi::FDSP_MetaDataList& mlist) {
+    for (fds_uint32_t i = 0; i < mlist.size(); ++i) {
+        (*this)[mlist[i].key] = mlist[i].value;
+    }
+}
+
+MetaDataList::~MetaDataList() {
+}
+
+//
+// Updates metadata value for the given key, or if key does not exist
+// adds new key-value pair
+//
+void MetaDataList::updateMetaDataPair(const std::string& key,
+                                      const std::string& value) {
+    (*this)[key] = value;
+}
+
+//
+// Copies metadata into fdsp metadata list structure
+//
+void MetaDataList::toFdspPayload(fpi::FDSP_MetaDataList& mlist) const {
+    mlist.clear();
+    for (const_iter cit = (*this).cbegin();
+         cit != (*this).cend();
+         ++cit) {
+        fpi::FDSP_MetaDataPair mpair;
+        mpair.key = cit->first;
+        mpair.value = cit->second;
+        mlist.push_back(mpair);
+    }
+}
+
+uint32_t MetaDataList::write(serialize::Serializer* s) const {
+    uint32_t bytes = 0;
+    uint32_t size = (*this).size();
+    bytes += s->writeI32(size);
+    for (const_iter cit = (*this).cbegin();
+         cit != (*this).cend();
+         ++cit) {
+        bytes += s->writeString(cit->first);
+        bytes += s->writeString(cit->second);
+    }
+    return bytes;
+}
+
+uint32_t MetaDataList::read(serialize::Deserializer* d) {
+    uint32_t bytes = 0;
+    uint32_t size = 0;
+    (*this).clear();
+    bytes += d->readI32(size);
+    for (fds_uint32_t i = 0; i < size; ++i) {
+        std::string key;
+        std::string value;
+        bytes += d->readString(key);
+        bytes += d->readString(value);
+        (*this)[key] = value;
+    }
+    return bytes;
+}
+
+std::ostream& operator<<(std::ostream& out, const MetaDataList& metaList) {
+    out << "Metadata: \n";
+    for (MetaDataList::const_iter cit = metaList.cbegin();
+         cit != metaList.cend();
+         ++cit) {
+        out << cit->first << ":" << cit->second << "\n";
+    }
+    return out;
+}
+
+//---------   BlobMetaDesc implementation   -----------//
+
+
 /**
  * Constructs invalid version of a blob meta
  * until someone else actually assigns valid data
@@ -14,23 +97,43 @@ BlobMetaDesc::BlobMetaDesc() {
     version = blob_version_invalid;
     vol_id = invalid_vol_id;
     blob_size = 0;
-    meta_list.clear();
 }
 
 BlobMetaDesc::~BlobMetaDesc() {
 }
 
-void BlobMetaDesc::mkFDSPMetaDataList(fpi::FDSP_MetaDataList& mlist) const {
-    mlist.clear();
-    for (BlobKeyValue::const_iterator cit = meta_list.cbegin();
-         cit != meta_list.cend();
-         ++cit) {
-        fpi::FDSP_MetaDataPair mpair;
-        mpair.key = cit->first;
-        mpair.value = cit->second;
-        mlist.push_back(mpair);
-    }
+uint32_t BlobMetaDesc::write(serialize::Serializer* s) const {
+    uint32_t bytes = 0;
+    bytes += s->writeString(blob_name);
+    bytes += s->writeI64(vol_id);
+    bytes += s->writeI64(version);
+    bytes += s->writeI64(blob_size);
+    bytes += meta_list.write(s);
+    return bytes;
 }
+
+uint32_t BlobMetaDesc::read(serialize::Deserializer* d) {
+    uint32_t bytes = 0;
+    blob_name.clear();
+    bytes += d->readString(blob_name);
+    bytes += d->readI64(reinterpret_cast<int64_t&>(vol_id));
+    bytes += d->readI64(version);
+    bytes += d->readI64(blob_size);
+    bytes += meta_list.read(d);
+    return bytes;
+}
+
+std::ostream& operator<<(std::ostream& out, const BlobMetaDesc& blobMetaDesc) {
+    out << "BlobMeta: "
+        << "name " << blobMetaDesc.blob_name
+        << "volume id " << std::hex << blobMetaDesc.vol_id << std::dec
+        << "version " << blobMetaDesc.version
+        << "size " << blobMetaDesc.blob_size << " bytes ";
+    out << blobMetaDesc.meta_list;
+    return out;
+}
+
+//----------- BlobObjList implementation ---------------//
 
 BlobObjList::BlobObjList() {
 }
@@ -57,7 +160,7 @@ void BlobUtil::toFDSPQueryCatalogMsg(const BlobMetaDesc::const_ptr& blob_meta_de
     query_msg->blob_name = blob_meta_desc->blob_name;
     query_msg->blob_size = blob_meta_desc->blob_size;
     query_msg->blob_version = blob_meta_desc->version;
-    blob_meta_desc->mkFDSPMetaDataList(query_msg->meta_list);
+    (blob_meta_desc->meta_list).toFdspPayload(query_msg->meta_list);
 
     // fill in blob_obj_list
     (query_msg->obj_list).clear();
