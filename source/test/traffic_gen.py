0da592cb492e67aa3f08d737cadca1a39f89b001
Matteo Monchiero <matteo@formationds.com>
2014-07-14 19:07:05 +0000

    Added latency tracking to traffic generator

--- source/test/traffic_gen.py
+++ source/test/traffic_gen.py
@@ -8,8 +8,26 @@ import tempfile
 from Queue import *
 
 stats = {"reqs" : 0,
-	 "fails" : 0
+	 "fails" : 0,
+	 "elapsed_time" : 0.0,
+	 "tot_latency" : 0.0,
+	 "min_latency" : 1.0e10,
+	 "max_latency" : 0.0,
+         "latency_cnt" : 0
 }
+
+def clear_stats():
+	for e in stats:
+		e.second = 0
+
+def update_latency_stats(start, end):
+	elapsed = end - start
+	stats["latency_cnt"] +=1
+	stats["tot_latency"] += elapsed
+	if elapsed < stats["min_latency"]: 
+		stats["min_latency"] = elapsed
+	if elapsed > stats["max_latency"]: 
+		stats["max_latency"] = elapsed
 
 def create_random_file(size):
 	fout, fname = tempfile.mkstemp()
@@ -23,7 +41,6 @@ def create_file_queue(n,size):
 	for i in range(0,n):
 		q.put(create_random_file(size))
 	return q
-
 
 def do_put(conn, target, fname):
 	body = open(fname,"r").read()
@@ -39,6 +56,7 @@ def task(task_id, n_reqs, req_type, file_q):
 	conn = httplib.HTTPConnection("localhost:8000")
 	for i in range(0,n_reqs):
 		#conn.request("PUT", "/volume")
+		time_start = time.time()
 		if req_type == "PUT":
 			do_put(conn, "/volume/file%d:%d" % (task_id,i), files.get())
 			files.task_done()
@@ -47,6 +65,8 @@ def task(task_id, n_reqs, req_type, file_q):
 		elif req_type == "DELETE":
 			do_delete(conn, "/volume/file%d:%d" % (task_id,i))
 		r1 = conn.getresponse()
+		time_end = time.time()
+		update_latency_stats(time_start, time_end)
 		data = r1.read()
 		stats["reqs"] += 1
 		if r1.status != 200:
@@ -89,5 +109,6 @@ if __name__ == "__main__":
 	time_start = time.time()
 	main(options,files)
 	time_end = time.time()
-	print "threads:", options.threads, "n_reqs:", options.n_reqs, "req_type:", options.req_type, "elapsed time:", (time_end - time_start), "reqs/sec:", options.threads * options.n_reqs / (time_end - time_start)
-	print stats
+	stats["elapsed_time"] = time_end - time_start
+	print "threads:", options.threads, "n_reqs:", options.n_reqs, "req_type:", options.req_type, "elapsed time:", (time_end - time_start), "reqs/sec:", options.threads * options.n_reqs / (time_end - time_start), "avg_latency[ms]:",stats["tot_latency"]/stats["latency_cnt"]*1e3
+	print "Options:", options,"Stats:", stats
