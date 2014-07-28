2ac4d5a6018dafdd1547ba413d77d72e21520520
Umesh Agashe <umesh@formationds.com>
2014-07-28 14:23:28 -0700

    WIN-489: unit tests for cache libraries.

--- source/unit-test/lib/kvcache_unit_test.cpp
+++ source/unit-test/lib/kvcache_unit_test.cpp
@@ -2,11 +2,68 @@
  * Copyright 2014 Formation Data Systems, Inc.
  */
 
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <bitset>
+
+#include <fds_types.h>
 #include <cache/VolumeKvCache.h>
+#include <concurrency/ThreadPool.h>
+
+using namespace fds;    // NOLINT
+
+static const fds_uint32_t MAX_TEST_OBJ = 1000;
+static const fds_uint32_t MAX_VOLUMES = 50;
+
+static fds_uint32_t keyCounter = 0;
+
+static fds_threadpool pool;
+
+struct TestObject {
+    fds_volid_t volId;
+    fds_uint32_t key;
+    std::string * value;
+
+    TestObject() : volId(keyCounter % MAX_VOLUMES), key(keyCounter++),
+            value(new std::string(tmpnam(NULL))) {}
+    ~TestObject() {
+        delete value;
+    }
+};
+
+static std::vector<TestObject> testObjs(MAX_TEST_OBJ);
+
+static VolumeCacheManager<fds_uint32_t, std::string> strCacheManager("String cache manager");
+
+static std::bitset<MAX_VOLUMES> volSet;
+
+void getObj(TestObject & obj) {
+    std::string *ptr = 0;
+    strCacheManager.get(obj.volId, obj.key, &ptr);
+    std::cout << "compare " << *obj.value << " = " << *ptr << std::endl;
+    fds_assert(*ptr == *obj.value);
+}
+
+void addObj(TestObject & obj) {
+    std::cout << "Adding value " << *obj.value << " with key " << obj.key << " and address " <<
+        std::hex << obj.value << std::dec << std::endl;
+    strCacheManager.add(obj.volId, obj.key, obj.value);
+    pool.schedule(getObj, obj);
+}
+
+void create(TestObject & obj) {
+    if (!volSet[obj.volId]) {
+        std::cout << "Creating cache for vol " << obj.volId << std::endl;
+        strCacheManager.createCache(obj.volId, 50, fds::LRU);
+        volSet[obj.volId] = 1;
+    }
+    pool.schedule(addObj, obj);
+}
 
 int
 main(int argc, char** argv) {
-    fds::VolumeCacheManager<fds_uint32_t, fds_uint32_t> cacheManager("Unit test cache manager");
+    VolumeCacheManager<fds_uint32_t, fds_uint32_t> cacheManager("Integer cache manager");
     fds::fds_volid_t volId = 321;
     cacheManager.createCache(volId, 3, fds::LRU);
 
@@ -43,6 +100,14 @@ main(int argc, char** argv) {
     }
 
     cacheManager.deleteCache(volId);
+
+    for (std::vector<TestObject>::iterator i = testObjs.begin(); testObjs.end() != i; ++i) {
+        std::cout << "Details volume=" << (*i).volId << " key=" << (*i).key << " value="
+                << *(*i).value << std::endl;
+        create(*i);
+    }
+
+    sleep(-1);
 
     return 0;
 }
