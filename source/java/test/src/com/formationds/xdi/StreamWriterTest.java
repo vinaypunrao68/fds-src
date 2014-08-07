203b91b262110045ac292c9f92977139b2ae2dfd
Andrew Leung <andrew@formationds.com>
2014-08-07 12:52:11 -0700

    Add back missing interface function from AM test code.

--- source/java/test/src/com/formationds/xdi/StreamWriterTest.java
+++ source/java/test/src/com/formationds/xdi/StreamWriterTest.java
@@ -98,6 +98,11 @@ public class StreamWriterTest {
         }
 
         @Override
+        public void updateBlobOnce(String domainName, String volumeName, String blobName, int blobMode, ByteBuffer bytes, int length, ObjectOffset objectOffset, Map<String,String> metadata) throws ApiException, TException {
+
+        }
+
+        @Override
         public void deleteBlob(String domainName, String volumeName, String blobName) throws ApiException, TException {
 
         }
