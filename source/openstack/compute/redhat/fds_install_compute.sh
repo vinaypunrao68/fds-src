af057ad2f5c6caeca19ea4dcc09d75a5c07eaf2a
baop <baothaipham2001@gmail.com>
2014-08-12 17:49:37 -0700

    Daemonize nbdadnd.py. WIN-549.  Add missing file.

--- source/openstack/compute/redhat/fds_install_compute.sh
+++ source/openstack/compute/redhat/fds_install_compute.sh
@@ -12,6 +12,8 @@ cp ./nbd.ko /lib/modules/$(uname -r)/kernel/drivers/block/
 BIN_DIR=/usr/local/bin
 DRIVER=./fds_driver.tar.gz
 DRIVER_DIR=./fds
+INITRC=/etc/init.d/
+FDS_NBD_SVC=openstack-fds-nbdadmd
 
 tar xzvf $DRIVER
 
@@ -28,6 +30,20 @@ if [ ! -e $DRIVER_DIR/nbdadmd.py ]; then
 fi
 echo cp $DRIVER_DIR/nbdadmd.py $BIN_DIR/
 cp $DRIVER_DIR/nbdadmd.py $BIN_DIR/
+
+if [ ! -e $DRIVER_DIR/openstack-fds-nbdadmd ]; then
+    echo "FAIL: openstack-fds-nbdadmd not exist"
+    exit 1
+fi
+
+echo cp $DRIVER_DIR/$FDS_NBD_SVC $INITRC/
+cp $DRIVER_DIR/$FDS_NBD_SVC $INITRC/
+
+echo service $FDS_NBD_SVC start
+service $FDS_NBD_SVC start
+
+echo chkconfig $FDS_NBD_SVC on
+chkconfig $FDS_NBD_SVC on
 
 depmod
 modprobe nbd
