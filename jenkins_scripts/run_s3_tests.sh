04564e56bcead93a55ca1fd0e474a7ffb257dcdf
Jenkins <eidler@formationds.com>
2014-08-15 15:50:17 -0700

    updated test scripts

--- jenkins_scripts/run_s3_tests.sh
+++ jenkins_scripts/run_s3_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd ..
+cd /home/jenkins/build/fds-dev-daily-build-test/
 source/tools/fds cleanstart
 cd s3-tests
 S3TEST_CONF=myconf.conf ./virtualenv/bin/nosetests -d -v -a '!fails_on_rgw' 2> test_output.txt
+tail test_output
 cd ..
 source/tools/fds stop
