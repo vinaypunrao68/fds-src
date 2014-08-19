ee945d931ea6ae491ac5618af35e6e7822d234cd
Premkumar <prem@formationds.com>
2014-08-19 14:55:52 -0700

    alignment fix

--- source/include/kvstore/configdb.h
+++ source/include/kvstore/configdb.h
@@ -15,89 +15,90 @@
 #include <fds_dmt.h>
 #include <fdsp/fds_stream_types.h>
 #include <apis/apis_types.h>
-namespace fds {
-    namespace kvstore {
-        using PolicyInfo = fpi::FDSP_PolicyInfoType;
-        struct ConfigDB : KVStore {
-            ConfigDB(const std::string& host = "localhost", uint port = 6379, uint poolsize = 10);
-            virtual ~ConfigDB();
-            fds_uint64_t getLastModTimeStamp();
+namespace fds { namespace kvstore {
+using PolicyInfo = fpi::FDSP_PolicyInfoType;
 
-            // domains
-            std::string getGlobalDomain();
-            bool setGlobalDomain(ConstString globalDomain="fds");
-            bool addLocalDomain (ConstString name="local", int localDomain = 0, int globalDomain = 0); //NOLINT
-            bool getLocalDomains(std::map<int, std::string>& mapDomains, int globalDomain = 0);
+struct ConfigDB : KVStore {
+    ConfigDB(const std::string& host = "localhost", uint port = 6379, uint poolsize = 10);
+    virtual ~ConfigDB();
+    fds_uint64_t getLastModTimeStamp();
 
-            // volumes
-            bool addVolume(const VolumeDesc& volumeDesc);
-            bool updateVolume(const VolumeDesc& volumeDesc);
-            bool deleteVolume(fds_volid_t volumeId, int localDomain = 0);
-            bool volumeExists(fds_volid_t volumeId);
-            bool volumeExists(ConstString volumeName, int localDomain = 0);
-            bool getVolumeIds(std::vector<fds_volid_t>& volumeIds, int localDomain = 0);
-            bool getVolumes(std::vector<VolumeDesc>& volumes, int localDomain = 0);
-            bool getVolume(fds_volid_t volumeId, VolumeDesc& volumeDesc);
+    // domains
+    std::string getGlobalDomain();
+    bool setGlobalDomain(ConstString globalDomain= "fds");
+    bool addLocalDomain (ConstString name= "local", int localDomain = 0, int globalDomain = 0); //NOLINT
+    bool getLocalDomains(std::map<int, std::string>& mapDomains, int globalDomain = 0);
 
-            // dlt
-            // to store different types of dlt [current,new,old,target]
-            fds_uint64_t getDltVersionForType(const std::string type, int localDomain = 0);
-            bool setDltType(fds_uint64_t version, const std::string type, int localDomain = 0);
+    // volumes
+    bool addVolume(const VolumeDesc& volumeDesc);
+    bool updateVolume(const VolumeDesc& volumeDesc);
+    bool deleteVolume(fds_volid_t volumeId, int localDomain = 0);
+    bool volumeExists(fds_volid_t volumeId);
+    bool volumeExists(ConstString volumeName, int localDomain = 0);
+    bool getVolumeIds(std::vector<fds_volid_t>& volumeIds, int localDomain = 0);
+    bool getVolumes(std::vector<VolumeDesc>& volumes, int localDomain = 0);
+    bool getVolume(fds_volid_t volumeId, VolumeDesc& volumeDesc);
 
-            bool storeDlt(const DLT& dlt, const std::string type = "" , int localDomain = 0);
-            bool getDlt(DLT& dlt, fds_uint64_t version, int localDomain = 0);
-            bool loadDlts(DLTManager& dltMgr, int localDomain = 0);
-            bool storeDlts(DLTManager& dltMgr, int localDomain = 0);
+    // dlt
+    // to store different types of dlt [current, new, old, target]
+    fds_uint64_t getDltVersionForType(const std::string type, int localDomain = 0);
+    bool setDltType(fds_uint64_t version, const std::string type, int localDomain = 0);
 
-            // dmt
-            // to store different types of dmt [committed, target]
-            fds_uint64_t getDmtVersionForType(const std::string type, int localDomain = 0);
-            bool setDmtType(fds_uint64_t version, const std::string type, int localDomain = 0);
+    bool storeDlt(const DLT& dlt, const std::string type = "" , int localDomain = 0);
+    bool getDlt(DLT& dlt, fds_uint64_t version, int localDomain = 0);
+    bool loadDlts(DLTManager& dltMgr, int localDomain = 0);
+    bool storeDlts(DLTManager& dltMgr, int localDomain = 0);
 
-            bool storeDmt(const DMT& dmt, const std::string type = "" , int localDomain = 0);
-            bool getDmt(DMT& dmt, fds_uint64_t version, int localDomain = 0);
+    // dmt
+    // to store different types of dmt [committed, target]
+    fds_uint64_t getDmtVersionForType(const std::string type, int localDomain = 0);
+    bool setDmtType(fds_uint64_t version, const std::string type, int localDomain = 0);
 
-            // nodes
-            bool addNode(const NodeInvData& node);
-            bool updateNode(const NodeInvData& node);
-            bool removeNode(const NodeUuid& uuid);
-            bool getNode(const NodeUuid& uuid, NodeInvData& node);
-            bool nodeExists(const NodeUuid& uuid);
-            bool getNodeIds(std::unordered_set<NodeUuid, UuidHash>& nodes, int localDomain = 0);
-            bool getAllNodes(std::vector<NodeInvData>& nodes, int localDomain = 0);
-            std::string getNodeName(const NodeUuid& uuid);
+    bool storeDmt(const DMT& dmt, const std::string type = "" , int localDomain = 0);
+    bool getDmt(DMT& dmt, fds_uint64_t version, int localDomain = 0);
 
-            bool getNodeServices(const NodeUuid& uuid, NodeServices& services);
-            bool setNodeServices(const NodeUuid& uuid, const NodeServices& services);
-            uint getNodeNameCounter();
+    // nodes
+    bool addNode(const NodeInvData& node);
+    bool updateNode(const NodeInvData& node);
+    bool removeNode(const NodeUuid& uuid);
+    bool getNode(const NodeUuid& uuid, NodeInvData& node);
+    bool nodeExists(const NodeUuid& uuid);
+    bool getNodeIds(std::unordered_set<NodeUuid, UuidHash>& nodes, int localDomain = 0);
+    bool getAllNodes(std::vector<NodeInvData>& nodes, int localDomain = 0);
+    std::string getNodeName(const NodeUuid& uuid);
 
-            // volume policies
-            bool getPolicy(fds_uint32_t volPolicyId, FDS_VolumePolicy& volumePolicy, int localDomain = 0); //NOLINT
-            bool addPolicy(const FDS_VolumePolicy& volumePolicy, int localDomain = 0);
-            bool updatePolicy(const FDS_VolumePolicy& volumePolicy, int localDomain = 0);
-            bool deletePolicy(fds_uint32_t volPolicyId, int localDomain = 0);
-            bool getPolicies(std::vector<FDS_VolumePolicy>& policies, int localDomain = 0);
+    bool getNodeServices(const NodeUuid& uuid, NodeServices& services);
+    bool setNodeServices(const NodeUuid& uuid, const NodeServices& services);
+    uint getNodeNameCounter();
 
-            // stat streaming registrations
-            int32_t getNewStreamRegistrationId();
-            bool addStreamRegistration(fpi::StreamingRegistrationMsg& streamReg);
-            bool removeStreamRegistration(int regId);
-            bool getStreamRegistration(int regId, fpi::StreamingRegistrationMsg& streamReg);
-            bool getStreamRegistrations(std::vector<fpi::StreamingRegistrationMsg>& vecReg);
+    // volume policies
+    bool getPolicy(fds_uint32_t volPolicyId, FDS_VolumePolicy& volumePolicy, int localDomain = 0); //NOLINT
+    bool addPolicy(const FDS_VolumePolicy& volumePolicy, int localDomain = 0);
+    bool updatePolicy(const FDS_VolumePolicy& volumePolicy, int localDomain = 0);
+    bool deletePolicy(fds_uint32_t volPolicyId, int localDomain = 0);
+    bool getPolicies(std::vector<FDS_VolumePolicy>& policies, int localDomain = 0);
 
-            // tenant stuff
-            int64_t createTenant(const std::string& identifier);
-            void listTenants(std::vector<fds::apis::Tenant>& tenants);
-            int64_t createUser(const std::string& identifier, const std::string& secret, bool isAdmin); //NOLINT
-            void assignUserToTenant(int64_t userId, int64_t tenantId);
-            void revokeUserFromTenant(int64_t userId, int64_t tenantId);
-            void listUsersForTenant(std::vector<fds::apis::User>& users, int64_t tenantId);
-            void updateUser(int64_t  userId, const std::string& identifier, const std::string& secret, bool isFdsAdmin); //NOLINT
+    // stat streaming registrations
+    int32_t getNewStreamRegistrationId();
+    bool addStreamRegistration(fpi::StreamingRegistrationMsg& streamReg);
+    bool removeStreamRegistration(int regId);
+    bool getStreamRegistration(int regId, fpi::StreamingRegistrationMsg& streamReg);
+    bool getStreamRegistrations(std::vector<fpi::StreamingRegistrationMsg>& vecReg);
 
-          protected:
-            void setModified();
-        };
-    }  // namespace kvstore
+    // tenant stuff
+    int64_t createTenant(const std::string& identifier);
+    void listTenants(std::vector<fds::apis::Tenant>& tenants);
+    int64_t createUser(const std::string& identifier, const std::string& secret, bool isAdmin); //NOLINT
+    void assignUserToTenant(int64_t userId, int64_t tenantId);
+    void revokeUserFromTenant(int64_t userId, int64_t tenantId);
+    void listUsersForTenant(std::vector<fds::apis::User>& users, int64_t tenantId);
+    void updateUser(int64_t  userId, const std::string& identifier, const std::string& secret, bool isFdsAdmin); //NOLINT
+
+  protected:
+    void setModified();
+};
+
+}  // namespace kvstore
 }  // namespace fds
 
 #endif  // SOURCE_INCLUDE_KVSTORE_CONFIGDB_H_
