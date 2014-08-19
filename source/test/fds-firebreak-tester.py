f15cf8b7d8c8354ae318dff4ed02ecfa50a0f7d6
Brian Madden <brian@formationds.com>
2014-08-18 18:07:25 -0700

    Updated test to find log files, and initial verification code

--- source/test/fds-firebreak-tester.py
+++ source/test/fds-firebreak-tester.py
@@ -11,6 +11,7 @@ with the previous series. This should trigger the metrics in the perf
 counts to change.
 '''
 
+import argparse
 import string
 import os
 import sys
@@ -18,6 +19,9 @@ import requests
 import csv
 import random
 import time
+import datetime
+import re
+import functools
 
 class TestUtil(object):
 
@@ -39,9 +43,47 @@ class TestUtil(object):
             for line in res.iter_lines():
                 if 'BucketAlreadyExists' in line:
                     return
-        print "An error has occurred, and a volume could not be created!"
-        sys.exit(-1)
 
+            print "An error has occurred, and a volume could not be created!"
+            print res
+            sys.exit(-1)
+
+    @staticmethod
+    def single_cap_run(url, args):
+        args = vars(args)
+        del args['url']
+        del args['func']
+
+        for key, val in args.items():
+            if val is None:
+                del args[key]
+
+        cap_run = CapacityTest(**args)
+        cap_run.run(url)
+
+    @staticmethod
+    def single_perf_run(url, args):
+        args = vars(args)
+        del args['url']
+        del args['func']
+
+        for key, val in args.items():
+            if val is None:
+                del args[key]
+        
+        perf_run = PerfTest(**args)
+        perf_run.run(url)
+
+
+NAME_TS_RE = re.compile(r'(.*),(\d*),')
+STCS_RE = re.compile(r'Short term capacity sigma: (\d*.\d*)')
+LTCS_RE = re.compile(r'Long term capacity sigma: (\d*.\d*)')
+STCW_RE = re.compile(r'Short term capacity wma: (-?\d*.\d*)')
+STPS_RE = re.compile(r'Short term perf sigma: (\d*.\d*)')
+LTPS_RE = re.compile(r'Long term perf sigma: (\d*.\d*)')
+STPW_RE = re.compile(r'Short term perf wma: (-?\d*.\d*)')
+
+STATS_RE = re.compile(r'(.*),(.*),\[.* Short term capacity sigma: (\d*.\d*), Long term capacity sigma: (\d*.\d*), Short term capacity wma: (-?\d*.\d*), Short term perf sigma: (\d*.\d*), Long term perf sigma: (\d*.\d*), Short term perf wma: (-?\d*.\d*)')
 
 class CapacityTest(object):
     '''
@@ -49,7 +91,10 @@ class CapacityTest(object):
     test for the firebreak system.
     '''
 
-    def __init__(self, interval=20, avg_size=512, std_dev=10, break_time=300, num_anoms=5):
+    # TODO: Add additional params such as std_dev of anom requests
+
+    def __init__(self, interval=20, avg_size=512, std_dev=10, break_time=300, num_anoms=10,
+                 fds_root='/fds'):
         '''
         Defaults to 20 second interval, 5 minute break time, average size
         of 512 bytes, with 10 byte variance
@@ -66,16 +111,72 @@ class CapacityTest(object):
         self.std_dev = std_dev
         self.break_time = break_time
         self.num_anoms = num_anoms
+        self.fds_root = fds_root
 
-    def __verify(self, start_time, end_time):
+    def __find_logs(self, start_time, end_time, url):
         '''
-        Verifies that there were no anomolies between start_time and end_time
+        Gets all of the stat logs from the directory logdir
+        Params:
+           start_time - int: timestamp of start of script run
+           end_time - int: timestamp of end of script run
+           url - string: url we're making requests to (used to determine volume name)
+        Returns:
+           list of log files to open
+        '''
+        # May need to filter out some log files, not sure if/how this
+        # will be possible without opening each and reading first/last line
+        full_path = os.path.join(self.fds_root, 'user-repo/vol-stats')        
+        files = os.listdir(full_path)
+        out = []
+        for d in files:
+            # These will be volume dirs
+            logs_path = os.path.join(full_path, d)
+            logs = os.listdir(logs_path)
+            for log in logs:
+                # These will be logs within the log dirs
+                with open(os.path.join(logs_path, log), 'r') as fh:
+                    line = fh.readline()
+                    res = NAME_TS_RE.search(line)
+                    if res is not None:
+                        vol_name = res.group(1)
+                        ts = int(res.group(2))
+                    else:
+                        continue
+
+                    v_name = os.path.basename(url)
+                    if v_name != vol_name:
+                        break
+                    out.append(os.path.join(logs_path, log))
+                            
+        return out
+        
+
+    def __verify(self, start_time, end_time, url):
+        '''
+        Compares log numbers vs script settings to ensure expected values
+        Params:
+           start_time - int: The start time of the script
+           end_time - int: The end time of the script run
         '''
         # Open files
-
+        stats = {}
+        files = self.__find_logs(start_time, end_time, url)
+        for f in files:
+            with open(f, 'r') as fh:
+                for line in fh:
+                    res = STATS_RE.match(line)
+                    if res is not None:
+                        # Prune log entries that aren't within our window
+                        ts = datetime.datetime.fromtimestamp(res.group(3))
+                        if start_time <= ts <= end_time:
+                            # We only need the first 3 groups as those are capacity #s
+                            stats[res.group(3)] = (res.group(4), res.group(5),
+                                                   res.group(6))
         # Verify the #s
-        pass
-
+        
+        # The sigma from 0 to break time should be = self.std_dev
+        # The sigma from break time until num_anoms * interval should be > previous sigma
+    
     def run(self, url):
         '''
         Generates PUT requests filled with ~self.avg_size bytes. Sends
@@ -91,7 +192,7 @@ class CapacityTest(object):
         
         # Figure out how many requests to make based on interval/break_time
         num_reqs = self.break_time / self.interval
-        start_time = time.clock()
+        start_time = time.time()
 
         for i in range(num_reqs):
             print "Putting request #" + str(i)
@@ -113,7 +214,6 @@ class CapacityTest(object):
             print "Putting anomalous request #" + str(i)
             rand_dev = random.randint(2, 6) # How many standard deviations from the avg
             rand = random.randint(0, rand_dev * self.std_dev)
-            rand = (rand * -1) if random.randint(0, 1) == 0 else rand
             temp_data = os.urandom(self.avg_size + rand)
             res = requests.put(os.path.join(url, TestUtil.gen_random_name(6)), data=temp_data)
             if res.status_code != 200:
@@ -122,9 +222,9 @@ class CapacityTest(object):
             
             time.sleep(self.interval)
                 
-        end_time = time.clock()
+        end_time = time.time()
 
-        self.__verify(start_time, end_time)
+        self.__verify(start_time, end_time, url)
 
 class PerfTest(object):
     '''
@@ -137,19 +237,21 @@ class PerfTest(object):
     the firebreak.
     '''
 
-    def __init__(self, avg_rate=10, std_dev=2, break_time=300, num_anoms=10):
+    def __init__(self, avg_rate=10, std_dev=2, break_time=300, anom_rate=1, num_anoms=10):
         '''
         Params:
            avg_rate - int: average 'normal' request rate in seconds
            std_dev - int: standard deviation of normal request rate in seconds
            break_time - int: time after which the system should issue
                         requests at faster rate
+           anom_rate - int: rate at which anomalous requests will be sent
            num_anoms - int: number of anomalous requests to make
         '''
         self.avg_rate = avg_rate
         self.std_dev = std_dev
         self.break_time = break_time
         self.num_anoms = num_anoms
+        self.anom_rate = anom_rate
         self.__valid_gets = []
         
     def __make_random_request(self, url):
@@ -225,10 +327,33 @@ class PerfTest(object):
 
 if __name__ == "__main__":
 
-    # Run capacity test
-    # cap_test = CapacityTest()
-    # cap_test.run('http://localhost:8000/abc')
+    # Parse CLI args
+    parser = argparse.ArgumentParser(description='Run tests for '
+                                     'performance or capacity firebreaks.')
 
-    # Run performance test
-    perf_test = PerfTest()
-    perf_test.run('http://localhost:8000/abc')
+    parser.add_argument('url', type=str, help='URL to send PUT/GET requests to. This should include volume name, e.g.'
+                        'http://localhost:8000/vol1')
+    parser.add_argument('--fds-root', type=str, help='FDS root location.', default='/fds')
+    
+    subparsers = parser.add_subparsers()
+    cap_parser = subparsers.add_parser('capacity', help='Run a capacity firebreak test.')
+    cap_parser.add_argument('--interval', type=int, help='Interval in seconds between requests. Default: 20')
+    cap_parser.add_argument('--avg_size', type=int, help='Average object size. Default: 512')
+    cap_parser.add_argument('--std_dev', type=int, help='Standard deviation of normal requests. Default: 10')
+    cap_parser.add_argument('--break_time', type=int, help='Time in seconds after which firebreak should occur. Default: 300')
+    cap_parser.add_argument('--num_anoms', type=int, help='Number of anomalous requests to make after break_time.'
+                            ' These will occur at interval time. Default: 10')
+    cap_parser.set_defaults(func=TestUtil.single_cap_run)
+
+    perf_parser = subparsers.add_parser('performance', help='Run a performance firebreak test.')
+    # avg_rate=10, std_dev=2, break_time=300, num_anoms=10
+    perf_parser.add_argument('--avg_rate', type=int, help='Average rate in seconds to send requests. Default: 10')
+    perf_parser.add_argument('--std_dev', type=int, help='Standard deviation in seconds in request rate. Default: 2')
+    perf_parser.add_argument('--break_time', type=int, help='Number of seconds after which firebreak should occur. Default: 300')
+    perf_parser.add_argument('--anom_rate', type=int, help='Anomalous request rate in seconds. Default: 1')
+    perf_parser.add_argument('--num_anoms', type=int, help='Number of anomalous requests to make. Default: 10')
+    perf_parser.set_defaults(func=TestUtil.single_perf_run)
+    
+    args = parser.parse_args()
+    print args
+    args.func(args.url, args)
