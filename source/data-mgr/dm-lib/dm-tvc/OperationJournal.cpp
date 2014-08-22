7f480c7be8edaf909031434226cc2d52da178286
Umesh Agashe <umesh@formationds.com>
2014-08-22 14:48:23 -0700

    WIP[WIN-734]: Changes to commit log for GC and persistence

---
+++ source/data-mgr/dm-lib/dm-tvc/OperationJournal.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2014 Formation Data Systems, Inc.
+ */
+
+#include <boost/shared_ptr.hpp>
+#include <dm-tvc/OperationJournal.h>
+
+namespace fds {
+
+void DmTvcOperationJournal::log(const CommitLogTx & tx) {
+    // TODO(umesh): implement this
+}
+
+}   // namespace fds
---
+++ source/data-mgr/include/dm-tvc/OperationJournal.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2014 Formation Data Systems, Inc.
+ */
+#ifndef SOURCE_DATA_MGR_INCLUDE_DM_TVC_OPERATIONJOURNAL_H_
+#define SOURCE_DATA_MGR_INCLUDE_DM_TVC_OPERATIONJOURNAL_H_
+
+#include <fds_types.h>
+
+namespace fds {
+
+struct CommitLogTx;
+
+/**
+ * Journal of all write operations pushed to VolumeCatalog
+ */
+class DmTvcOperationJournal {
+  public:
+    typedef boost::shared_ptr<DmTvcOperationJournal> ptr;
+    typedef boost::shared_ptr<const DmTvcOperationJournal> const_ptr;
+
+    explicit DmTvcOperationJournal(fds_volid_t volId) : volId_(volId) {}
+    virtual ~DmTvcOperationJournal() {}
+
+    inline fds_volid_t volId() const {
+        return volId_;
+    }
+
+    // log operation
+    virtual void log(const CommitLogTx & tx);
+
+    // retrieve operations
+    // TODO(umesh): implement this
+
+  private:
+    const fds_volid_t volId_;
+};
+}  /* namespace fds */
+
+#endif  // SOURCE_DATA_MGR_INCLUDE_DM_TVC_OPERATIONJOURNAL_H_
