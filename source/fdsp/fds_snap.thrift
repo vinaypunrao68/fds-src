08eb3467f4204083e5922081fa1bdfa069006acf
Umesh Agashe <umesh@formationds.com>
2014-08-26 09:48:46 -0700

    Updates to thrift interface for snapshot

--- source/fdsp/fds_snap.thrift
+++ source/fdsp/fds_snap.thrift
@@ -5,51 +5,76 @@ include "FDSP.thrift"
 namespace cpp FDS_ProtocolInterface
 
 /*
- * snapshot  and clone CLI interface  structures 
+ * snapshot and clone from CLI => OM structures 
  */
 struct SnapshotPolicyMsg {
-     1: string policyName,
-     2: i64  snapFrequency,
-     3: i64  retensionTime,
-     4: i64  timeOfDay,
-     5: i64  dayOfMonth
+    1: string policyName,   /* Name of the policy, should be unique */
+    2: string recurrenceRule, /* Recurrence rule as per iCal format */
+    3: i64 retentionTime    /* Retention time in seconds */
 }
 
-struct CreateSnapshot {
-     1: string volName,
-     2: string snapPolicyName
+struct SnapshotPolicyRespMsg {
+    1: i64 id   /* id of newly created policy */
+}
+
+struct AttachVolumeToSnapshotPolicyMsg {
+    1: string volName,
+    2: string snapshotPolicyName
+}
+
+struct AttachVolumeToSnapshotPolicyRespMsg {
+    1: i64 id
+}
+
+struct DetachVolumeFromSnapshotPolicyMsg {
+    1: string volName,
+    2: string snapshotPolicyName
+}
+
+struct DetachVolumeFromSnapshotPolicyRespMsg {
+}
+
+struct ListSnapshotForVolumeMsg {
+    1: string volName
+}
+
+struct ListSnapshotForVolumeRespMsg {
+    1: i64 id,
+    2: string snapshotName,
+    3: i64 timestamp
+}
+
+struct RestoreVolumeFromSnapshotMsg {
+    1: string volName,
+    2: string snapshotName
+}
+
+struct RestoreVolumeFromSnapshotRespMsg {
+}
+
+/* From OM => DM */
+struct CreateSnapshotMsg {
+    1: string volName,
+    2: string snapshotPolicyName
 }  
 
-struct deleteSnapshot {
-     1: string snapName  /* snap name is system generated, list and provide one*/
+struct CreateSnapshotRespMsg {
+    1: string snapshotName
 }
 
-struct attachVolSnapPloicy {
-     1: string volName,
-     2: string snapPolicyName
+struct DeleteSnapshotMsg {
+    1: string snapshotName  /* snap name is system generated, list and provide one*/
 }
 
-struct detachVolSnapPloicy {
-     1: string volName,
-     2: string snapPolicyName
+struct DeleteSnapshotRespMsg {
 }
 
-struct CreateVolumeClone {
+struct CreateVolumeCloneMsg {
      1: string volName,
      2: string cloneName,
      3: string clonePolicyName /* this is same as volume policy  structure */
 }  
 
-struct DeleteVolumeClone {
-     1: string volName,
-     2: string cloneName
+struct CreateVolumeCloneRespMsg {
 }
 
-struct RestoreVolumeSnap {
-     1: string volName,
-     2: string snapName  /* snap name is system generated, list and provide one*/
-}
-
-struct listVolumeSnap {
-     1: string volName
-}
