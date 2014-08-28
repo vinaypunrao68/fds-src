a3a6ce988475d11d2dbc7cfb74fbf51cd58e2c75
nickeidler <nickeidler@gmail.com>
2014-08-28 15:08:44 -0700

    updated smoke script to use Boto and fixed make-precheckin

--- source/test/fds-s3-compliance.py
+++ source/test/fds-s3-compliance.py
@@ -28,20 +28,11 @@ class TestMultiUpload(unittest.TestCase):
      Assumes that FDS is already running locally
     '''
     def setUp(self):
-        # Connect to FDS S3 interface
-        self.c = boto.connect_s3(
-                aws_access_key_id='blablabla',
-                aws_secret_access_key='kekekekek',
-                host=HOST,
-                port=PORT,
-                is_secure=False,
-                calling_format=boto.s3.connection.OrdinaryCallingFormat())
-        log('Connected!')
-
+        global c
         # Create bucket
         #TODO: Randomized bucket names are a temporary workaround for dodgy bucket delete on the server side.
         self._bucket_name = BUCKET_NAME + str(random.randint(0, 100000))
-        self.b = self.c.create_bucket(self._bucket_name)
+        self.b = c.create_bucket(self._bucket_name)
         log('Bucket <%s> created!' % self._bucket_name)
 
         # Get file info
@@ -99,22 +90,59 @@ class TestMultiUpload(unittest.TestCase):
         except Exception as e:
             print e
 
+    def test_multi_upload_list(self):
+        # Create multipart upload request
+        _key_name = os.path.basename(self.source_path)
+        mp = self.b.initiate_multipart_upload(_key_name)
+        log('MPU initiated!')
+
+        # Use a chunk size of 50 MB
+        chunk_size = 52428800
+        chunk_count = int(math.ceil(self.source_size / chunk_size))
+
+        # Send the file parts, using FileChunkIO to create a file-like object
+        # that points to a certain byte range within the original file. We
+        # set bytes to never exceed the original file size.
+        # For this particular test case we will stop halfway
+        for i in range((chunk_count / 2) + 1):
+            offset = chunk_size * i
+            bytes = min(chunk_size, self.source_size - offset)
+            with FileChunkIO(self.source_path, 'r', offset=offset, bytes=bytes) as fp:
+                mp.upload_part_from_file(fp, part_num=i + 1)
+        log('MPU Pieces Uploaded!')
+
+        parts_list = self.b.list_multipart_uploads()
+        for part in parts_list:
+            log(str(part))
+        log('Listing complete!')
+
     def tearDown(self):
         log('Beginning teardown...')
-        self.b.delete_keys(self.b.get_all_keys())
+        for key in self.b.list():
+            key.delete()
         log('Keys successfully deleted')
 
         #TODO: Waiting until bug is fixed upstream, temporary workaround
         try:
             self.b.delete()
         except Exception as e:
-            print e
+            log(str(e))
             log('Bucket successfully deleted!')
 
-        self.c.close()
         log('Teardown complete!')
 
 if __name__ == '__main__':
+    global c
+    # Connect to FDS S3 interface
+    c = boto.connect_s3(
+        aws_access_key_id='blablabla',
+        aws_secret_access_key='kekekekek',
+        host=HOST,
+        port=PORT,
+        is_secure=False,
+        calling_format=boto.s3.connection.OrdinaryCallingFormat())
+    log('Connected!')
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', action='store_true', help='Enables verbose mode')
     args = parser.parse_args()
@@ -122,3 +150,6 @@ if __name__ == '__main__':
 
     suite = unittest.TestLoader().loadTestsFromTestCase(TestMultiUpload)
     unittest.TextTestRunner(verbosity=2).run(suite)
+
+    c.close()
+    log('All tests complete!')
