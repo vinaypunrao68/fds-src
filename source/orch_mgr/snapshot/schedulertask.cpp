d11a6cae6863cb6dc45c87530ca1aa16d7c37903
Premkumar <prem@formationds.com>
2014-09-02 11:28:04 -0700

    set the right recurrence time

--- source/orch_mgr/snapshot/schedulertask.cpp
+++ source/orch_mgr/snapshot/schedulertask.cpp
@@ -5,13 +5,36 @@
 #include <memory>
 #include <functional>
 #include <string>
-
+#include <libical/ical.h>
 namespace fds { namespace snapshot {
 
 bool Task::setRecurrence(std::string recurStr) {
     if (this->recurStr != recurStr) {
         this->recurStr = recurStr;
         recur = icalrecurrencetype_from_string(recurStr.c_str());
+
+        // set the defaults as minute, second, hour
+        // so that the event is triggered exactly at the hour/minute start
+        // 00:00:00
+
+        if (recur.by_second[0] == ICAL_RECURRENCE_ARRAY_MAX
+            && recur.freq > ICAL_SECONDLY_RECURRENCE) {
+            recur.by_second[1] = ICAL_RECURRENCE_ARRAY_MAX;
+            recur.by_second[0] = 0;
+        }
+
+        if (recur.by_minute[0] == ICAL_RECURRENCE_ARRAY_MAX
+            && recur.freq > ICAL_MINUTELY_RECURRENCE) {
+            recur.by_minute[1] = ICAL_RECURRENCE_ARRAY_MAX;
+            recur.by_minute[0] = 0;
+        }
+
+        if (recur.by_hour[0] == ICAL_RECURRENCE_ARRAY_MAX
+            && recur.freq > ICAL_HOURLY_RECURRENCE) {
+            recur.by_hour[1] = ICAL_RECURRENCE_ARRAY_MAX;
+            recur.by_hour[0] = 0;
+        }
+
         setNextRecurrence();
         return true;
     }
@@ -31,7 +54,14 @@ uint64_t Task::getNextRecurrance() {
     ritr = icalrecur_iterator_new(recur, start);
     next = icalrecur_iterator_next(ritr);
     if (!icaltime_is_null_time(next)) {
-        tt = icaltime_as_timet(start);
+        tt = icaltime_as_timet(next);
+        if (runAtTime == (uint64_t)tt) {
+            tt = 0;
+            next = icalrecur_iterator_next(ritr);
+            if (!icaltime_is_null_time(next)) {
+                tt = icaltime_as_timet(next);
+            }
+        }
     }
     icalrecur_iterator_free(ritr);
     return tt;
@@ -44,7 +74,11 @@ bool Task::operator < (const Task& task) const {
 }
 
 std::ostream& operator<<(std::ostream& os, const Task& task) {
-    os << "[policy:" << task.policyId <<" at:" << task.runAtTime << "]";
+    time_t tt = (time_t)task.runAtTime;
+    os << "[policy:" << task.policyId
+       << " at:" << task.runAtTime
+       << " : " << asctime(gmtime(&tt)) //NOLINT
+       << "]";
     return os;
 }
 
