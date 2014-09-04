92db5f26b9d4a4337e1a1026a50aff416527c86e
Premkumar <prem@formationds.com>
2014-09-04 15:40:30 -0700

    adding svchandler into snapshot ns

--- source/orch_mgr/include/snapshot/svchandler.h
+++ source/orch_mgr/include/snapshot/svchandler.h
@@ -4,36 +4,39 @@
  * Copyright 2014 Formation Data Systems, Inc.
  */
 
-#ifndef SOURCE_ORCH_MGR_INCLUDE_OMSVCHANDLER_H_
-#define SOURCE_ORCH_MGR_INCLUDE_OMSVCHANDLER_H_
+#ifndef SOURCE_ORCH_MGR_INCLUDE_SNAPSHOT_SVCHANDLER_H_
+#define SOURCE_ORCH_MGR_INCLUDE_SNAPSHOT_SVCHANDLER_H_
 
 #include <string>
 #include <fds_error.h>
 #include <fds_types.h>
 #include <fds_typedefs.h>
 #include <fdsp/fds_service_types.h>
+#include <fdsp/snapshot_types.h>
 #include <net/SvcRequestPool.h>
 
 namespace fds {
+class OrchMgr;
+namespace snapshot {
 
 class OmSnapshotSvcHandler {
   public:
-    /*
-     * message handler 
-     */ 
-     Error omSnapshotCreate(const fpi::CreateSnapshotMsgPtr &stSnapCreateTxMsg);
-     void omSnapshotCreateResp(QuorumSvcRequest* svcReq,
-                            const Error& error,
-                            boost::shared_ptr<std::string> payload);
-     Error omSnapshotDelete(fds_uint64_t snapshotId, fds_uint64_t volId);
-     void omSnapshotDeleteResp(QuorumSvcRequest* svcReq,
-                            const Error& error,
-                            boost::shared_ptr<std::string> payload);
-     Error omVolumeCloneCreate(const fpi::CreateVolumeCloneMsgPtr &stVolumeCloneTxMsg);
-     void  omVolumeCloneCreateResp(QuorumSvcRequest* svcReq,
-                            const Error& error,
-                            boost::shared_ptr<std::string> payload);
-   private:
+    explicit OmSnapshotSvcHandler(OrchMgr* om);
+    Error omSnapshotCreate(const fpi::Snapshot& snapshot);
+    void omSnapshotCreateResp(QuorumSvcRequest* svcReq,
+                              const Error& error,
+                              boost::shared_ptr<std::string> payload);
+    Error omSnapshotDelete(fds_uint64_t snapshotId, fds_uint64_t volId);
+    void omSnapshotDeleteResp(QuorumSvcRequest* svcReq,
+                              const Error& error,
+                              boost::shared_ptr<std::string> payload);
+    Error omVolumeCloneCreate(const fpi::CreateVolumeCloneMsgPtr &stVolumeCloneTxMsg);
+    void  omVolumeCloneCreateResp(QuorumSvcRequest* svcReq,
+                                  const Error& error,
+                                  boost::shared_ptr<std::string> payload);
+  protected:
+    OrchMgr* om;
 };
+}  // namespace snapshot
 }  // namespace fds
-#endif  // SOURCE_ORCH_MGR_INCLUDE_OMSVCHANDLER_H_
+#endif  // SOURCE_ORCH_MGR_INCLUDE_SNAPSHOT_SVCHANDLER_H_
--- source/orch_mgr/snapshot/svchandler.cpp
+++ source/orch_mgr/snapshot/svchandler.cpp
@@ -3,27 +3,30 @@
  * Copyright 2014 Formation Data Systems, Inc.
  */
 #include <string>
-#include <OmSvcHandler.h>
+
 #include <fds_error.h>
 #include <fds_types.h>
 #include <net/net-service.h>
 #include <net/net-service-tmpl.hpp>
 #include <net/SvcRequestPool.h>
+#include <snapshot/svchandler.h>
 #include <orchMgr.h>
 
-namespace fds {
-Error
-OmSnapshotSvcHandler::omSnapshotCreate(const fpi::CreateSnapshotMsgPtr &stSnapCreateTxMsg) {
-    Error err(ERR_OK);
-    fds_verify(stSnapCreateTxMsg != NULL);
-    fds_volid_t   volId = stSnapCreateTxMsg->snapshot.volumeId;
-    fpi::CreateSnapshotRespMsg crtSnapshotRespMsg;
+namespace fds { namespace snapshot {
 
+OmSnapshotSvcHandler::OmSnapshotSvcHandler(OrchMgr* om) : om(om) {
+}
+
+Error OmSnapshotSvcHandler::omSnapshotCreate(const fpi::Snapshot& snapshot) {
+    Error err(ERR_OK);
+    fds_volid_t   volId = snapshot.volumeId;
+
+    fpi::CreateSnapshotMsgPtr msg(new fpi::CreateSnapshotMsg());
+    msg->snapshot = snapshot;
     auto cb = RESPONSE_MSG_HANDLER(OmSnapshotSvcHandler::omSnapshotCreateResp);
     auto asyncReq = gSvcRequestPool->newQuorumSvcRequest(
-        boost::make_shared<DltObjectIdEpProvider>(gl_orch_mgr->getDMTNodesForVolume(volId)));
-    asyncReq->setPayload(FDSP_MSG_TYPEID(fpi::CreateSnapshotMsg),
-                                                                 stSnapCreateTxMsg);
+        boost::make_shared<DltObjectIdEpProvider>(om->getDMTNodesForVolume(volId)));
+    asyncReq->setPayload(FDSP_MSG_TYPEID(fpi::CreateSnapshotMsg), msg);
     asyncReq->onResponseCb(cb);
     asyncReq->invoke();
 
@@ -37,8 +40,7 @@ void OmSnapshotSvcHandler::omSnapshotCreateResp(QuorumSvcRequest* svcReq,
 }
 
 
-Error
-OmSnapshotSvcHandler::omSnapshotDelete(fds_uint64_t snapshotId, fds_uint64_t volId) {
+Error OmSnapshotSvcHandler::omSnapshotDelete(fds_uint64_t snapshotId, fds_uint64_t volId) {
     Error err(ERR_OK);
 
     fpi::DeleteSnapshotMsgPtr stSnapDeleteTxMsg(new fpi::DeleteSnapshotMsg());
@@ -47,7 +49,7 @@ OmSnapshotSvcHandler::omSnapshotDelete(fds_uint64_t snapshotId, fds_uint64_t vol
 
     auto cb = RESPONSE_MSG_HANDLER(OmSnapshotSvcHandler::omSnapshotDeleteResp);
     auto asyncReq = gSvcRequestPool->newQuorumSvcRequest(
-        boost::make_shared<DltObjectIdEpProvider>(gl_orch_mgr->getDMTNodesForVolume(volId)));
+        boost::make_shared<DltObjectIdEpProvider>(om->getDMTNodesForVolume(volId)));
 
     asyncReq->setPayload(FDSP_MSG_TYPEID(fpi::DeleteSnapshotMsg), stSnapDeleteTxMsg);
     asyncReq->onResponseCb(cb);
@@ -62,15 +64,14 @@ void OmSnapshotSvcHandler::omSnapshotDeleteResp(QuorumSvcRequest* svcReq,
                             boost::shared_ptr<std::string> payload) {
 }
 
-Error
-OmSnapshotSvcHandler::omVolumeCloneCreate(const fpi::CreateVolumeCloneMsgPtr &msg) {
+Error OmSnapshotSvcHandler::omVolumeCloneCreate(const fpi::CreateVolumeCloneMsgPtr &msg) {
     Error err(ERR_OK);
     fds_verify(msg != NULL);
     fds_volid_t   volId = msg->volumeId;
 
     auto cb = RESPONSE_MSG_HANDLER(OmSnapshotSvcHandler::omVolumeCloneCreateResp);
     auto asyncReq = gSvcRequestPool->newQuorumSvcRequest(
-        boost::make_shared<DltObjectIdEpProvider>(gl_orch_mgr->getDMTNodesForVolume(volId)));
+        boost::make_shared<DltObjectIdEpProvider>(om->getDMTNodesForVolume(volId)));
 
     asyncReq->setPayload(FDSP_MSG_TYPEID(fpi::CreateVolumeCloneMsg), msg);
     asyncReq->onResponseCb(cb);
@@ -85,5 +86,5 @@ void OmSnapshotSvcHandler::omVolumeCloneCreateResp(QuorumSvcRequest* svcReq,
                             const Error& error,
                             boost::shared_ptr<std::string> payload) {
 }
-
+}  // namespace snapshot
 }  // namespace fds
