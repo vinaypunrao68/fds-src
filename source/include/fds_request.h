59b1d1e95f7435b50c0457ef256f01dacbca4d51
Vy Nguyen <vynguyen@bienhoa>
2014-09-09 17:36:36 -0700

    Increamental merge from vy-transport to dev. - Generic state machine lib. - Generic tracebuffer lib.

--- source/include/fds_request.h
+++ source/include/fds_request.h
@@ -11,6 +11,18 @@
 
 namespace fdsio {
 
+class Request;
+class RequestIter
+{
+  public:
+    RequestIter() {}
+    virtual ~RequestIter() {}
+
+    // Return true will terminate the iter loop.
+    //
+    virtual bool rq_iter_fn(Request *req) = 0;
+};
+
 //
 // Generic Request Queue
 // ---------------------
@@ -42,8 +54,17 @@ class RequestQueue
     RequestQueue(int nr_queue, int max_depth);
     virtual ~RequestQueue();
 
-    virtual void rq_enqueue(Request *rqt, int queue_idx);
-    virtual void rq_move_queue(Request *rqt, int new_idx);
+    virtual void rq_enqueue(Request *rqt, int queue_idx, fds_uint32_t flag = 0);
+    virtual void rq_enqueue_mtx(Request *rqt, int queue_idx, fds_uint32_t flag = 0);
+
+    virtual void rq_detach(Request *rqt);
+    virtual void rq_detach_mtx(Request *rqt);
+
+    virtual void rq_move_queue(Request *rqt, int new_idx, fds_uint32_t flag = 0);
+    virtual void rq_move_queue_mtx(Request *rqt, int new_idx, fds_uint32_t flag = 0);
+
+    virtual void rq_iter_queue(RequestIter *it, int queue_idx);
+    virtual void rq_iter_queue_mtx(RequestIter *it, int queue_idx);
 
     // \rq_request_wait
     // ----------------
@@ -74,6 +95,24 @@ class RequestQueue
     // @return: nullptr if the queue is empty.
     //
     virtual Request *rq_dequeue(int queue_idx, int new_idx);
+    virtual Request *rq_dequeue_mtx(int queue_idx, int new_idx);
+
+    // rq_peek
+    // -------
+    // @return FIFO element from the queue at the given index.
+    //
+    virtual Request *rq_peek_set_flag(int queue_idx, fds_uint32_t flag);
+    virtual Request *rq_peek_set_flag_mtx(int queue_idx, fds_uint32_t flag);
+
+    inline Request *rq_peek(int queue_idx) {
+        return rq_peek_set_flag(queue_idx, 0);
+    }
+    inline Request *rq_peek_mtx(int queue_idx) {
+        return rq_peek_set_flag_mtx(queue_idx, 0);
+    }
+    inline fds::fds_mutex *rq_get_mtx() {
+        return &rq_mutex;
+    }
 
   private:
     int                      rq_nr_queue;
@@ -81,15 +120,6 @@ class RequestQueue
     int                      rq_pending;
     fds::ChainList           *rq_lists;
     fds::fds_mutex           rq_mutex;
-};
-
-class RequestStatus
-{
-  public:
-    RequestStatus();
-    ~RequestStatus();
-
-    std::string              req_result;
 };
 
 //
@@ -120,6 +150,7 @@ class Request
     static const fds_uint32_t req_state_wait = 0x20000000;
     static const fds_uint32_t req_state_done = 0x40000000;
     static const fds_uint32_t req_block      = 0x80000000;
+    static const fds_uint32_t req_user_flag  = 0x0000ffff;
 
     // \req_abort
     // ----------
@@ -139,7 +170,7 @@ class Request
     // ---------
     // Block the caller until the request is done.
     //
-    virtual void req_wait();
+    virtual void req_wait(bool set_block = false);
 
     // \req_complete
     // -------------
@@ -165,19 +196,30 @@ class Request
     //
     virtual void req_set_wakeup_cond();
 
-    // \req_status
-    // -----------
+    // Must be called under's queue mutex if it's linked to that queue.
     //
-    virtual void req_status(RequestStatus *status);
+    void req_set_uflags(fds_uint32_t flags);
 
     // \req_blocking_mode
     // ------------------
     // Return true if the request is in the blocking mode.
     //
-    inline bool req_blocking_mode()
-    {
+    inline bool req_blocking_mode() {
         return ((req_queue != nullptr) && (req_state & Request::req_block));
     }
+    inline void req_set_blocking_mode() {
+        req_state |= Request::req_block;
+    }
+    inline fds_uint32_t req_get_uflags() const {
+        return (req_state & Request::req_user_flag);
+    }
+    // \req_owner_queue
+    // ----------------
+    //
+    inline const RequestQueue *req_owner_queue() const {
+        return req_queue;
+    }
+
   private:
     friend class RequestQueue;
     fds::ChainLink           req_link;
@@ -186,7 +228,6 @@ class Request
 
   protected:
     fds_uint32_t             req_state;
-    RequestStatus            req_res;
 };
 
 }  // namespace fdsio
