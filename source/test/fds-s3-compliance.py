bd849c8c8c952ac824c8bb343601bef28324cab1
nickeidler <nickeidler@gmail.com>
2014-09-11 14:05:35 -0700

    added new MPU test case, removed unsupported feature test case

--- source/test/fds-s3-compliance.py
+++ source/test/fds-s3-compliance.py
@@ -53,17 +53,18 @@ class TestMultiUpload(unittest.TestCase):
         for b in rs:
             log(b.name)
 
-    def test_list_bucket_objects_prefix(self):
-        global c
-        log('Uploading small object to %s' % self._bucket_name)
-        k = Key(self.b)
-        k.key = 'pref/test_key'
-        k.set_contents_from_filename(SMALL_DATA_FILE)
-
-        log('Listing keys...')
-        list = self.b.list(prefix='pref/')
-        for key in list:
-            log(key.key)
+    # # Not supported for Goldman release
+    # def test_list_bucket_objects_prefix(self):
+    #     global c
+    #     log('Uploading small object to %s' % self._bucket_name)
+    #     k = Key(self.b)
+    #     k.key = 'pref/test_key'
+    #     k.set_contents_from_filename(SMALL_DATA_FILE)
+    #
+    #     log('Listing keys...')
+    #     list = self.b.list(prefix='pref/')
+    #     for key in list:
+    #         log(key.key)
 
     def test_basic_head_object(self):
         global c
@@ -82,7 +83,6 @@ class TestMultiUpload(unittest.TestCase):
         log('Creating auxiliary bucket %s' % _aux_bucket_name)
         _aux_b = c.create_bucket(_aux_bucket_name)
         log('Bucket %s created!' % _aux_bucket_name)
-        time.sleep(1)
 
         # Upload a key
         log('Uploading test key...')
@@ -102,9 +102,10 @@ class TestMultiUpload(unittest.TestCase):
 
         # Cleanup
         log('Cleaning up put_copy test results (data, bucket, keys)...')
-        os.system('rm /copied_output')
+        os.system('rm /tmp/copied_output')
         for _k in _aux_b.list():
             _k.delete()
+        time.sleep(7)
         _aux_b.delete()
 
     def test_multi_upload_to_completion(self):
@@ -142,12 +143,58 @@ class TestMultiUpload(unittest.TestCase):
         # Cleanup
         os.system('rm ' + DATA_FILE + '_verify')
 
-    def test_multi_upload_copy(self):
-        # Create multipart upload request
-        _key_name = os.path.basename(self.source_path)
-        mp = self.b.initiate_multipart_upload(_key_name)
-        log('MPU initiated!')
+    def test_multi_upload_copy_multi_key(self):
+        log('Uploading test keys')
+        for i in range(4):
+            _k = Key(self.b)
+            _k.key = 'test_key' + str(i)
+            _k.set_contents_from_filename(SMALL_DATA_FILE)
 
+        # Verify keys
+        log('Verifying keys')
+        for i in range(4):
+            _k = Key(self.b)
+            _k.key = 'test_key' + str(i)
+            _k.get_contents_to_filename('/tmp/test_verify' + str(i))
+            assert(hashlib.sha1(open('/tmp/test_verify' + str(i), 'rb').read()).hexdigest() == hashlib.sha1(open(SMALL_DATA_FILE).read()).hexdigest())
+
+        # Create MPU req
+        _key_name = 'MPU_Key'
+        mp = self.b.initiate_multipart_upload(_key_name)
+        log('MPU Initiated!')
+        log('Copying keys into MP file')
+
+        for i in range(4):
+            mp.copy_part_from_key(self.b, 'test_key' + str(i), i+1)
+
+        # Finish the upload
+        log('Completing MP upload...')
+        mp.complete_upload()
+        log('MPU Complete!')
+
+        log('Retrieving MP object...')
+        k = Key(self.b)
+        k.key = _key_name
+        k.get_contents_to_filename(DATA_FILE+'_verify')
+
+        # Create reference object
+        log('Creating ref object locally...')
+        with open('/tmp/tmp_file_verify', 'wb') as tmp_file:
+            with open(SMALL_DATA_FILE, 'rb') as data_file:
+                for i in range(4):
+                    tmp_file.write(data_file.read())
+                    data_file.seek(0)
+
+        # Verify
+        log('Verifying...')
+        assert(hashlib.sha1(open('/tmp/tmp_file_verify', 'rb').read()).hexdigest() == hashlib.sha1(open(DATA_FILE+'_verify').read()).hexdigest())
+
+        # Cleanup
+        os.system('rm ' + DATA_FILE + '_verify')
+        for i in range(4):
+            os.system('rm ' + '/tmp/test_verify' + str(i))
+
+    def test_multi_upload_copy_same_key(self):
         # Upload a key to copy from
         log('Uploading test key...')
         _key = Key(self.b)
@@ -159,6 +206,11 @@ class TestMultiUpload(unittest.TestCase):
         _key_verify.key = 'test_key'
         _key_verify.get_contents_to_filename('/tmp/test_verify')
         assert(hashlib.sha1(open('/tmp/test_verify', 'rb').read()).hexdigest() == hashlib.sha1(open(SMALL_DATA_FILE).read()).hexdigest())
+
+        # Create multipart upload request
+        _key_name = os.path.basename(self.source_path)
+        mp = self.b.initiate_multipart_upload(_key_name)
+        log('MPU initiated!')
 
         log('Copying keys into multipart file...')
         mp.copy_part_from_key(self.b, 'test_key', 1)
@@ -252,8 +304,11 @@ class TestMultiUpload(unittest.TestCase):
         for key in self.b.list():
             log('  Deleting keys...')
             key.delete()
-        log('Keys successfully deleted!')
         log('Attempting to delete bucket...')
+
+        #TODO: This sleep is a workaround for WIN-913
+        time.sleep(7)
+
         self.b.delete()
         log('Bucket successfully deleted!')
         log('Teardown complete!')
