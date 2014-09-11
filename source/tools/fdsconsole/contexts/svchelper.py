f3273f550499feac6efac4e047a6ef1865b53d13
Premkumar <prem@formationds.com>
2014-09-11 15:29:20 -0700

    svc helpers

---
+++ source/tools/fdsconsole/contexts/svchelper.py
@@ -0,0 +1,38 @@
+import sys
+
+from SvcHandle import *
+from fdslib import process
+from fdslib import thrift_json
+from tabulate import tabulate
+
+log = process.setup_logger(file = 'console.log')
+
+class ServiceMap:
+    serviceMap = None
+    @staticmethod
+    def init(ip, port):
+        ServiceMap.serviceMap = SvcMap(ip, port)
+
+    @staticmethod
+    def omConfig():
+        try :
+            client =  ServiceMap.serviceMap.omConfig()
+        except:
+            raise Exception("unable to get a client connection")
+        return client
+
+    @staticmethod
+    def client(*args):
+        try:
+            client = ServiceMap.serviceMap.client(*args)
+        except:
+            raise Exception("unable to get a client connection")
+        return client
+
+    @staticmethod
+    def list(*args):
+        return ServiceMap.serviceMap.list(*args)
+
+    @staticmethod
+    def refresh(*args):
+        return ServiceMap.serviceMap.refresh(*args)
