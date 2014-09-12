c3c8e49cdf58151d17cdda6a9821458da073f7f7
Vy Nguyen <vynguyen@bienhoa>
2014-09-12 09:49:40 -0700

    Check in new files

---
+++ source/orch_mgr/include/om-svc-handler.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2014 by Formation Data Systems, Inc.
+ */
+#ifndef SOURCE_ORCH_MGR_INCLUDE_OM_SVC_HANDLER_H_
+#define SOURCE_ORCH_MGR_INCLUDE_OM_SVC_HANDLER_H_
+
+#include <OmResources.h>
+#include <fdsp/fds_service_types.h>
+#include <net/PlatNetSvcHandler.h>
+
+namespace fds {
+
+class OmSvcHandler : virtual public PlatNetSvcHandler
+{
+  public:
+    OmSvcHandler();
+    virtual ~OmSvcHandler();
+
+    void om_node_svc_info(boost::shared_ptr<fpi::AsyncHdr>    &hdr,
+                          boost::shared_ptr<fpi::NodeSvcInfo> &svc);
+
+    void om_node_info(boost::shared_ptr<fpi::AsyncHdr>    &hdr,
+                      boost::shared_ptr<fpi::NodeInfoMsg> &node);
+
+  protected:
+    OM_NodeDomainMod         *om_mod;
+};
+
+}  // namespace fds
+#endif  // SOURCE_ORCH_MGR_INCLUDE_OM_SVC_HANDLER_H_
