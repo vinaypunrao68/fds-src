b68e1a5c382e5f5eb5ddeee1ae92090e9d198c23
Umesh Agashe <umesh@formationds.com>
2014-09-15 19:04:01 -0700

    Modified SM to use snapshot QoS queues for gets/ puts.

--- source/stor-mgr/smlib/SMSvcHandler.cpp
+++ source/stor-mgr/smlib/SMSvcHandler.cpp
@@ -56,7 +56,10 @@ void SMSvcHandler::getObject(boost::shared_ptr<fpi::AsyncHdr>& asyncHdr,
             asyncHdr,
             std::placeholders::_1, std::placeholders::_2);
 
-    err = objStorMgr->enqueueMsg(read_req->getVolId(), read_req);
+    StorMgrVolume* smVol = objStorMgr->sm_getVolTables()->getVolume(read_req->getVolId());
+    fds_assert(smVol);
+
+    err = objStorMgr->enqueueMsg(smVol->getQueue()->getVolUuid(), read_req);
     if (err != fds::ERR_OK) {
         fds_assert(!"Hit an error in enqueing");
         LOGERROR << "Failed to enqueue to SmIoReadObjectMetadata to StorMgr.  Error: "
@@ -125,7 +128,10 @@ void SMSvcHandler::putObject(boost::shared_ptr<fpi::AsyncHdr>& asyncHdr,
             asyncHdr,
             std::placeholders::_1, std::placeholders::_2);
 
-    err = objStorMgr->enqueueMsg(put_req->getVolId(), put_req);
+    StorMgrVolume* smVol = objStorMgr->sm_getVolTables()->getVolume(put_req->getVolId());
+    fds_assert(smVol);
+
+    err = objStorMgr->enqueueMsg(smVol->getQueue()->getVolUuid(), put_req);
     if (err != fds::ERR_OK) {
         fds_assert(!"Hit an error in enqueing");
         LOGERROR << "Failed to enqueue to SmIoPutObjectReq to StorMgr.  Error: "
@@ -184,7 +190,10 @@ void SMSvcHandler::deleteObject(boost::shared_ptr<fpi::AsyncHdr>& asyncHdr,
         asyncHdr,
         std::placeholders::_1, std::placeholders::_2);
 
-    err = objStorMgr->enqueueMsg(delReq->getVolId(), delReq);
+    StorMgrVolume* smVol = objStorMgr->sm_getVolTables()->getVolume(delReq->getVolId());
+    fds_assert(smVol);
+
+    err = objStorMgr->enqueueMsg(smVol->getQueue()->getVolUuid(), delReq);
     if (err != fds::ERR_OK) {
         fds_assert(!"Hit an error in enqueing");
         LOGERROR << "Failed to enqueue to SmIoDeleteObjectReq to StorMgr.  Error: "
@@ -230,7 +239,10 @@ void SMSvcHandler::addObjectRef(boost::shared_ptr<fpi::AsyncHdr>& asyncHdr,
         asyncHdr,
         std::placeholders::_1, std::placeholders::_2);
 
-    err = objStorMgr->enqueueMsg(addObjRefReq->getSrcVolId(), addObjRefReq);
+    StorMgrVolume* smVol = objStorMgr->sm_getVolTables()->getVolume(addObjRefReq->getSrcVolId());
+    fds_assert(smVol);
+
+    err = objStorMgr->enqueueMsg(smVol->getQueue()->getVolUuid(), addObjRefReq);
     if (err != fds::ERR_OK) {
         fds_assert(!"Hit an error in enqueing");
         LOGERROR << "Failed to enqueue to SmIoAddObjRefReq to StorMgr.  Error: "
