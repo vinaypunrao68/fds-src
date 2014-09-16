26c945bb137c473101134f053fc97d56f55a39ca
Umesh Agashe <umesh@formationds.com>
2014-09-16 16:39:18 -0700

    Fixed runtime error in SM for initial DLT sync up

--- source/stor-mgr/smlib/StorMgr.cpp
+++ source/stor-mgr/smlib/StorMgr.cpp
@@ -817,8 +817,9 @@ void ObjectStorMgr::dltcloseEventHandler(FDSP_DltCloseTypePtr& dlt_close,
     // until we start getting dlt from platform, we need to path dlt
     // width to object store, so that we can correctly map object ids
     // to SM tokens
-    const DLT* curDlt = objStorMgr->omClient->getCurrentDLT();
-    objStorMgr->objectStore->setNumBitsPerToken(curDlt->getNumBitsForToken());
+    // TODO(anna): fix this
+    // const DLT* curDlt = objStorMgr->omClient->getCurrentDLT();
+    // objStorMgr->objectStore->setNumBitsPerToken(curDlt->getNumBitsForToken());
 
     fds_verify(objStorMgr->cached_dlt_close_.second == nullptr);
     objStorMgr->cached_dlt_close_.first = session_uuid;
