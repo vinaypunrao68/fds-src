b4e276a42033e98519dddeb67e3f27fc42c336d6
Vy Nguyen <vynguyen@bienhoa>
2014-09-18 11:06:53 -0700

    Add test library to build stand-alone test app. The test app has FDS platform runtime environment to run with FDS modules. The test app can inherit from the ProbeProcess which provide it the probe front-end.

--- source/unit-test/fds-probe/s3-template/mod-main.cpp
+++ source/unit-test/fds-probe/s3-template/mod-main.cpp
@@ -6,32 +6,30 @@
  *
  * Replace XX with your adapter name.
  */
+#include <unistd.h>
+#include <string>
+#include <iostream>
 #include <template_probe.h>
 #include <util/fds_stat.h>
 #include <fds-probe/s3-probe.h>
 #include <fds_process.h>
+#include <platform/platform-lib.h>
 
 namespace fds {
 
-class ProbeProcess : public FdsProcess
+class TestProcess : public ProbeProcess
 {
   public:
-    virtual ~ProbeProcess() {}
-    ProbeProcess(int argc, char **argv, Module **vec)
-        : FdsProcess(argc, argv, "platform.conf", "fds.plat.", "probe-s3.log", vec) {}
+    TestProcess(int argc, char *argv[],
+                const std::string &log, ProbeMod *probe, Module **vec) :
+        ProbeProcess(argc, argv, log, probe, vec) {}
 
-    void proc_pre_startup() override
+    int run() override
     {
-        gl_probeS3Eng.probe_add_adapter(&gl_XX_ProbeMod);
-        for (int i = 0; i < 0; i ++) {
-            gl_probeS3Eng.probe_add_adapter(gl_XX_ProbeMod.pr_new_instance());
+        while (1) {
+            std::cout << "Your code here" << std::endl;
+            sleep(2);
         }
-    }
-    void proc_pre_service() override {}
-    int run()
-    {
-        FDS_NativeAPI *api = new FDS_NativeAPI(FDS_NativeAPI::FDSN_AWS_S3);
-        gl_probeS3Eng.run_server(api);
         return 0;
     }
 };
@@ -41,13 +39,10 @@ class ProbeProcess : public FdsProcess
 int main(int argc, char **argv)
 {
     fds::Module *probe_vec[] = {
-        &fds::gl_fds_stat,
-        &fds::gl_probeS3Eng,
-
         /* Add your vector and its dependencies here. */
         &fds::gl_XX_ProbeMod,
         NULL
     };
-    fds::ProbeProcess probe(argc, argv, probe_vec);
+    fds::TestProcess probe(argc, argv, "my.log", &fds::gl_XX_ProbeMod, probe_vec);
     return probe.main();
 }
