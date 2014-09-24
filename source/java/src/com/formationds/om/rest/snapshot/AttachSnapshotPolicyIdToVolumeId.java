96f4c398219935ac4670183765992fd671673840
paul-tinius <paul@formationds.com>
2014-09-24 07:09:06 -0600

    Cleaned up Attach/Detach api. Unit Test for Building the RecurrenceRule

--- source/java/src/com/formationds/om/rest/snapshot/AttachSnapshotPolicyIdToVolumeId.java
+++ source/java/src/com/formationds/om/rest/snapshot/AttachSnapshotPolicyIdToVolumeId.java
@@ -6,21 +6,25 @@
 
 package com.formationds.om.rest.snapshot;
 
+import com.formationds.commons.model.helper.VolumeId;
 import com.formationds.web.toolkit.JsonResource;
 import com.formationds.web.toolkit.RequestHandler;
 import com.formationds.web.toolkit.Resource;
 import com.formationds.xdi.ConfigurationApi;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Request;
 import org.json.JSONObject;
 
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Map;
 
 public class AttachSnapshotPolicyIdToVolumeId implements RequestHandler {
   private static final Logger LOG =
     Logger.getLogger(AttachSnapshotPolicyIdToVolumeId.class);
 
-  private static final String REQ_PARAM_VOLUME_ID = "volumeId";
     private static final String REQ_PARAM_POLICY_ID = "policyId";
     private ConfigurationApi config;
 
@@ -32,12 +36,18 @@ public class AttachSnapshotPolicyIdToVolumeId implements RequestHandler {
     public Resource handle(final Request request,
                            final Map<String, String> routeParameters)
             throws Exception {
-      final long volumeId = requiredLong( routeParameters, REQ_PARAM_VOLUME_ID );
       final long policyId = requiredLong( routeParameters, REQ_PARAM_POLICY_ID );
 
-      LOG.trace( "ATTACH:: VOLUME ID: " + volumeId + " POLICY ID: " + policyId );
-      config.attachSnapshotPolicy( volumeId, policyId );
+      try(final Reader reader =
+            new InputStreamReader( request.getInputStream(), "UTF-8")) {
+        Gson gson = new GsonBuilder().create();
+        final VolumeId volumeId = gson.fromJson( reader, VolumeId.class);
 
-        return new JsonResource(new JSONObject().put("status", "OK"));
+        LOG.trace( "ATTACH:: VOLUME ID: " + volumeId.getVolumeId() +
+                   " POLICY ID: " + policyId );
+        config.attachSnapshotPolicy( volumeId.getVolumeId(), policyId );
+      }
+
+      return new JsonResource(new JSONObject().put("status", "OK"));
     }
 }
