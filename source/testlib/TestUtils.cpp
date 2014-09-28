1818c2151fbf75072fcd393453888ac1ee03cb68
Rao Bayyana <rao@formationds.com>
2014-09-28 12:41:40 -0700

    testlib for supporting component level testing.  Added basic classes for data generation, domain deployment, generating thrift packets.  Created helper test fixture class that users can inherit to get single node domain

---
+++ source/testlib/TestUtils.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2014 Formation Data Systems, Inc.
+ */
+#include <platform/platform-lib.h>
+#include <TestUtils.h>
+
+namespace fds {
+/**
+* @brief Returns sm svc uuid that is controlled by a different platform daemon than
+* the current one
+*
+* @param platform
+*
+* @return 
+*/
+fpi::SvcUuid TestUtils::getAnyNonResidentSmSvcuuid(Platform* platform)
+{
+    fpi::SvcUuid svcUuid;
+    fpi::SvcUuid mySmUuid;
+    platform->plf_get_my_sm_svc_uuid(&mySmUuid);
+
+    RsArray smNodes;
+    platform->plf_sm_nodes()->rs_container_snapshot(&smNodes);
+    for (auto n : smNodes) {
+        if (mySmUuid.svc_uuid != (int64_t)(n->rs_get_uuid().uuid_get_val())) {
+            svcUuid.svc_uuid = n->rs_get_uuid().uuid_get_val();
+            break;
+        }
+    }
+    return svcUuid;
+}
+}  // namespace fds
