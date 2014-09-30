164dafa46f512c67f1c49ff6b724ed9c6d03c097
paul-tinius <paul@formationds.com>
2014-09-30 08:45:04 -0600

    Removed CANNED data toggle

--- source/java/src/com/formationds/om/rest/snapshot/CloneSnapshot.java
+++ source/java/src/com/formationds/om/rest/snapshot/CloneSnapshot.java
@@ -5,7 +5,6 @@
 
 package com.formationds.om.rest.snapshot;
 
-import com.formationds.commons.togglz.feature.flag.FdsFeatureToggles;
 import com.formationds.web.toolkit.JsonResource;
 import com.formationds.web.toolkit.RequestHandler;
 import com.formationds.web.toolkit.Resource;
@@ -15,31 +14,27 @@ import org.json.JSONObject;
 
 import java.util.Map;
 
-public class CloneSnapshot implements RequestHandler {
+public class CloneSnapshot
+  implements RequestHandler {
 
-    private static final String REQ_PARAM_VOLUME_ID = "volumeId";
-    private static final String REQ_PARAM_POLICY_ID = "policyId";
-    private static final String REQ_PARAM_CLONED_VOLUME_NAME = "clonedVolumeName";
-    private ConfigurationApi config;
+  private static final String REQ_PARAM_VOLUME_ID = "volumeId";
+  private static final String REQ_PARAM_POLICY_ID = "policyId";
+  private static final String REQ_PARAM_CLONED_VOLUME_NAME = "clonedVolumeName";
+  private ConfigurationApi config;
 
-    public CloneSnapshot(final ConfigurationApi config) {
-        this.config = config;
-    }
+  public CloneSnapshot( final ConfigurationApi config ) {
+    this.config = config;
+  }
 
-    @Override
-    public Resource handle(final Request request,
-                           final Map<String, String> routeParameters) throws Exception {
-        long clonedVolumeId;
+  @Override
+  public Resource handle( final Request request,
+                          final Map<String, String> routeParameters )
+    throws Exception {
+    final long clonedVolumeId = config.cloneVolume(
+      requiredLong( routeParameters, REQ_PARAM_VOLUME_ID ),
+      requiredLong( routeParameters, REQ_PARAM_POLICY_ID ),
+      requiredString( routeParameters, REQ_PARAM_CLONED_VOLUME_NAME ) );
 
-        if (FdsFeatureToggles.USE_CANNED.isActive()) {
-            clonedVolumeId = 1234L;
-        } else {
-            clonedVolumeId = config.cloneVolume(
-                    requiredLong(routeParameters, REQ_PARAM_VOLUME_ID),
-                    requiredLong(routeParameters, REQ_PARAM_POLICY_ID),
-                    requiredString(routeParameters, REQ_PARAM_CLONED_VOLUME_NAME));
-        }
-
-        return new JsonResource(new JSONObject(clonedVolumeId));
-    }
+    return new JsonResource( new JSONObject( clonedVolumeId ) );
+  }
 }
