0de2892c95e2d90b13344f3bd56d201ce9a78537
paul-tinius <paul@formationds.com>
2014-10-03 12:02:46 -0600

    fixed unit tester

--- source/java/test/src/com/formationds/commons/model/SnapshotPolicyTest.java
+++ source/java/test/src/com/formationds/commons/model/SnapshotPolicyTest.java
@@ -1,6 +1,17 @@
-/**
- * Copyright (c) 2014 Formation Data Systems.
- * All rights reserved.
+/*
+ * Copyright (c) 2014, Formation Data Systems, Inc. All Rights Reserved.
+ *
+ * This software is furnished under a license and may be used and copied only
+ * in  accordance  with  the  terms  of such  license and with the inclusion
+ * of the above copyright notice. This software or  any  other copies thereof
+ * may not be provided or otherwise made available to any other person.
+ * No title to and ownership of  the  software  is  hereby transferred.
+ *
+ * The information in this software is subject to change without  notice
+ * and  should  not be  construed  as  a commitment by Formation Data Systems.
+ *
+ * Formation Data Systems assumes no responsibility for the use or  reliability
+ * of its software on equipment which is not supplied by Formation Date Systems.
  */
 
 package com.formationds.commons.model;
@@ -31,7 +42,7 @@ public class SnapshotPolicyTest {
       Assert.assertEquals("Name don't match", policy.getName(),
                           "" + "snapshot policy name 1");
       Assert.assertEquals( policy.getRecurrenceRule().toString(),
-                           "FREQ=DAILY;COUNT=10");
+                           "[ FREQ=DAILY;COUNT=10; ]");
       Assert.assertEquals("Retention don't match", policy.getRetention(),
                           retention);
 
