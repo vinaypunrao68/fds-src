1a0eed4838e0844e161649a54647e5479ee0e6fd
Brian Madden <brian@formationds.com>
2014-10-08 15:24:21 -0700

    Separated checker functionality from driver functionality. Added bytes reclaimable GC check. [WIN-1104]

--- source/checker/smchk.cpp
+++ source/checker/smchk.cpp
@@ -14,72 +14,13 @@
 
 #include "smchk.h"
 
-// WE LOVE GLOBALS
-fds::SmDiskMap::ptr smDiskMap = fds::SmDiskMap::ptr(
-        new fds::SmDiskMap("SMchk"));
-
-fds::ObjectDataStore::unique_ptr smObjStore = fds::ObjectDataStore::unique_ptr(
-        new fds::ObjectDataStore("SMchk"));
-
 namespace fds {
-    SMChk::SMChk(int argc, char * argv[], const std::string &config,
-            const std::string &basePath, Module * vec[]):
-            FdsProcess(argc, argv, config, basePath, "smchk.log", vec),
-            error_count(0), objs_count(0) {
-        // Get from command line args
-        namespace po = boost::program_options;
-        po::options_description desc("Run SM checker");
-        desc.add_options()
-                ("help,h", "Print this help message")
-                ("sm-count,c",
-                        po::value<int>(&sm_count)->default_value(1),
-                        "Number of active SMs.")
-                ("full-check,f",
-                        po::bool_switch()->default_value(false),
-                        "Run full consistency check")
-                ("list-tokens",
-                        po::bool_switch()->default_value(false),
-                        "Print all tokens by path.")
-                ("list-paths",
-                        po::bool_switch()->default_value(false),
-                        "Print paths by token.")
-                ("list-metadata,p",
-                        po::bool_switch()->default_value(false),
-                        "Print all metadata.");
-
-        po::variables_map vm;
-        po::parsed_options parsed =
-                po::command_line_parser(argc, argv).options(desc).allow_unregistered().run();
-        po::store(parsed, vm);
-        po::notify(vm);
-
-        if (vm.count("help")) {
-            std::cout << desc << "\n";
-            exit(0);
-        }
-
-        if (vm["full-check"].as<bool>()) {
-            cmd = RunFunc::FULL_CHECK;
-        } else if (vm["list-tokens"].as<bool>()) {
-            cmd = RunFunc::PRINT_TOK_BY_PATH;
-        } else if (vm["list-paths"].as<bool>()) {
-            cmd = RunFunc::PRINT_PATH_BY_TOK;
-        } else if (vm["list-metadata"].as<bool>()) {
-            cmd = RunFunc::PRINT_MD;
-        } else {
-            cmd = RunFunc::FULL_CHECK;
-        }
-
-        sm_count = vm["sm-count"].as<int>();
-
-        // ------------------------------------------------ //
-
-
-        smMdDb = fds::ObjectMetadataDb::unique_ptr(
-                new fds::ObjectMetadataDb());
-    }
-
-    int SMChk::run() {
+    SMChk::SMChk(int sm_count, SmDiskMap::ptr smDiskMap,
+            ObjectDataStore::ptr smObjStore, ObjectMetadataDb::ptr smMdDb):
+            sm_count(sm_count),
+            smDiskMap(smDiskMap),
+            smObjStore(smObjStore),
+            smMdDb(smMdDb) {
         fds_uint32_t cols = (sm_count < 4) ? sm_count : 4;
         DLT* dlt = new DLT(8, cols, 1, true);
         SmUtUtils::populateDlt(dlt, sm_count);
@@ -90,32 +31,10 @@ namespace fds {
         smObjStore->openDataStore(smDiskMap);
 
         // Meta db
-        // smMdDb->setNumBitsPerToken(16);
         smMdDb->openMetadataDb(smDiskMap);
 
         // we don't need dlt anymore
         delete dlt;
-
-        // ------------------------------------------------ //
-
-        switch (cmd) {
-            case RunFunc::FULL_CHECK:
-                full_consistency_check();
-                break;
-            case RunFunc::PRINT_MD:
-                list_metadata();
-                break;
-            case RunFunc::PRINT_PATH_BY_TOK:
-                list_path_by_token();
-                break;
-            case RunFunc::PRINT_TOK_BY_PATH:
-                list_token_by_path();
-                break;
-            default:
-                full_consistency_check();
-                break;
-        }
-        return 0;
     }
 
     SmTokenSet SMChk::getSmTokens() {
@@ -179,7 +98,41 @@ namespace fds {
         }
     }
 
+    int SMChk::bytes_reclaimable() {
+        SmTokenSet all_toks = getSmTokens();
+        int bytes = 0;
+
+        boost::shared_ptr<ObjMetaData> omd;
+        leveldb::ReadOptions options;
+
+        for (auto token : all_toks) {
+            leveldb::DB *ldb;
+            leveldb::Iterator *it;
+
+            GLOGNORMAL << "Reading metadata db for SM token " << token << "\n";
+            smMdDb->snapshot(token, ldb, options);
+            it = ldb->NewIterator(options);
+            for (it->SeekToFirst(); it->Valid(); it->Next()) {
+                omd = boost::shared_ptr<ObjMetaData>(new ObjMetaData());
+                omd->deserializeFrom(it->value());
+
+                if (omd->getRefCnt() == 0) {
+                    bytes += omd->getObjSize();
+                }
+                // omd will auto delete when it goes out of scope
+            }
+            ldb->ReleaseSnapshot(options.snapshot);
+            // Delete ldb and it
+            delete it;
+            delete ldb;
+        }
+        std::cout << "Found " << bytes << " bytes reclaimable.\n";
+        return bytes;
+    }
+
     bool SMChk::full_consistency_check() {
+        int error_count = 0;
+        int objs_count = 0;
         // Token stuffs
         SmTokenSet all_toks = getSmTokens();
 
@@ -240,18 +193,164 @@ namespace fds {
         std::cout << "SUCCESS! " << objs_count << " objects checked; no errors found.\n";
         return true;
     }
+    // ---------------------- MetadataIterator -------------------------------- //
+#if 0
+    SMChk::MetadataIterator::MetadataIterator(SMChk * instance) {
+        GLOGDEBUG << "MetadataIterator init";
+
+        smchk = instance;
+        ldb = nullptr;
+        ldb_it = nullptr;
+        all_toks = smchk->getSmTokens();
+    }
+
+    void SMChk::MetadataIterator::start() {
+        token_it = all_toks.begin();
+        smchk->smMdDb->snapshot(*token_it, ldb, options);
+        ldb_it = ldb->NewIterator(options);
+    }
+
+    bool SMChk::MetadataIterator::end() {
+        if (token_it == all_toks.end()) {
+            return true;
+        }
+        return false;
+    }
+
+    void SMChk::MetadataIterator::next() {
+        if (!ldb_it->Valid()) {
+            while (ldb_it == nullptr || !(ldb_it->Valid())) {
+                // No leveldb open or iterator invalid
+                if (ldb != nullptr) {
+                    ldb->ReleaseSnapshot(options.snapshot);
+                }
+                delete ldb_it;
+                delete ldb;
+
+                GLOGDEBUG << "Moving to next token in iterator.";
+                // Move to the next token
+                ++token_it;
+                if (token_it == all_toks.end()) {return;}
+                // Open levelDB, create new levelDB iterator
+                smchk->smMdDb->snapshot(*token_it, ldb, options);
+                ldb_it = ldb->NewIterator(options);
+            }
+        } else {
+            ldb_it->Next();
+        }
+    }
+
+    boost::shared_ptr<ObjMetaData> SMChk::MetadataIterator::value() {
+        if (!end()) {
+            // We should have a valid levelDB / ldb_it
+            omd = boost::shared_ptr<ObjMetaData>(new ObjMetaData());
+            omd->deserializeFrom(ldb_it->value());
+        }
+        return omd;
+    }
+#endif
+
+    SMChkDriver::SMChkDriver(int argc, char *argv[],
+            const std::string &config,
+            const std::string &basePath, Module *vec[],
+            fds::SmDiskMap::ptr smDiskMap,
+            fds::ObjectDataStore::ptr smObjStore):
+            FdsProcess(argc, argv, config, basePath, "smchk.log", vec),
+            checker(nullptr),
+            smDiskMap(smDiskMap),
+            smObjStore(smObjStore) {
+        // Create the metadata db
+        smMdDb = fds::ObjectMetadataDb::ptr(
+                new fds::ObjectMetadataDb());
+        // Get from command line args
+        namespace po = boost::program_options;
+        po::options_description desc("Run SM checker");
+        desc.add_options()
+                ("help,h", "Print this help message")
+                ("sm-count,c",
+                        po::value<int>(&sm_count)->default_value(1),
+                        "Number of active SMs.")
+                ("full-check,f",
+                        po::bool_switch()->default_value(false),
+                        "Run full consistency check")
+                ("list-tokens",
+                        po::bool_switch()->default_value(false),
+                        "Print all tokens by path.")
+                ("list-paths",
+                        po::bool_switch()->default_value(false),
+                        "Print paths by token.")
+                ("list-metadata,p",
+                        po::bool_switch()->default_value(false),
+                        "Print all metadata.");
+
+        po::variables_map vm;
+        po::parsed_options parsed =
+                po::command_line_parser(argc, argv).options(desc).allow_unregistered().run();
+        po::store(parsed, vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            std::cout << desc << "\n";
+            exit(0);
+        }
+
+        if (vm["full-check"].as<bool>()) {
+            cmd = RunFunc::FULL_CHECK;
+        } else if (vm["list-tokens"].as<bool>()) {
+            cmd = RunFunc::PRINT_TOK_BY_PATH;
+        } else if (vm["list-paths"].as<bool>()) {
+            cmd = RunFunc::PRINT_PATH_BY_TOK;
+        } else if (vm["list-metadata"].as<bool>()) {
+            cmd = RunFunc::PRINT_MD;
+        } else {
+            cmd = RunFunc::FULL_CHECK;
+        }
+        sm_count = vm["sm-count"].as<int>();
+    }
+
+    int SMChkDriver::run() {
+        checker = new SMChk(sm_count, smDiskMap, smObjStore, smMdDb);
+        switch (cmd) {
+            case RunFunc::FULL_CHECK:
+                checker->full_consistency_check();
+                break;
+            case RunFunc::PRINT_MD:
+                checker->list_metadata();
+                break;
+            case RunFunc::PRINT_PATH_BY_TOK:
+                checker->list_path_by_token();
+                break;
+            case RunFunc::PRINT_TOK_BY_PATH:
+                checker->list_token_by_path();
+                break;
+            case RunFunc::CALC_BYTES_RECLAIMABLE:
+                checker->bytes_reclaimable();
+            default:
+                checker->full_consistency_check();
+                break;
+        }
+        return 0;
+    }
+
 }  // namespace fds
 
 int
 main(int argc, char** argv) {
+    fds::SmDiskMap::ptr smDiskMap = fds::SmDiskMap::ptr(
+            new fds::SmDiskMap("SMchk"));
+
+    fds::ObjectDataStore::ptr smObjStore = fds::ObjectDataStore::ptr(
+            new fds::ObjectDataStore("SMchk"));
+
     fds::Module *vec[] = {
             &diskio::gl_dataIOMod,
             smDiskMap.get(),
             smObjStore.get(),
             nullptr
     };
-    fds::SMChk smChk(argc, argv, "sm_ut.conf",
-            "fds.diskmap_ut.", vec);
+
+    fds::SMChkDriver smChk(argc, argv, "sm_ut.conf", "fds.diskmap_ut.",
+            vec, smDiskMap, smObjStore);
     smChk.main();
     return 0;
 }
--- source/checker/smchk.h
+++ source/checker/smchk.h
@@ -10,41 +10,82 @@ class ObjectMetadataDb;
 
 namespace fds {
 
+    typedef boost::shared_ptr<ObjMetaData> MdPtr;
+
     enum RunFunc {
         FULL_CHECK = 0,
         PRINT_MD,
         PRINT_TOK_BY_PATH,
-        PRINT_PATH_BY_TOK
+        PRINT_PATH_BY_TOK,
+        CALC_BYTES_RECLAIMABLE
     };
 
-    class SMChk : public FdsProcess {
-    friend class ObjMeta;
+    class SMChk {
+    // friend class MetadataIterator;
     public:
-        SMChk(int argc, char * argv[], const std::string &config,
-                const std::string &basePath, Module * vec[]);
-        ~SMChk() {
-        }
+        SMChk(int sm_count, SmDiskMap::ptr smDiskMap,
+                ObjectDataStore::ptr smObjStore,
+                ObjectMetadataDb::ptr smMdDb);
+        ~SMChk() {}
 
-        int run() override;
         void list_path_by_token();
         void list_token_by_path();
         void list_metadata();
         bool full_consistency_check();
+        int  bytes_reclaimable();
         // bool consistency_check(ObjectId obj_id);  // test a single object
 
-    private:
+    protected:
         // Data
         int sm_count;
-        int error_count;
-        int objs_count;
         RunFunc cmd;
-        // fds::SmDiskMap::ptr smDiskMap;
-        // fds::ObjectDataStore::unique_ptr smObjStore;
-        fds::ObjectMetadataDb::unique_ptr smMdDb;
-
+        fds::SmDiskMap::ptr smDiskMap;
+        fds::ObjectDataStore::ptr smObjStore;
+        fds::ObjectMetadataDb::ptr smMdDb;
         // Methods
         SmTokenSet getSmTokens();
+
+#if 0
+        class MetadataIterator {
+        public:
+            MetadataIterator(SMChk * instance);
+            ~MetadataIterator() {}
+            bool end();
+            void start();
+            MdPtr value();
+            void next();
+        private:
+            SMChk * smchk;
+
+            SmTokenSet all_toks;
+            SmTokenSet::iterator token_it;
+
+            leveldb::ReadOptions options;
+            leveldb::DB *ldb;
+            leveldb::Iterator *ldb_it;
+
+            MdPtr omd;
+        };
+#endif
     };
 
+    class SMChkDriver : public FdsProcess {
+    public:
+        SMChkDriver(int argc, char * argv[], const std::string &config,
+                const std::string &basePath, Module * vec[],
+                fds::SmDiskMap::ptr smDiskMap,
+                fds::ObjectDataStore::ptr smObjStore);
+        ~SMChkDriver() {}
+
+        int run() override;
+
+    private:
+        SMChk *checker;
+        RunFunc cmd;
+        fds::SmDiskMap::ptr smDiskMap;
+        fds::ObjectDataStore::ptr smObjStore;
+        fds::ObjectMetadataDb::ptr smMdDb;
+        int sm_count;
+    };
 }  // namespace fds
 #endif  // SOURCE_PLATFORM_INCLUDE_DISK_H_
