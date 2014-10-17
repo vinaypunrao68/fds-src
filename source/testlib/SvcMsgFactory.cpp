22a30239c214d917d4e51b30df18f7beb5d42f04
Sanjay <sanjay@formationds.com>
2014-10-17 16:01:53 -0700

    intial work  standalone DM  test

--- source/testlib/SvcMsgFactory.cpp
+++ source/testlib/SvcMsgFactory.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright 2014 Formation Data Systems, Inc.
  */
+#include <string>
 #include <fdsp_utils.h>
 #include <ObjectId.h>
 #include <apis/ConfigurationService.h>
@@ -23,12 +24,124 @@ SvcMsgFactory::newPutObjectMsg(const uint64_t& volId, DataGenIfPtr dataGen)
     return putObjMsg;
 }
 
+
+void
+UpdateBlobInfo(FDS_ProtocolInterface::UpdateCatalogMsgPtr  updateCatMsg,
+                          DataGenIfPtr dataGen, size_t blobSize)
+{
+        uint64_t blobOffset = 0;
+        while (dataGen->hasNext()) {
+             auto data = dataGen->nextItem();
+             auto objId = ObjIdGen::genObjectId(data->c_str(), data->length());
+             FDS_ProtocolInterface::FDSP_BlobObjectInfo updBlobInfo;
+             updBlobInfo.offset   = blobOffset;
+             updBlobInfo.size     = blobSize;
+             updBlobInfo.blob_end = dataGen->hasNext() == false;
+             updBlobInfo.data_obj_id.digest =
+                   std::string((const char *)objId.GetId(), (size_t)objId.GetLen());
+             updateCatMsg->obj_list.push_back(updBlobInfo);
+             blobOffset += data->length();
+       }
+}
+
 apis::VolumeSettings SvcMsgFactory::defaultS3VolSettings()
 {
     apis::VolumeSettings vs;
     vs.maxObjectSizeInBytes = 2 << 20;
     vs.volumeType = apis::OBJECT;
     return vs;
+}
+
+fpi::QueryCatalogMsgPtr
+SvcMsgFactory::newQueryCatalogMsg(const uint64_t& volId,
+             const std::string blobName, const uint64_t& blobOffset )
+{
+    fpi::QueryCatalogMsgPtr queryCatMsg(new fpi::QueryCatalogMsg);
+    queryCatMsg->volume_id = volId;
+    queryCatMsg->blob_name = blobName;
+    queryCatMsg->blob_offset = blobOffset;
+    queryCatMsg->blob_version = blob_version_invalid;
+    return queryCatMsg;
+}
+
+fpi::UpdateCatalogMsgPtr
+SvcMsgFactory::newUpdateCatalogMsg(const uint64_t& volId, const std::string blobName)
+{
+    fpi::UpdateCatalogMsgPtr updCatMsg(new fpi::UpdateCatalogMsg);
+    updCatMsg->volume_id = volId;
+    updCatMsg->blob_name = blobName;
+    updCatMsg->blob_version = blob_version_invalid;
+    return updCatMsg;
+}
+
+fpi::UpdateCatalogOnceMsgPtr
+SvcMsgFactory::newUpdateCatalogOnceMsg(const uint64_t& volId, const std::string blobName)
+{
+    fpi::UpdateCatalogOnceMsgPtr updCatOnceMsg(new fpi::UpdateCatalogOnceMsg);
+    updCatOnceMsg->volume_id = volId;
+    updCatOnceMsg->blob_name = blobName;
+    updCatOnceMsg->blob_version = blob_version_invalid;
+    return updCatOnceMsg;
+}
+
+fpi::StartBlobTxMsgPtr
+SvcMsgFactory::newStartBlobTxMsg(const uint64_t& volId, const std::string blobName)
+{
+    fpi::StartBlobTxMsgPtr  startBlbTx(new fpi::StartBlobTxMsg);
+    startBlbTx->volume_id = volId;
+    startBlbTx->blob_name = blobName;
+    startBlbTx->blob_version = blob_version_invalid;
+    return startBlbTx;
+}
+
+fpi::CommitBlobTxMsgPtr
+SvcMsgFactory::newCommitBlobTxMsg(const uint64_t& volId, const std::string blobName)
+{
+    fpi::CommitBlobTxMsgPtr  commitBlbTx(new fpi::CommitBlobTxMsg);
+    commitBlbTx->volume_id = volId;
+    commitBlbTx->blob_name = blobName;
+    commitBlbTx->blob_version = blob_version_invalid;
+    return commitBlbTx;
+}
+
+fpi::AbortBlobTxMsgPtr
+SvcMsgFactory::newAbortBlobTxMsg(const uint64_t& volId, const std::string blobName)
+{
+    fpi::AbortBlobTxMsgPtr  abortBlbTx(new fpi::AbortBlobTxMsg);
+    abortBlbTx->volume_id = volId;
+    abortBlbTx->blob_name = blobName;
+    abortBlbTx->blob_version = blob_version_invalid;
+    return abortBlbTx;
+}
+
+fpi::DeleteCatalogObjectMsgPtr
+SvcMsgFactory::newDeleteCatalogObjectMsg(const uint64_t& volId, const std::string blobName)
+{
+    fpi::DeleteCatalogObjectMsgPtr  deleteBlbTx(new fpi::DeleteCatalogObjectMsg);
+    deleteBlbTx->volume_id = volId;
+    deleteBlbTx->blob_name = blobName;
+    deleteBlbTx->blob_version = blob_version_invalid;
+    return deleteBlbTx;
+}
+
+fpi::SetBlobMetaDataMsgPtr
+SvcMsgFactory::newSetBlobMetaDataMsg(const uint64_t& volId, const std::string blobName)
+{
+    fpi::SetBlobMetaDataMsgPtr  setBlobMeta(new fpi::SetBlobMetaDataMsg);
+    setBlobMeta->volume_id = volId;
+    setBlobMeta->blob_name = blobName;
+    setBlobMeta->blob_version = blob_version_invalid;
+    return setBlobMeta;
+}
+
+fpi::GetBlobMetaDataMsgPtr
+SvcMsgFactory::newGetBlobMetaDataMsg(const uint64_t& volId, const std::string blobName)
+{
+    fpi::GetBlobMetaDataMsgPtr  getBlobMeta(new fpi::GetBlobMetaDataMsg);
+    getBlobMeta->volume_id = volId;
+    getBlobMeta->blob_name = blobName;
+    getBlobMeta->blob_version = blob_version_invalid;
+    return getBlobMeta;
 }
 
 }  // namespace fds
--- source/testlib/SvcMsgFactory.h
+++ source/testlib/SvcMsgFactory.h
@@ -9,7 +9,37 @@
 /* Forward declarations */
 namespace FDS_ProtocolInterface {
 class PutObjectMsg;
+class QueryCatalogMsg;
+class UpdateCatalogMsg;
+class UpdateCatalogOnceMsg;
+class StartBlobTxMsg;
+class AbortBlobTxMsg;
+class DeleteCatalogObjectMsg;
+class GetVolumeBlobListMsg;
+class SetBlobMetaDataMsg;
+class GetBlobMetaDataMsg;
+class GetDmStatsMsg;
+class GetVolumeMetaDataMsg;
+class DeleteBlobMsg;
+class CommitBlobTxMsg;
+class AbortBlobTxMsg;
+class FDSP_BlobObjectInfo;
 typedef boost::shared_ptr<PutObjectMsg> PutObjectMsgPtr;
+typedef boost::shared_ptr<QueryCatalogMsg> QueryCatalogMsgPtr;
+typedef boost::shared_ptr<UpdateCatalogMsg> UpdateCatalogMsgPtr;
+typedef boost::shared_ptr<UpdateCatalogOnceMsg> UpdateCatalogOnceMsgPtr;
+typedef boost::shared_ptr<StartBlobTxMsg> StartBlobTxMsgPtr;
+typedef boost::shared_ptr<AbortBlobTxMsg> AbortBlobTxMsgPtr;
+typedef boost::shared_ptr<DeleteCatalogObjectMsg> DeleteCatalogObjectMsgPtr;
+typedef boost::shared_ptr<GetVolumeBlobListMsg> GetVolumeBlobListMsgPtr;
+typedef boost::shared_ptr<SetBlobMetaDataMsg> SetBlobMetaDataMsgPtr;
+typedef boost::shared_ptr<GetBlobMetaDataMsg> GetBlobMetaDataMsgPtr;
+typedef boost::shared_ptr<GetDmStatsMsg> GetDmStatsMsgPtr;
+typedef boost::shared_ptr<GetVolumeMetaDataMsg> GetVolumeMetaDataMsgPtr;
+typedef boost::shared_ptr<DeleteBlobMsg> DeleteBlobMsgPtr;
+typedef boost::shared_ptr<CommitBlobTxMsg> CommitBlobTxMsgPtr;
+typedef boost::shared_ptr<AbortBlobTxMsg> AbortBlobTxMsgPtr;
+typedef boost::shared_ptr<FDSP_BlobObjectInfo> FDSP_BlobObjectInfoPtr;
 }
 
 namespace fds {
@@ -17,6 +47,8 @@ namespace fds {
 class DataGenIf;
 typedef boost::shared_ptr<DataGenIf> DataGenIfPtr;
 
+void UpdateBlobInfo(FDS_ProtocolInterface::UpdateCatalogMsgPtr  updateCatMsg,
+                          DataGenIfPtr dataGen, size_t blobSize);
 namespace apis {
 class VolumeSettings;
 }
@@ -27,7 +59,26 @@ class VolumeSettings;
 struct SvcMsgFactory {
     static FDS_ProtocolInterface::PutObjectMsgPtr
         newPutObjectMsg(const uint64_t& volId, DataGenIfPtr dataGen);
-
+    static FDS_ProtocolInterface::QueryCatalogMsgPtr
+        newQueryCatalogMsg(const uint64_t& volId, DataGenIfPtr dataGen);
+    static FDS_ProtocolInterface::QueryCatalogMsgPtr
+        newQueryCatalogMsg(const uint64_t& volId, const std::string blobName, const uint64_t& blobOffset );
+    static FDS_ProtocolInterface::UpdateCatalogMsgPtr
+        newUpdateCatalogMsg(const uint64_t& volId, const std::string blobName);
+    static FDS_ProtocolInterface::UpdateCatalogOnceMsgPtr
+        newUpdateCatalogOnceMsg(const uint64_t& volId, const std::string blobName);
+    static FDS_ProtocolInterface::StartBlobTxMsgPtr
+        newStartBlobTxMsg(const uint64_t& volId, const std::string blobName);
+    static FDS_ProtocolInterface::CommitBlobTxMsgPtr
+        newCommitBlobTxMsg(const uint64_t& volId, const std::string blobName);
+    static FDS_ProtocolInterface::AbortBlobTxMsgPtr
+        newAbortBlobTxMsg(const uint64_t& volId, const std::string blobName);
+    static FDS_ProtocolInterface::DeleteCatalogObjectMsgPtr
+        newDeleteCatalogObjectMsg(const uint64_t& volId, const std::string blobName);
+    static FDS_ProtocolInterface::SetBlobMetaDataMsgPtr
+        newSetBlobMetaDataMsg(const uint64_t& volId, const std::string blobName);
+    static FDS_ProtocolInterface::GetBlobMetaDataMsgPtr
+        newGetBlobMetaDataMsg(const uint64_t& volId, const std::string blobName);
     static apis::VolumeSettings defaultS3VolSettings();
 };
 
