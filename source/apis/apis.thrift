2e38c6ab501c6e948c9b9a5bae4d420b047dd824
Fabrice Medio <fabrice@formationds.com>
2014-10-20 13:27:24 -0700

    [WIN-1169] Async AmService definitions

--- source/apis/apis.thrift
+++ source/apis/apis.thrift
@@ -92,6 +92,67 @@ service AmService {
              throws (1: ApiException e),
 }
 
+struct RequestId {
+       1: required string id;
+}
+
+service AsyncAmServiceRequest {
+	oneway void statBlob(1:RequestId requestId, 2:string domainName, 
+	       3:string volumeName, 4:string blobName),
+
+        oneway void startBlobTx(1:RequestId requestId, 2:string domainName, 
+	       3:string volumeName, 4:string blobName, 5:i32 blobMode),
+
+	oneway void commitBlobTx(1:RequestId requestId, 2:string domainName, 
+	       3:string volumeName, 4:string blobName, 5:TxDescriptor txDesc),
+
+	oneway void abortBlobTx(1:RequestId requestId, 2:string domainName, 
+	       3:string volumeName, 4:string blobName, 5:TxDescriptor txDesc),
+
+        oneway void getBlob(1:RequestId requestId, 2:string domainName, 
+	       3:string volumeName, 4:string blobName, 5:i32 length, 6:ObjectOffset offset),
+
+        oneway void updateMetadata(1:RequestId requestId, 2:string domainName, 
+	       3:string volumeName, 4:string blobName, 5:TxDescriptor txDesc, 6:map<string, string> metadata),
+
+        oneway void updateBlob(1:RequestId requestId, 2:string domainName, 
+	       3:string volumeName, 4:string blobName, 5:TxDescriptor txDesc, 6:binary bytes, 
+	       7:i32 length, 8:ObjectOffset objectOffset, 9:bool isLast),
+
+        oneway void updateBlobOnce(1:RequestId requestId, 2:string domainName, 
+	       3:string volumeName, 4:string blobName, 5:i32 blobMode, 6:binary bytes, 
+	       7:i32 length, 8:ObjectOffset objectOffset, 9:map<string, string> metadata),
+
+        oneway void deleteBlob(1:RequestId requestId, 2:string domainName, 
+	       3:string volumeName, 4:string blobName),
+
+        oneway void volumeStatus(1:RequestId requestId, 2:string domainName, 3:string volumeName)
+}
+
+service AsyncAmServiceResponse {
+        oneway void statBlobResponse(1:RequestId requestId, 2:BlobDescriptor response),
+
+        oneway void startBlobTxResponse(1:RequestId requestId, 2:TxDescriptor response),
+
+        oneway void commitBlobTxResponse(1:RequestId requestId),
+
+	oneway void abortBlobTxResponse(1:RequestId requestId),
+
+        oneway void getBlobResponse(1:RequestId requestId, 2:binary response),
+
+        oneway void updateMetadataResponse(1:RequestId requestId),
+
+        oneway void updateBlobResponse(1:RequestId requestId),
+
+        oneway void updateBlobOnceResponse(1:RequestId requestId),
+
+        oneway void deleteBlobResponse(1:RequestId requestId),
+
+        oneway void volumeStatus(1:RequestId requestId, 2:VolumeStatus volumeStatus)
+}
+
+
+
 // Added for multi-tenancy
 struct User {
     1: i64 id,
