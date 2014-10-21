c29222b66e7eea89c54519a31ef61e8819f106a0
Nate Bever <nate@formationds.com>
2014-10-21 17:07:13 -0600

    adding the ability to have a confirm dialog before a snapshot is created

--- source/java/admin-webapp/src/scripts/directives/widgets/fds-modal/fds-modal.js
+++ source/java/admin-webapp/src/scripts/directives/widgets/fds-modal/fds-modal.js
@@ -5,20 +5,30 @@ angular.module( 'display-widgets' ).directive( 'fdsModal', function(){
         replace: true,
         transclude: false,
         templateUrl: 'scripts/directives/widgets/fds-modal/fds-modal.html',
-        scope: { showEvent: '@' },
+        scope: { showEvent: '@', confirmEvent: '@' },
         // event should be:  { text: '', type: 'INFO|WARN|ERROR|QUESTION', ok: Func, cancel: Func }
         controller: function( $scope, $rootScope, $filter ){
             
             $scope.$event = {};
             $scope.width = 0;
             $scope.show = false;
+            $scope.showCancel = false;
             
-            $scope.okPressed = function(){
+            $scope.cancel = function(){
                 $scope.$event = {};
                 $scope.show = false;
             };
             
-            $rootScope.$on( $scope.showEvent, function( eventName, $event ){
+            $scope.okPressed = function(){
+                
+                if ( angular.isFunction( $scope.$event.confirm ) ){
+                    $scope.$event.confirm( true );
+                }
+                
+                $scope.cancel();
+            };
+            
+            var showDialog = function( eventName, $event ){
                 
                 $scope.$event = $event;
                 
@@ -43,6 +53,9 @@ angular.module( 'display-widgets' ).directive( 'fdsModal', function(){
                         case 'ERROR':
                             $event.title = $filter( 'translate' )( 'common.l_error' );
                             break;
+                        case 'CONFIRM':
+                            $event.title = $filter( 'translate' )( 'common.l_confirm' );
+                            break;
                         default:
                             $event.title = $filter( 'translate' )( 'common.l_info' );
                             break;
@@ -51,6 +64,24 @@ angular.module( 'display-widgets' ).directive( 'fdsModal', function(){
                 }
                 
                 $scope.show = true;
+            };
+            
+            $rootScope.$on( $scope.showEvent,  function( eventName, $event ){
+                
+                $scope.okText = $filter( 'translate' )('common.l_ok');
+                $scope.cancelText = $filter( 'translate' )( 'common.l_cancel' );
+                $scope.showCancel = false;
+                
+                showDialog( eventName, $event );
+            });
+            
+            $rootScope.$on( $scope.confirmEvent, function( eventName, $event ){
+                
+                $scope.okText = $filter( 'translate' )( 'common.l_yes' );
+                $scope.cancelText = $filter( 'translate' )( 'common.l_no' );
+                $scope.showCancel = true;
+                
+                showDialog( eventName, $event );
             });
         }
     };
