ce1282f9f657021a41e80c4f15a83a1df8800ddf
Cody Boggs <cody@formationds.com>
2014-10-24 15:13:53 +0000

    got a helper script in place to perform better locking around devsetup. Work is still managed by Ansible, but the helper ensures that we don't end up with multiple devsetup playbooks converging the node (which will fail miserably every time)

---
+++ ansible/devsetup_ansible.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+### README ###
+#   This is only a helper script to quell parallel executions of the devsetup Ansible playbook
+#   If you need to make changes to devsetup, this is NOT LIKELY to be the file you need to edit
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
+sem_dir="${script_dir}/${0##*/}.semaphore"
+sem_expiration_seconds=1800
+
+if [ -d ${sem_dir} ]; then
+	dir_mtime=$(stat -c %Y ${sem_dir})
+	current_time=$(date +"%s")
+
+	if [ $((current_time - dir_mtime)) -gt ${sem_expiration_seconds} ]; then
+		echo "old semaphore expired - removing"
+		rmdir ${sem_dir}
+	fi
+fi
+
+echo ${sem_dir}
+mkdir "${sem_dir}" 2>/dev/null
+
+blah=$?
+echo "mkdir result: ${blah}"
+
+case ${blah} in
+	0)
+		echo "running devsetup"
+		sleep 30
+		rmdir ${sem_dir}
+		touch ${script_dir}/.devsetup-is-up-to-date
+		;;
+	*)
+	while [[ -d ${sem_dir} ]]
+	do
+		sleep 1
+	done
+	echo "devsetup already running, slept until complete - continuing now"
+	;;
+esac
