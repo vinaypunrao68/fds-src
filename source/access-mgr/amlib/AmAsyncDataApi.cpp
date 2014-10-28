de53ce8caaccabbeaea4db58060b578fa6a5cb47
Andrew Leung <andrew@formationds.com>
2014-10-28 07:49:06 -0700

    WIN-1220 Remove old sync code from new async handler.

--- source/access-mgr/amlib/AmAsyncDataApi.cpp
+++ source/access-mgr/amlib/AmAsyncDataApi.cpp
@@ -18,34 +18,6 @@ AmAsyncDataApi::AmAsyncDataApi() {
     if (true == testUturnAll) {
         LOGDEBUG << "Enabling uturn testing for all AM service APIs";
     }
-    testUturnStartTx = conf.get_abs<bool>("fds.am.testing.uturn_amserv_starttx");
-    if (true == testUturnStartTx) {
-        LOGDEBUG << "Enabling uturn testing for AM service start tx API";
-    }
-    testUturnUpdateBlob = conf.get_abs<bool>("fds.am.testing.uturn_amserv_updateblob");
-    if (true == testUturnUpdateBlob) {
-        LOGDEBUG << "Enabling uturn testing for AM service update blob API";
-    }
-    testUturnUpdateMeta = conf.get_abs<bool>("fds.am.testing.uturn_amserv_updatemeta");
-    if (true == testUturnUpdateMeta) {
-        LOGDEBUG << "Enabling uturn testing for AM service update metadata API";
-    }
-    testUturnCommitTx = conf.get_abs<bool>("fds.am.testing.uturn_amserv_committx");
-    if (true == testUturnCommitTx) {
-        LOGDEBUG << "Enabling uturn testing for AM service commit tx API";
-    }
-    testUturnAbortTx = conf.get_abs<bool>("fds.am.testing.uturn_amserv_aborttx");
-    if (true == testUturnAbortTx) {
-        LOGDEBUG << "Enabling uturn testing for AM service abort tx API";
-    }
-    testUturnStatBlob = conf.get_abs<bool>("fds.am.testing.uturn_amserv_statblob");
-    if (true == testUturnStatBlob) {
-        LOGDEBUG << "Enabling uturn testing for AM service stat blob API";
-    }
-    testUturnGetBlob = conf.get_abs<bool>("fds.am.testing.uturn_amserv_getblob");
-    if (true == testUturnGetBlob) {
-        LOGDEBUG << "Enabling uturn testing for AM service get blob API";
-    }
 
     responseApi = boost::make_shared<AmAsyncXdiResponse>();
 }
@@ -101,12 +73,6 @@ AmAsyncDataApi::startBlobTx(boost::shared_ptr<apis::RequestId>& requestId,
                             boost::shared_ptr<std::string>& volumeName,
                             boost::shared_ptr<std::string>& blobName,
                             boost::shared_ptr<fds_int32_t>& blobMode) {
-    if ((true == testUturnAll) ||
-        (true == testUturnStartTx)) {
-        LOGDEBUG << "Uturn testing start blob tx";
-        return;
-    }
-
     AsyncStartBlobTxResponseHandler::ptr handler(
         new AsyncStartBlobTxResponseHandler(responseApi,
                                             requestId));
@@ -134,35 +100,6 @@ AmAsyncDataApi::commitBlobTx(boost::shared_ptr<apis::RequestId>& requestId,
                              boost::shared_ptr<std::string>& volumeName,
                              boost::shared_ptr<std::string>& blobName,
                              boost::shared_ptr<apis::TxDescriptor>& txDesc) {
-    if ((true == testUturnAll) ||
-        (true == testUturnCommitTx)) {
-        LOGDEBUG << "Uturn testing commit blob tx";
-        return;
-    }
-
-    // Setup the transcation descriptor
-    BlobTxId::ptr blobTxDesc(new BlobTxId(
-        txDesc->txId));
-
-    SimpleResponseHandler::ptr handler(new SimpleResponseHandler(__func__));
-
-    FdsBlobReq *blobReq = new CommitBlobTxReq(invalid_vol_id,
-                                              *volumeName,
-                                              *blobName,
-                                              blobTxDesc,
-                                              SHARED_DYN_CAST(Callback, handler));
-    storHvisor->enqueueBlobReq(blobReq);
-
-    handler->wait();
-    if (handler->error != ERR_OK) {
-        apis::ApiException fdsE;
-        if (handler->error == ERR_BLOB_NOT_FOUND) {
-            fdsE.errorCode = apis::MISSING_RESOURCE;
-        } else {
-            fdsE.errorCode = apis::BAD_REQUEST;
-        }
-        throw fdsE;
-    }
 }
 
 void
@@ -180,27 +117,6 @@ AmAsyncDataApi::abortBlobTx(boost::shared_ptr<apis::RequestId>& requestId,
                             boost::shared_ptr<std::string>& volumeName,
                             boost::shared_ptr<std::string>& blobName,
                             boost::shared_ptr<apis::TxDescriptor>& txDesc) {
-    if ((true == testUturnAll) ||
-        (true == testUturnAbortTx)) {
-        LOGDEBUG << "Uturn testing abort blob tx";
-        return;
-    }
-
-    SimpleResponseHandler::ptr handler(new SimpleResponseHandler(__func__));
-
-    // Setup the transcation descriptor
-    BlobTxId::ptr blobTxDesc(new BlobTxId(
-        txDesc->txId));
-
-    FdsBlobReq *blobReq = new AbortBlobTxReq(invalid_vol_id,
-                                             *volumeName,
-                                             *blobName,
-                                             blobTxDesc,
-                                             SHARED_DYN_CAST(Callback, handler));
-    storHvisor->enqueueBlobReq(blobReq);
-
-    handler->wait();
-    handler->process();
 }
 
 void
@@ -239,36 +155,6 @@ AmAsyncDataApi::updateMetadata(boost::shared_ptr<apis::RequestId>& requestId,
                                boost::shared_ptr<std::string>& blobName,
                                boost::shared_ptr<apis::TxDescriptor>& txDesc,
                                boost::shared_ptr< std::map<std::string, std::string> >& metadata) {
-    if ((true == testUturnAll) ||
-        (true == testUturnUpdateMeta)) {
-        LOGDEBUG << "Uturn testing update metadata";
-        return;
-    }
-    SimpleResponseHandler::ptr handler(new SimpleResponseHandler(__func__));
-    boost::shared_ptr<fpi::FDSP_MetaDataList> metaDataList(new fpi::FDSP_MetaDataList());
-    LOGDEBUG << "received updateMetadata cmd";
-    fpi::FDSP_MetaDataPair metaPair;
-    for (auto const & meta : *metadata) {
-        LOGDEBUG << meta.first << ":" << meta.second;
-        metaPair.key = meta.first;
-        metaPair.value = meta.second;
-        metaDataList->push_back(metaPair);
-    }
-    // Setup the transcation descriptor
-    BlobTxId::ptr blobTxDesc(new BlobTxId(
-        txDesc->txId));
-
-    FdsBlobReq *blobReq = new SetBlobMetaDataReq(invalid_vol_id,
-                                                 *volumeName,
-                                                 *blobName,
-                                                 blobTxDesc,
-                                                 metaDataList,
-                                                 SHARED_DYN_CAST(Callback, handler));
-    storHvisor->enqueueBlobReq(blobReq);
-
-    handler->wait();
-    LOGDEBUG << "set meta data returned";
-    handler->process();
 }
 
 void
@@ -294,42 +180,6 @@ AmAsyncDataApi::updateBlobOnce(boost::shared_ptr<apis::RequestId>& requestId,
                                boost::shared_ptr<int32_t>& length,
                                boost::shared_ptr<apis::ObjectOffset>& objectOffset,
                                boost::shared_ptr< std::map<std::string, std::string> >& metadata) {
-    if ((true == testUturnAll) ||
-        (true == testUturnUpdateBlob)) {
-        LOGDEBUG << "Uturn testing update blob";
-        return;
-    }
-
-    fds_verify(*length >= 0);
-    fds_verify(objectOffset->value >= 0);
-
-    // Create context handler
-    UpdateBlobResponseHandler::ptr putHandler(
-        boost::make_shared<UpdateBlobResponseHandler>());
-
-    FdsBlobReq *blobReq = new PutBlobReq(invalid_vol_id,
-                                         *volumeName,
-                                         *blobName,
-                                         static_cast<fds_uint64_t>(objectOffset->value),
-                                         *length,
-                                         const_cast<char *>(bytes->c_str()),
-                                         *blobMode,
-                                         metadata,
-                                         putHandler);
-    storHvisor->enqueueBlobReq(blobReq);
-
-    // Wait for a signal from the callback thread
-    putHandler->wait();
-
-    LOGDEBUG << "Finishing updateBlobOnce for blob " << *blobName
-             << " at object offset " << objectOffset->value
-             << " status:" << putHandler->status;
-
-        // Throw an exception if we didn't get an OK response
-    if (putHandler->status != ERR_OK) {
-        apis::ApiException fdsE;
-        throw fdsE;
-    }
 }
 
 void
@@ -355,50 +205,6 @@ AmAsyncDataApi::updateBlob(boost::shared_ptr<apis::RequestId>& requestId,
                            boost::shared_ptr<int32_t>& length,
                            boost::shared_ptr<apis::ObjectOffset>& objectOffset,
                            boost::shared_ptr<bool>& isLast) {
-    if ((true == testUturnAll) ||
-        (true == testUturnUpdateBlob)) {
-        LOGDEBUG << "Uturn testing update blob";
-        return;
-    }
-
-    BucketContext bucket_ctx("host", *volumeName, "accessid", "secretkey");
-
-    fds_verify(*length >= 0);
-    fds_verify(objectOffset->value >= 0);
-
-    // Create context handler
-    UpdateBlobResponseHandler::ptr putHandler(
-        boost::make_shared<UpdateBlobResponseHandler>());
-
-    // Setup the transcation descriptor
-    BlobTxId::ptr blobTxDesc(new BlobTxId(
-        txDesc->txId));
-
-    FdsBlobReq *blobReq = new PutBlobReq(invalid_vol_id,
-                                         *volumeName,
-                                         *blobName,
-                                         static_cast<fds_uint64_t>(objectOffset->value),
-                                         *length,
-                                         const_cast<char *>(bytes->c_str()),
-                                         blobTxDesc,
-                                         *isLast,
-                                         &bucket_ctx,
-                                         NULL,
-                                         NULL,
-                                         putHandler);
-    storHvisor->enqueueBlobReq(blobReq);
-
-    // Wait for a signal from the callback thread
-    putHandler->wait();
-
-    LOGDEBUG << "Finishing updateBlob for blob " << *blobName
-             << " at object offset " << objectOffset->value;
-
-    // Throw an exception if we didn't get an OK response
-    if (putHandler->status != ERR_OK) {
-        apis::ApiException fdsE;
-        throw fdsE;
-    }
 }
 
 void
