77014ec60f74736674f3c11e5ac8f01f098bb192
Nate Bever <nate@formationds.com>
2014-10-29 15:19:24 -0600

    fixes to firebreak and addition of the activity pager

--- source/java/admin-webapp/src/scripts/activity/activityController.js
+++ source/java/admin-webapp/src/scripts/activity/activityController.js
@@ -1,7 +1,12 @@
 angular.module( 'activity-management' ).controller( 'activityController', ['$scope', '$activity_service', '$authorization', function( $scope, $activity_service, $authorization ){
 
     $scope.activities = [];
+    $scope.filter = {};
 
+    $scope.getActivities = function( filter, callback ){
+        $activity_service.getActivities( filter, callback );
+    };
+    
     $scope.activityCallback = function( response ){
         $scope.activities = eval( response );
     };
@@ -10,6 +15,6 @@ angular.module( 'activity-management' ).controller( 'activityController', ['$sco
         return $authorization.isAllowed( permission );
     };
 
-    $activity_service.getActivities( '', '', 1000, $scope.activityCallback );
+//    $activity_service.getActivities( '', '', 1000, $scope.activityCallback );
 
 }]);
