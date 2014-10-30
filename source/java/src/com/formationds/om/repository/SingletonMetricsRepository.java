e30dc7298e91633743f8d82a4b4b6103ec2a869f
Dave Setzke <dave@formationds.com>
2014-10-30 09:42:18 -0600

    reformat before adding support for the event repository.

--- source/java/src/com/formationds/om/repository/SingletonMetricsRepository.java
+++ source/java/src/com/formationds/om/repository/SingletonMetricsRepository.java
@@ -8,43 +8,42 @@ package com.formationds.om.repository;
  * @author ptinius
  */
 public class SingletonMetricsRepository {
-  private static SingletonMetricsRepository instance = null;
+    private static SingletonMetricsRepository instance = null;
 
-  /**
-   * singleton instance of SingletonMetricsRepository
-   */
-  public static SingletonMetricsRepository instance() {
-    if( instance == null ) {
-      instance = new SingletonMetricsRepository();
+    /**
+     * singleton instance of SingletonMetricsRepository
+     */
+    public static SingletonMetricsRepository instance() {
+        if (instance == null) {
+            instance = new SingletonMetricsRepository();
+        }
+
+        return instance;
     }
 
-    return instance;
-  }
-
-  /**
-   * singleton default constructor
-   */
-  private SingletonMetricsRepository() {
-  }
-
-  private MetricsRepository metricsRepository = null;
-
-  /**
-   * @return Returns the {@link com.formationds.om.repository.MetricsRepository}
-   */
-  public MetricsRepository getMetricsRepository() {
-    if( metricsRepository == null )
-    {
-      setMetricsRepository( new MetricsRepository() );
+    /**
+     * singleton default constructor
+     */
+    private SingletonMetricsRepository() {
     }
 
-    return metricsRepository;
-  }
+    private MetricsRepository metricsRepository = null;
 
-  /**
-   * @param metricsRepository the {@link com.formationds.om.repository.MetricsRepository}
-   */
-  public void setMetricsRepository( final MetricsRepository metricsRepository ) {
-    this.metricsRepository = metricsRepository;
-  }
+    /**
+     * @return Returns the {@link com.formationds.om.repository.MetricsRepository}
+     */
+    public MetricsRepository getMetricsRepository() {
+        if (metricsRepository == null) {
+            setMetricsRepository(new MetricsRepository());
+        }
+
+        return metricsRepository;
+    }
+
+    /**
+     * @param metricsRepository the {@link com.formationds.om.repository.MetricsRepository}
+     */
+    public void setMetricsRepository(final MetricsRepository metricsRepository) {
+        this.metricsRepository = metricsRepository;
+    }
 }
