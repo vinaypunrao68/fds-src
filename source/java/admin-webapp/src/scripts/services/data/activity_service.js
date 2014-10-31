ca727cada03dd8c475a3f5bb3303312c2d1f303b
Nate Bever <nate@formationds.com>
2014-10-31 10:18:41 -0600

    temporary activity call wiring for testing purposes

--- source/java/admin-webapp/src/scripts/services/data/activity_service.js
+++ source/java/admin-webapp/src/scripts/services/data/activity_service.js
@@ -4,10 +4,16 @@ angular.module( 'activity-management' ).factory( '$activity_service', [ '$http_f
 
     service.getActivities = function( filter, callback ){
 
-        return $http_fds.get( '/scripts/services/data/fakelogs.js',
-            function( response ){
-                callback( eval( response )[0] );
-            });
+//        return $http_fds.get( '/scripts/services/data/fakelogs.js',
+//            function( response ){
+//                callback( eval( response )[0] );
+//            });
+        
+        filter = {
+            points: 10
+        };
+        
+        return $http_fds.put( '/api/config/events', filter, callback );
     };
 
     return service;
