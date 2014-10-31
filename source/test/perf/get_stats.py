0644d669d82105584c7aefa9060653f5c8eede07
Matteo Monchiero <matteo@formationds.com>
2014-10-31 11:53:00 -0700

    Performance scripts update

--- source/test/perf/get_stats.py
+++ source/test/perf/get_stats.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from parse_cntrs import Counters
+import dataset
 
 g_pidmap = {}
 
@@ -46,6 +47,8 @@ def get_avglat():
             cmd = "grep 'Time per request' %s |head -1| awk '{print $4}'" % (of)
         elif options.block_enable:
             return None
+        elif options.java_tester:
+            cmd ="cat %s |grep Average| awk '{e+=$4; c+=1}END{print e/c*1e-6}'" % (of)
         else:
             # cmd = "cat %s | grep Summary |awk '{print $17}'" % (of)
             cmd = "cat %s | grep Summary | awk '{e+=$17 ; i+=1}END{print e/i}'" % (of)
@@ -76,7 +79,9 @@ def get_avgth():
             cmd = "grep 'Requests per second' %s | awk '{print $4}'" % (of)
         elif options.block_enable:
             cmd = "cat %s | grep 'Experiment result' | awk '{print $11}'" % (of)
-        else:        
+        elif options.java_tester:
+            cmd = "cat %s | grep IOPs | awk '{e+=$2}END{print e}'" % (of)    
+        else:         
             # cmd = "cat %s | grep Summary |awk '{print $15}'" % (of)
             cmd = "cat %s | grep Summary |awk '{e+=$15}END{print e}'" % (of)
 
@@ -136,7 +141,7 @@ def ma(v, w):
         _ma.append(1.0*sum(v[i-w:i])/w)
     return _ma
 
-def getmax(series, window = 5, skipbeg = 2, skipend = 2):
+def getmax(series, window = 5, skipbeg = 1, skipend = 1):
     _s = series[skipbeg:len(series)-skipend]
     if len(_s) == 0:
         return 0
@@ -178,6 +183,16 @@ def get_iops(node):
     iops_series = [sum(e) for e in zip(*all_values)]
     return iops_series
 
+def get_int(node):
+    cmd = "cat %s/%s/col/stdout | awk '{print $12}' | tail -n +3" % (options.directory, node)
+    values = [float(x.rstrip("\n")) for x in os.popen(cmd)]
+    return values
+
+def get_cs(node):
+    cmd = "cat %s/%s/col/stdout | awk '{print $13}' | tail -n +3" % (options.directory, node)
+    values = [float(x.rstrip("\n")) for x in os.popen(cmd)]
+    return values
+
 def get_await(node):
     await_map = {}
     await_series = []
@@ -206,14 +221,32 @@ def get_test_parms():
     #results_2014-08-05_nvols\:1.threads\:1.type\:GET.nfiles\:10000.test_type\:tgen.fsize\:4096.nreqs\:100000.test
     m = re.match("(\w+)_(\d+-)_nvols:(\d+).threads:(\d+).type:(\w+).nfiles:(\d+).test_type:(\w+).fsize:(\d+).nreqs:(\d+).test", options.directory)
 
-def print_counter(counters, agent, counter, ctype):
+def print_counter(counters, agent, counter, ctype, table):
     c_node = "han" #FIXME
     series = counters.get_cntr(c_node, agent, counter, ctype)
-    if series is not None:
-        for v in series:
-            print agent + "-" + counter + ", ", getmax(series[v][0])/1e6, ", ",
+    if ctype == "count":
+        if series is not None:
+            for v in series:
+                values = np.asarray(series[v][0])
+                time = np.asarray(series[v][1])
+                time -= time[0]
+                rate = np.diff(values) / np.diff(time)
+                value = getmax(rate)
+                print agent + "-" + counter + ", ", value, ", ",
+                table[agent+":"+counter] = value 
+        else:
+            print agent + "-" + counter + ", ", -1, ", ",
+            table[agent+":"+counter] = -1 
+
     else:
-        print agent + "-" + counter + ", ", 0, ", ",
+        if series is not None:
+            for v in series:
+                value = getmax(series[v][0])/1e6
+                print agent + "-" + counter + ", ", value, ", ",
+                table[agent+":"+counter] = value
+        else:
+            print agent + "-" + counter + ", ", -1, ", ",
+            table[agent+":"+counter] = -1
 
 # FIXME: multivolumes!
 
@@ -226,17 +259,28 @@ def main():
     parser.add_option("-A", "--ab-enable", dest = "ab_enable", action = "store_true", default = False, help = "AB mode")
     parser.add_option("-B", "--block-enable", dest = "block_enable", action = "store_true", default = False, help = "Block mode")
     parser.add_option("-g", "--use-graphite-streaming", dest = "use_graphite_streaming", default = False, action = "store_true", help = "Use graphite streaming as input")
+    parser.add_option("-j", "--java-tester", dest = "java_tester", default = False, action = "store_true", help = "Java tester output")
+    parser.add_option("-b", "--dump-to-db", dest = "dump_to_db", default = None, help = "Dump to database as specified")
     global options
     (options, args) = parser.parse_args()
     compute_pidmap()
-    counters = Counters(graphite = False)
+    if options.dump_to_db != None:
+        db = dataset.connect('sqlite:///%s' % options.dump_to_db)
+
+    counters = Counters(graphite = False, volume_zero = True)
     c_file = open(options.directory + "/counters.dat")
     counters.parse(c_file.read())
     c_file.close()
+    table = {}
     # counters.get_cntr()
     print options.name,",",
+    table["name"] = options.name
     time.sleep(1)
-    print "th,", get_avgth(),",",
+    th = get_avgth()
+    print "th,", th,",",
+    table["th"] = th
+    lat = get_avglat()
+    table["lat"] = lat
     print "lat,", get_avglat(),",",
     for node in nodes.keys():
         for agent,pid in g_pidmap[node].items():
@@ -245,18 +289,49 @@ def main():
                 plot_series(get_mem(node, agent, pid), "Memory - " + agent + " - " + options.name, "megabatyes")
                 plot_series(get_cpu(node, agent, pid), "CPU - " + agent + " - " + options.name, "CPU utilization [%]")
             else:
-                print "maxmem,",getmax(get_mem(node, agent, pid)),", ",
-                print "maxcpu,",getmax(get_cpu(node, agent, pid)),", ",
+                mem = getmax(get_mem(node, agent, pid))
+                table[agent+":mem"] = mem
+                print "maxmem,", mem,", ",
+                cpu = getmax(get_cpu(node, agent, pid))
+                print "maxcpu,", cpu,", ",
+                table[agent+":cpu"] = cpu
         if options.plot_enable:
             plot_series(get_iops(node), "IOPS - " + options.name, "IOPS (disk)")
             plot_series(get_await(node), "AWAIT - " + options.name, "AWAIT (disk)")
             plot_series(get_idle(node), "Avg Idle - " + options.name, "Idle CPU Time [%]")
         else:
-            print "maxiops,",getmax(get_iops(node)),", ",
-            print "maxawait,",getmax(get_await(node)),", ",
-            print "avgidle,",getavg(get_idle(node)),", ",
-    print_counter(counters, "am","am_get_obj_req", "latency")
-    print "avgjavalat,", getavg(get_java_cntr()), ", ",
+            iops = getmax(get_iops(node))
+            print "maxiops,", iops,", ",
+            table["iops"] = iops
+            await = getmax(get_await(node))
+            print "maxawait,", await,", ",
+            table["await"] = await
+            idle = getavg(get_idle(node))
+            print "avgidle,",idle,", ",
+            table["idle"] = idle
+            interrupts = getavg(get_int(node))
+            print "interrupts,",interrupts,", ",
+            table["interrupts"] = interrupts
+            cs = getavg(get_cs(node))
+            print "cs,",cs,", ",
+            table["cs"] = cs
+    print_counter(counters, "am", "am_get_obj_req", "latency", table)
+    print_counter(counters, "am", "am_get_obj_req", "count", table)
+    # am:am_get_obj_req,am:am_get_sm,am:am_get_dm,am:am_get_qos
+    print_counter(counters, "am", "am_get_sm", "latency", table)
+    print_counter(counters, "am", "am_get_dm", "latency", table)
+    print_counter(counters, "am", "am_get_qos", "latency", table)
+
+
+    print_counter(counters, "am", "am_put_obj_req", "latency", table)
+    print_counter(counters, "am", "am_put_obj_req", "count", table)
+    print_counter(counters, "am", "am_put_sm", "latency", table)
+    print_counter(counters, "am", "am_put_dm", "latency", table)
+    print_counter(counters, "am", "am_put_qos", "latency", table)
+    
+    javalat = getavg(get_java_cntr())
+    print "avgjavalat,", javalat, ", ",
+    table["javalat"] = javalat
     # print_counter(counters, "am","am_stat_blob_obj_req", "latency")
     # print_counter(counters, "sm","get_obj_req", "latency")
     #print_counter("AMAgent","am_stat_blob_obj_req", "latency")
@@ -274,7 +349,10 @@ def main():
     # else:
     #     print "am_get_latency: ", -1,
     print "" 
-
+    if options.dump_to_db != None:
+        db["experiments"].insert(table)
+    for e in db["experiments"].all():
+        print e
 
 if __name__ == "__main__":
     main()
