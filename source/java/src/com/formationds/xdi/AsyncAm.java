5c5077ac258ddcfa9853816d73cf4dfb2d698c2a
Fabrice Medio <fabrice@formationds.com>
2014-11-05 15:13:50 -0800

    [WIN-1155] Andrew's unit test

--- source/java/src/com/formationds/xdi/AsyncAm.java
+++ source/java/src/com/formationds/xdi/AsyncAm.java
@@ -36,6 +36,7 @@ import static org.junit.Assert.assertArrayEquals;
  */
 public class AsyncAm {
     public static final int PORT = 9876;
+    public static final int BLOCKSIZE = 4096;
     private static final Logger LOG = Logger.getLogger(AsyncAm.class);
     private final AsyncAmResponseListener responseListener;
     private AsyncResourcePool<XdiClientConnection<AmService.AsyncIface>> amPool;
@@ -51,6 +52,11 @@ public class AsyncAm {
         responseListener = new AsyncAmResponseListener(2, TimeUnit.SECONDS);
         AsyncAmServiceResponse.Processor<AsyncAmResponseListener> processor = new AsyncAmServiceResponse.Processor<>(responseListener);
 
+//        TNonblockingServer server = new TNonblockingServer(new TNonblockingServer.Args(new TNonblockingServerSocket(PORT))
+//                .protocolFactory(new TBinaryProtocol.Factory())
+//                .transportFactory(new TFramedTransport.Factory())
+//                .processor(processor));
+//
         TSimpleServer server = new TSimpleServer(new TServer.Args(new TServerSocket(PORT))
                 .protocolFactory(new TBinaryProtocol.Factory())
                 .transportFactory(new TFramedTransport.Factory())
@@ -61,6 +67,22 @@ public class AsyncAm {
     }
 
     public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.out.println("Usage: AsyncAm hostName volumeName objectName");
+            System.exit(-1);
+        }
+        String host = args[0];
+        String volumeName = args[1];
+        String objectName = args[2];
+        XdiClientFactory factory = new XdiClientFactory();
+        AsyncAmServiceRequest.Iface am = factory.remoteOnewayAm(host, 8899);
+        byte[] bytes = new byte[BLOCKSIZE];
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(bytes);
+        am.updateBlobOnce(new RequestId(UUID.randomUUID().toString()), S3Endpoint.FDS_S3, volumeName, objectName, 1, ByteBuffer.wrap(bytes), BLOCKSIZE, new ObjectOffset(0), new HashMap<>());
+    }
+
+    public static void _main(String[] args) throws Exception {
         SecureRandom secureRandom = new SecureRandom();
         int length = 4096;
         byte[] bytes = new byte[length];
@@ -75,9 +97,9 @@ public class AsyncAm {
 
         factory.remoteOmService("localhost", 9090).createVolume(S3Endpoint.FDS_S3, volumeName, volumeSettings, 0);
         Thread.sleep(2000);
-
+        System.out.println("Done creating volume");
         AsyncAm asyncAm = new AsyncAm(factory.makeAmAsyncPool("localhost", 9988), factory.remoteOnewayAm("localhost", 8899), new DumbAuthorizer());
-        Thread.sleep(1000);
+        Thread.sleep(500);
 
         CompletableFuture<Void> updateCf = asyncAm.updateBlobOnce(AuthenticationToken.ANONYMOUS, S3Endpoint.FDS_S3, volumeName, blobName, 1, ByteBuffer.wrap(bytes), length, new ObjectOffset(0), new HashMap<>());
         updateCf.get();
@@ -197,11 +219,11 @@ public class AsyncAm {
 
     public CompletableFuture<Void> updateBlobOnce(AuthenticationToken token, String domainName, String volumeName, String blobName,
                                                   int blobMode, ByteBuffer bytes, int length, ObjectOffset offset, Map<String, String> metadata) {
-//        return scheduleAsync(token, domainName, volumeName, rid ->
-//                oneWayAm.updateBlobOnce(rid, domainName, volumeName, blobName, blobMode, bytes, length, offset, metadata));
-        return schedule(token, "updateBlobOnce", volumeName, (am, cf) -> {
-            am.updateBlobOnce(domainName, volumeName, blobName, blobMode, bytes, length, new ObjectOffset(offset), metadata, makeThriftCallbacks(cf, v -> null));
-        });
+        return scheduleAsync(token, domainName, volumeName, rid ->
+                oneWayAm.updateBlobOnce(rid, domainName, volumeName, blobName, blobMode, bytes, length, offset, metadata));
+//        return schedule(token, "updateBlobOnce", volumeName, (am, cf) -> {
+//            am.updateBlobOnce(domainName, volumeName, blobName, blobMode, bytes, length, new ObjectOffset(offset), metadata, makeThriftCallbacks(cf, v -> null));
+//        });
     }
 
     public CompletableFuture<Void> deleteBlob(AuthenticationToken token, String domainName, String volumeName, String blobName) {
