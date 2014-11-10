1e6fa39023adf0c61d2df7eaad0d8c5eaf7b7de1
Brian Szmyd <szmyd@formationds.com>
2014-11-10 17:18:45 +0000

    WIN 1272 : Improve cache hit performance.

--- source/include/cache/KvCache.h
+++ source/include/cache/KvCache.h
@@ -273,18 +273,9 @@ template <class K, class V, class _Hash = std::hash<K>>
             return ERR_NOT_FOUND;
         }
 
-        KvMapEntry existingEntry = mapIt->second;
-        V* value  = existingEntry->second;
-        // Remove the existing map entry since the
-        // iterator into the eviction list is changing
-        cacheMap->erase(mapIt);
-
-        // Move the entry's position to the front
-        // in the eviction list
-        evictionList->erase(existingEntry);
-        KvPair updatedEntry(key, value);
-        evictionList->push_front(updatedEntry);
-        (*cacheMap)[key] = evictionList->begin();
+        evictionList->splice(evictionList->begin(),
+                             *evictionList,
+                             mapIt->second);
 
         return ERR_OK;
     }
