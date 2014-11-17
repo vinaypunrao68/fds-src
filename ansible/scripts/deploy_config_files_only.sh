14b91c077e6a212e0c5d3af820e6f4022989568e
Matteo Monchiero <matteo@formationds.com>
2014-11-17 10:51:32 -0800

    Change to deploy_config_files_only to add an option to disable sudo (-k) Made authentication a parameter in fds_config_defaults

--- ansible/scripts/deploy_config_files_only.sh
+++ ansible/scripts/deploy_config_files_only.sh
@@ -1,5 +1,29 @@
 #! /bin/bash
 
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+no_sudo=0
+
+function show_help {
+    echo "Usage: ./prog [-k] hostname"
+    echo "Use -k to disable request for sudo password (when passwordless sudo is enabled)"
+}
+
+while getopts "h?k" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    k)  no_sudo=1
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+echo "no_sudo=$no_sudo, Leftovers: $@"
+
 if [ $# -eq 0 ]; then 
     echo "Need to provide a hostname or groupname that is present in the ansible_hosts"
     exit 1
@@ -9,7 +33,12 @@ hosts="$1"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
 inventory=${script_dir}/../ansible_hosts
 playbooks=${script_dir}/../playbooks
-ansible_args=" -i ${inventory} -e "target_hosts=${hosts}" ${playbooks}/deploy_config_files_only.yml -K"
+
+ansible_args=" -i ${inventory} -e "target_hosts=${hosts}" ${playbooks}/deploy_config_files_only.yml"
+if [ $no_sudo -eq 0 ]; then 
+    echo sudo
+    ansible_args="$ansible_args -K"
+fi
 
 # Test auth to target hosts
 ansible -i ${inventory} ${hosts} -m ping > /dev/null 2>&1
