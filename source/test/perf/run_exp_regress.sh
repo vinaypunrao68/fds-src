35a379ca8539b7d7f0b0640e627c1ffb6fb6cc03
Matteo Monchiero <matteo@formationds.com>
2014-11-18 18:59:10 -0800

    Fixed bug in run_exp_regress script

--- source/test/perf/run_exp_regress.sh
+++ source/test/perf/run_exp_regress.sh
@@ -5,6 +5,7 @@
 WORKSPACE=$1
 TEST_IP=$2
 TAG=$3
+TEST_JSON=$4
 
 PYRO_IP="10.1.10.139"
 PYRO_PORT=47672
@@ -12,7 +13,6 @@ PYRO_PORT=47672
 ./deploy_confs.sh `echo $TAG | awk -F ":" '{print $3}'` $HOSTNAME
 ./is_fds_up.py -K
 TEST_DIR=/regress/test-$RANDOM
-TEST_JSON=regress/config/get.json
 ./run_experiment.py -n $PYRO_IP -p $PYRO_PORT -m $HOSTNAME -M local-single -J $TEST_JSON -d $TEST_DIR -c 1 -j -t $TEST_IP -D $WORKSPACE --fds-nodes $HOSTNAME
 ./do_stats.sh $TEST_DIR $HOSTNAME test.db $HOSTNAME $TAG > $TEST_DIR/results.csv
 
