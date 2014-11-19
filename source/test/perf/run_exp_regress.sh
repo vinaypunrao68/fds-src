b0b440db48cb5cf46720a86d6b1b5f5d73cb0286
Matteo Monchiero <matteo@formationds.com>
2014-11-18 18:31:44 -0800

    Restructuring regression scripts

---
+++ source/test/perf/run_exp_regress.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ./run_exp_regress.sh $WORKSPACE $TEST_IP s3:get:amcache0
+
+WORKSPACE=$1
+TEST_IP=$2
+TAG=$3
+
+PYRO_IP="10.1.10.139"
+PYRO_PORT=47672
+
+./deploy_confs.sh `echo $TAG | awk -F ":" '{print $3}'` $HOSTNAME
+./is_fds_up.py -K
+TEST_DIR=/regress/test-$RANDOM
+TEST_JSON=regress/config/get.json
+./run_experiment.py -n $PYRO_IP -p $PYRO_PORT -m $HOSTNAME -M local-single -J $TEST_JSON -d $TEST_DIR -c 1 -j -t $TEST_IP -D $WORKSPACE --fds-nodes $HOSTNAME
+./do_stats.sh $TEST_DIR $HOSTNAME test.db $HOSTNAME $TAG > $TEST_DIR/results.csv
