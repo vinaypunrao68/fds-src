cae34faeea655278af63ac9cdc12b1014abe2df3
Dave Setzke <dave@formationds.com>
2014-11-21 08:13:31 -0700

    fix whitespace and add javadoc comment

--- source/java/src/com/formationds/commons/events/EventType.java
+++ source/java/src/com/formationds/commons/events/EventType.java
@@ -1,11 +1,13 @@
 /*
  * Copyright (c) 2014, Formation Data Systems, Inc. All Rights Reserved.
  */
-
 package com.formationds.commons.events;
 
 import java.io.Serializable;
 
+/**
+ * Supported event types
+ */
 public enum EventType implements Serializable {
     USER_ACTIVITY,
     SYSTEM_EVENT,
