cd2484d7bda5ff1ced2ea8648518acbdf56f0359
Cody Boggs <cody@formationds.com>
2014-12-04 14:33:52 -0700

    moved output line I just changed so that it doesn't get repeated unnecessarily

--- jenkins_scripts/expire_nightly_artifacts.py
+++ jenkins_scripts/expire_nightly_artifacts.py
@@ -15,6 +15,8 @@ resp, content = conn.request("http://artifacts.artifactoryonline.com/artifacts/a
 
 content_json = json.loads(content)
 
+print "\nINFO: Expriring old artifacts from Artifactory (> 13 builds ago)"
+        
 for deb_name in deb_names:
     deb_dict[deb_name] = []
 
@@ -25,7 +27,6 @@ for record in content_json['children']:
 
 for deb_name in deb_dict.keys():
     if len(deb_dict[deb_name]) > artifacts_to_keep:
-        print "\nINFO: Expriring old artifacts from Artifactory (> 13 builds ago)"
         for artifact in deb_dict[deb_name][:-artifacts_to_keep]:
             print "Deleting: " + artifact
             try:
