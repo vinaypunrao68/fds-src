9edc0e4d77afe8326e051fd7172c57a1c6745c59
Sanjay <sanjay@formationds.com>
2014-12-08 13:07:12 -0800

    cli support for listing the blob based on the  blob name pattern

--- source/tools/fdsconsole/contexts/service.py
+++ source/tools/fdsconsole/contexts/service.py
@@ -158,7 +158,7 @@ class ServiceContext(Context):
     #--------------------------------------------------------------------------------------
     @clicmd
     @arg('volname', help='-volume name')
-    @arg('pattern', help='-blob name pattern for seach')
+    @arg('pattern', help='-blob name pattern for search')
     @arg('maxkeys', help= "-max number for keys", nargs='?' , type=long, default=1000)
     @arg('startpos', help= "-starting  position of the blob list", nargs='?' , type=long, default=0)
     def listhdfsblob(self, volname, pattern, maxkeys, startpos):
