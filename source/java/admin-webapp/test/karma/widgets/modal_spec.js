c9c5a028c0c4184c76722e4fade10152b7e4a9b4
Nate Bever <nate@formationds.com>
2014-12-15 10:53:57 -0700

    introducing a tolerance for the waterfall slider due to browser text rendering differences

--- source/java/admin-webapp/test/karma/widgets/modal_spec.js
+++ source/java/admin-webapp/test/karma/widgets/modal_spec.js
@@ -16,7 +16,7 @@ describe( 'Modal dialog', function(){
                'common.l_warning': 'Warning',
                'common.l_error': 'Error',
                'common.l_confirm': 'Confirm',
-               'common.l_info': 'Info',
+               'common.l_info': 'Information',
                'common.l_ok': 'OK',
                'common.l_cancel': 'Cancel',
                'common.l_yes': 'Yes',
@@ -57,9 +57,99 @@ describe( 'Modal dialog', function(){
         $scope.$apply();
         expect( $modal.css( 'display' ) ).toBe( 'block' );
         expect( $modal.find( '.text' ).text().trim() ).toBe( text );
+        expect( $modal.find( '.header' ).text().trim() ).toBe( 'Information' );
+        expect( $modal.find( '.content-pane' ).css( 'width' ) ).toBe( '350px' );
         
         $modal.find( '.fds-modal-ok' ).click();
         
         expect( $modal.css( 'display' ) ).toBe( 'none' );
     });
+    
+    it( 'should show a warning message', function(){
+        
+        var text = 'Something is going to explode!';
+        var event = { type: 'WARN', text: text };
+        $scope.$emit( 'show-dialog', event );
+        $scope.$apply();
+        expect( $modal.css( 'display' ) ).toBe( 'block' );
+        expect( $modal.find( '.text' ).text().trim() ).toBe( text );
+        expect( $modal.find( '.header' ).text().trim() ).toBe( 'Warning' );
+        
+        $modal.find( '.fds-modal-ok' ).click();
+        
+        expect( $modal.css( 'display' ) ).toBe( 'none' );        
+    });
+    
+    it( 'should show an error message', function(){
+        
+        var text = 'Something is exploding!';
+        var event = { type: 'ERROR', text: text };
+        $scope.$emit( 'show-dialog', event );
+        $scope.$apply();
+        expect( $modal.css( 'display' ) ).toBe( 'block' );
+        expect( $modal.find( '.text' ).text().trim() ).toBe( text );
+        expect( $modal.find( '.header' ).text().trim() ).toBe( 'Error' );
+        
+        $modal.find( '.fds-modal-ok' ).click();
+        
+        expect( $modal.css( 'display' ) ).toBe( 'none' );        
+    });    
+    
+    it( 'should become larger when the message is really long', function(){
+        
+        var text = 'This is really long message that should cause the modal to expand to 500px instead of the default 350px size.  Therefore this message needs to be ridiculously long to ensure the spillover';
+        var event = { type: 'someothername', text: text };
+        $scope.$emit( 'show-dialog', event );
+        $scope.$apply();
+        
+        expect( $modal.css( 'display' ) ).toBe( 'block' );
+        expect( $modal.find( '.text' ).text().trim() ).toBe( text );
+        expect( $modal.find( '.header' ).text().trim() ).toBe( 'Information' );   
+        expect( $modal.find( '.content-pane' ).css( 'width' ) ).toBe( '500px' );
+        
+        $modal.find( '.fds-modal-ok' ).click();
+        
+        expect( $modal.css( 'display' ) ).toBe( 'none' );        
+        
+    });
+    
+    it( 'should handle a confirm dialog and return true when the user picks yes', function(){
+        
+        var confirmHandler = function( result ){
+            expect( result ).toBe( true );
+        };
+        
+        var text = 'Are you sure you want to use this UI?';
+        var event = { type: 'CONFIRM', text: text };
+        $scope.$emit( 'confirm-dialog', event, confirmHandler );
+        $scope.$apply();
+        
+        expect( $modal.css( 'display' ) ).toBe( 'block' );
+        expect( $modal.find( '.text' ).text().trim() ).toBe( text );
+        expect( $modal.find( '.header' ).text().trim() ).toBe( 'Confirm' );
+        
+        $modal.find( '.fds-modal-ok' ).click();
+        
+        expect( $modal.css( 'display' ) ).toBe( 'none' );          
+    });
+    
+    it( 'should handle a confirm dialog and return false when the user picks no', function(){
+        
+        var confirmHandler = function( result ){
+            expect( result ).toBe( false );
+        };
+        
+        var text = 'Are you sure you want to use this UI?';
+        var event = { type: 'CONFIRM', text: text };
+        $scope.$emit( 'confirm-dialog', event, confirmHandler );
+        $scope.$apply();
+        
+        expect( $modal.css( 'display' ) ).toBe( 'block' );
+        expect( $modal.find( '.text' ).text().trim() ).toBe( text );
+        expect( $modal.find( '.header' ).text().trim() ).toBe( 'Confirm' );
+        
+        $modal.find( '.fds-modal-cancel' ).click();
+        
+        expect( $modal.css( 'display' ) ).toBe( 'none' );          
+    });    
 });
