a45bb9f8fdd929e93568735b8f5a0caaa7c4c5d5
Dave Corby <davec@formationds.com>
2014-12-22 18:23:59 -0700

    Reformatted/minor refactoring of qos_tokbucket.h

--- source/lib/qos_tokbucket.h
+++ source/lib/qos_tokbucket.h
@@ -1,155 +1,261 @@
-/*
- * Copyright 2013 Formation Data Systems, Inc.
- *
- *  Basic Token Bucket implementation that 
- *  controls rate and and cannot accumulate
- *  more than burst size number of tokens 
- *  
- *  Currently using microsec granularity timers
- *  so would not support more than 1M IOPS that well.
- *  Will need to reconsider a finer granularity timers.
- */
-
+/// @file
+/// @copyright  2013 Formation Data Systems, Inc.
+///
+/// Basic Token Bucket implementation.
+///
+/// Accumulates tokens at a specified rate into a bucket of a specified capacity. Once the bucket is
+/// full, no more tokens may be accumulated until tokens are removed.
+///
+/// @remark  Currently using microsecond-granularity times, so this will not support more than 1M
+/// @remark  IOPS.
+///
 #ifndef SOURCE_LIB_QOS_TOKBUCKET_H_
 #define SOURCE_LIB_QOS_TOKBUCKET_H_
 
-#include <fds_types.h>
-#include <util/timeutils.h>
+#include <algorithm>
+
+#include "fds_types.h"
+#include "util/timeutils.h"
 
 namespace fds {
 
- /*  Demand-driven token bucket 
-  *  For use by dispatchers that will drive the token bucket state, or 
-  *  as a building block for a more complex token buckets. 
-  * 
-  *  Tokens are accumulated with rate 'rate', but never can exceed 
-  *  burst size 'burst'. Thus, token bucket control rate 'rate' and 
-  *  if tokens are not consumed and accumulate above burst size, 
-  *  the tokens above burst size will be thrown away. 
-  */
-  class TokenBucket {
-  public:
-    /* if _burst_length_microsec > 0, calculates burst based on time interval that we 
-     * wait before we start throwing away tokens, and assigns burst as min of this value and _burst */
-    TokenBucket(fds_uint64_t _rate, fds_uint64_t _burst, fds_uint64_t _burst_length_microsec=0)
-      : rate(_rate)
-    {
-       burst = _burst;
-       if (_burst_length_microsec > 0) {
-          fds_uint64_t wburst = (fds_uint64_t)( (double)_burst_length_microsec/1000000.0 * (double)rate);
-          if (wburst < burst) burst = wburst;
-       }
-       t_last_update = util::getTimeStampMicros();   // current time in microseconds
-       token_count = 0.0; /* start with 0 tokens */
+///
+/// Demand-driven token bucket.
+///
+/// For use by dispatchers that will drive the token bucket state, or as a building block for more
+/// complex token buckets.
+///
+/// Tokens are accumulated at a rate of ::rate tokens per second. The maximum number of tokens that
+/// may accumulate at one time is specified by ::burst.
+///
+class TokenBucket {
+public:
+    ///
+    /// Constructor.
+    ///
+    /// @param  rate          The number of tokens accumulated per second.
+    /// @param  burst         The maximum number of tokens that can accumulate at one time.
+    /// @param  max_burst_us  @p burst will be clamped to the number of tokens that accumulate in
+    ///                         this many microseconds.
+    ///
+    TokenBucket(fds_uint64_t rate, fds_uint64_t burst, fds_uint64_t max_burst_us = 0)
+            : rate(rate), burst(burst) {
+        if (max_burst_us > 0) {
+            TokenBucket::burst = std::min(
+                    // TODO(DAC): Is truncation the desired behavior?
+                    (fds_uint64_t)((double)max_burst_us / 1000000.0 * (double)rate), burst);
+        }
+        t_last_update = util::getTimeStampMicros();  // current time in microseconds
+        token_count = 0.0;
     }
+
+    ///
+    /// Destructor.
+    ///
     ~TokenBucket() {}
 
-    inline fds_uint64_t getRate() const { return rate; }
-    inline fds_bool_t hasTokens(fds_uint32_t num_tokens) const { return ((double)num_tokens <= token_count); }
+    ///
+    /// Get the number of tokens that are accumulated per second.
+    ///
+    /// @return  The current property value..
+    ///
+    inline fds_uint64_t getRate() const {
+        return rate;
+    }
 
-    /* Change rate and burst parameters 
-     * Means that tokens will start accumulating with new rate, and capped 
-     * to a new burst. This does not change the number of tokens that we 
-     * accumulated with the old rate. */
-    inline void modifyParams(fds_uint64_t _rate, fds_uint64_t _burst) {
+    ///
+    /// Check whether there are at least a specified number of tokens accumulated.
+    ///
+    /// @param  num_tokens  The number of tokens to check for.
+    ///
+    /// @return  Whether there are @p num_tokens currently in the bucket.
+    ///
+    inline fds_bool_t hasTokens(fds_uint32_t num_tokens) const {
+        return ((double) num_tokens <= token_count);
+    }
+
+    ///
+    /// Change the ::rate and ::burst parameters at the same time.
+    ///
+    /// Tokens since the last update will be accumulated at the new rate, but will not be clamped to
+    /// either the new or old ::burst.
+    ///
+    /// @param  rate   The new number of tokens to be accumulated per second.
+    /// @param  burst  The new maximum number of tokens that may be accumulated.
+    ///
+    inline void modifyParams(fds_uint64_t rate, fds_uint64_t burst) {
         fds_uint64_t nowMicrosec = util::getTimeStampMicros();
         updateTokensOnly(nowMicrosec);
-        /* do not cap to burst size now, updateState() will do it so it can 
-         * the number of spilled tokens */
-        rate = _rate;
-        burst = _burst;
+
+        TokenBucket::rate = rate;
+        TokenBucket::burst = burst;
     }
-    inline void modifyRate(fds_uint64_t _rate, fds_uint64_t _burst_length_microsec) {
-        modifyParams(_rate, (fds_uint64_t)( (double)_burst_length_microsec/1000000.0 * (double)_rate));
+
+    ///
+    /// Change the ::rate and ::burst parameters at the same time.
+    ///
+    /// Tokens since the last update will be accumulated at the new rate, but will not be clamped to
+    /// either the new or old ::burst.
+    ///
+    /// @param  rate      The new number of tokens to be accumulated per second.
+    /// @param  burst_us  ::burst will be set so that, at @p rate, this bucket will fill in burst_us
+    ///                     microseconds.
+    ///
+    inline void modifyRate(fds_uint64_t rate, fds_uint64_t burst_us) {
+        modifyParams(rate, (fds_uint64_t) ((double) burst_us / 1000000.0 * (double) rate));
     }
-    inline void modifyRate(fds_uint64_t _rate) {
-        modifyParams(_rate, burst);
+
+    ///
+    /// Change the ::rate parameter.
+    ///
+    /// Tokens since the last update will be accumulated at the new rate, but will not be clamped to
+    /// ::burst.
+    ///
+    /// @param  rate  The new number of tokens to be accumulated per second.
+    ///
+    inline void modifyRate(fds_uint64_t rate) {
+        modifyParams(rate, burst);
     }
 
     /* If token bucket has 'num_tokens', consume them and return true
      * otherwise return false. Assumes that token bucket state is current --
      * !! Must call updateTBState() first to update the state. */
+    ///
+    /// Attempt to consume a specified number of tokens.
+    ///
+    /// If this bucket does not have sufficient tokens to fulfill the requested number, no tokens
+    /// will be consumed.
+    ///
+    /// @pre  updateTBState() must be called if the state (number of accumulated tokens) is not
+    ///       current.
+    ///
+    /// @param  num_tokens  The number of tokens to attempt to consume.
+    ///
+    /// @return  Whether the requested number of tokens were consumed.
+    ///
     inline fds_bool_t tryToConsumeTokens(fds_uint32_t num_tokens) {
-       if ((double)num_tokens <= token_count) {
-          token_count -= (double)num_tokens;
-          return true;
-       }
-       return false;
+        if ((double)num_tokens <= token_count) {
+            token_count -= (double)num_tokens;
+            return true;
+        }
+        return false;
     }
 
-    /* if tokens are not available, returns time delay in microseconds 
-     * when tokens will become available, othewise returns 0
-     * !! Must call updateTBState() first to update the state */
+    ///
+    /// Get the number of microseconds it will take for a specified number of tokens to be
+    /// available.
+    ///
+    /// @pre  updateTBState() must be called if the state (number of accumulated tokens) is not
+    ///       current.
+    ///
+    /// @param  num_tokens  The number of tokens to get the delay for.
+    ///
+    /// @return  The number of microseconds before @p num_tokens are available.
+    ///
     inline fds_uint64_t getDelayMicrosec(fds_uint32_t num_tokens) {
-       if (token_count < (double)num_tokens) {
-          double delay_microsec = ( ((double)num_tokens - token_count)/(double)rate ) * 1000000.0;
-          return (fds_uint64_t)delay_microsec;
-       }
-       return 0; /* otherwise enough tokens */
+        if (token_count < (double) num_tokens) {
+            double delay_microsec = (((double) num_tokens - token_count)
+                    / (double) rate) * 1000000.0;
+            return (fds_uint64_t) delay_microsec;
+        }
+        return 0; /* otherwise enough tokens */
     }
- 
-    /* Update number of tokens in the token bucket
-     * returns the number of tokens that spilled over the burst size */
-    inline fds_uint64_t updateTBState(fds_uint64_t nowMicrosec) {
-       fds_uint64_t expired_tokens = 0;
-       updateTokensOnly(nowMicrosec);
-    
-       /* cap token counter to burst size */
-       if ((fds_uint64_t)token_count > burst) {
-          expired_tokens = (fds_uint64_t)token_count - burst;
-          /* important not to assign burst size, because if token_count is over burst by 
-           * fraction of a token we will lose a fraction of a token. */
-          token_count -= (double)expired_tokens;
-       }
-       return expired_tokens;
-    } 
 
-  protected: /* methods */
+    ///
+    /// Update the number of tokens in the bucket.
+    ///
+    /// @param  nowMicrosec  The current number of microseconds since epoch.
+    ///
+    /// @return  The number of tokens that spilled over ::burst.
+    ///
+    inline fds_uint64_t updateTBState(fds_uint64_t nowMicrosec) {
+        updateTokensOnly(nowMicrosec);
+        auto expired_tokens = expireTokens();
+        return expired_tokens;
+    }
+
+protected:
+    ///
+    /// Accumulate tokens without clamping to ::burst.
+    ///
+    /// @param  nowMicrosec  The current number of microseconds since epoch.
+    ///
     inline void updateTokensOnly(fds_uint64_t nowMicrosec) {
+        // FIXME(DAC): Should we freak out if we go back in time?
         fds_uint64_t elapsed_microsec = nowMicrosec - t_last_update;
         if (elapsed_microsec > 0) {
-           token_count += ((double)elapsed_microsec / 1000000.0) * (double)rate;
-           t_last_update = nowMicrosec;
+            token_count += ((double) elapsed_microsec / 1000000.0) * (double) rate;
+            t_last_update = nowMicrosec;
         }
     }
 
-  protected:
-    /* configurable parameters */
-    fds_uint64_t rate; /* rate controlled by this token bucket */
-    fds_uint64_t burst; /* maximum number of tokens that bucket can accumulate */
+    ///
+    /// Clamp the number of whole tokens in this bucket to ::burst.
+    ///
+    /// @return  The number of tokens that were expired.
+    ///
+    inline fds_uint64_t expireTokens() {
+        fds_uint64_t expired_tokens = 0;
 
-    /* dynamic state */
-    fds_uint64_t t_last_update; /* last time in microsec token bucket state was updated */
-    double token_count; /* number of tokens in the token bucket since t_last_update */
-  };
+        if ((fds_uint64_t)token_count > burst) {
+            expired_tokens = (fds_uint64_t)token_count - burst;
+            // As expired_tokens truncates, it is important to subtract the truncated number instead
+            // of assigning token_count = burst--fractional tokens cannot be used, but will
+            // accumulate until a whole token is present.
+            token_count -= (double)expired_tokens;
+        }
 
-  /*
-   * Demand-driven token bucket that can get tokens
-   * from outside (in addition to tokens generated with configured rate)
-   */
-  class RecvTokenBucket: public TokenBucket {
-  public: 
-    RecvTokenBucket(fds_uint64_t _rate, fds_uint64_t _burst)
-      : TokenBucket(_rate, _burst) {
+        return expired_tokens;
     }
-    ~RecvTokenBucket() {
-    }
+
+protected:
+    /// @defgroup  Config  Configurable parameters.
+    ///@{
+    fds_uint64_t rate;   ///< Number of tokens accumulated per second.
+    fds_uint64_t burst;  ///< Maximum number of whole tokens that this bucket can accumulate.
+    ///@}
+
+    /// @defgroup  State  Dynamic state.
+    ///@{
+    fds_uint64_t t_last_update;  ///< Microseconds since epoch on which this bucket last accumulated
+                                 ///< tokens.
+    double token_count;          ///< Number of tokens currently in the bucket.
+    ///@}
+};
+
+///
+/// Demand-driven token bucket that can receive tokens as well as generating them at the configured
+/// rate.
+///
+class RecvTokenBucket : public TokenBucket {
+public:
+    ///
+    /// Constructor.
+    ///
+    /// @param  rate   The number of tokens accumulated per second.
+    /// @param  burst  The maximum number of tokens that can accumulate at one time.
+    ///
+    RecvTokenBucket(fds_uint64_t rate, fds_uint64_t burst) : TokenBucket(rate, burst) {}
+
+    ///
+    /// Destructor.
+    ///
+    ~RecvTokenBucket() {}
 
     /* add tokens to the token bucket, burst size still applies */
+    ///
+    /// Add tokens to this bucket.
+    ///
+    /// After adding, tokens above ::burst will be expired.
+    ///
+    /// @param  add_tokens  The number of tokens to add.
+    ///
     inline void addTokens(fds_uint64_t add_tokens) {
         token_count += (double)add_tokens;
-        if ((fds_uint64_t)token_count > burst) {
-           fds_uint64_t expired_toks = (fds_uint64_t)token_count - burst;
-           token_count -= (double)expired_toks;
-        }  
-    } 
-    
-    /* base class' update tokens will update tokens based on rate,
-    *  and consume tokens are also the same */
-  };
+        (void)expireTokens();
+    }
+};
 
+}  //namespace fds
 
-} //namespace fds
-
-#endif /* SOURCE_LIB_QOS_TOKBUCKET_H_ */
-
+#endif  // SOURCE_LIB_QOS_TOKBUCKET_H_
