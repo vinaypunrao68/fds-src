ebc9029c926d2288043e18fde0e940f38df00ddc
Greg Carter <greg@formationds.com>
2015-01-09 00:27:13 -0700

    FS-601: This test may be used to confirm DMT migration as a node is added and the removed from a cluster.

--- source/test/testsuites/ClusterWithTransientNodeSuite.py
+++ source/test/testsuites/ClusterWithTransientNodeSuite.py
@@ -2,14 +2,13 @@
 #
 # Copyright 2014 by Formation Data Systems, Inc.
 #
-# From the .../source/test/testsuite directory run
-# ./ClusterBootSuite.py -q ./<QAAutotestConfig.ini> -d <sudo_pwd> --verbose
+# This test suite checks correctness in booting a
+# cluster and then adding/removing another node. In particular
+# we wish to see that Data Management Tables are properly deployed
+# upon the nodes in the cluster.
 #
-# where
-#  - <QAAutotestConfig.ini> - One of TwoNodeCluster.ini or FourNodeCluster.ini found in the
-#  testsuite directory, or one of your own.
-#  - <sudo_pwd> - The password you use for executing sudo on your machine. Even
-#  if your machine does not require a password for sudo, you must provide something, e.g. "dummy".
+# Note that the scenario configuration file must specify at
+# least one transient node.
 #
 
 import sys
@@ -21,12 +20,13 @@ import testcases.TestFDSModMgt
 import testcases.TestFDSSysMgt
 import testcases.TestFDSSysLoad
 import NodeWaitSuite
+import TransientNodeWaitSuite
 
 
 def suiteConstruction():
     """
     Construct the ordered set of test cases that comprise the
-    Two Node Cluster Setup test suite.
+    Transient Node Cluster test suite.
     """
     suite = unittest.TestSuite()
 
@@ -55,12 +55,51 @@ def suiteConstruction():
     # Bring up any AMs.
     suite.addTest(testcases.TestFDSModMgt.TestAMBringup())
 
-    # Check that all nodes are up.
+    # Check that all non-transient nodes are up.
     nodeUpSuite = NodeWaitSuite.suiteConstruction()
     suite.addTest(nodeUpSuite)
 
-    # Given the nodes some time to initialize.
+    # Give the nodes some time to initialize.
     suite.addTest(testcases.TestFDSSysMgt.TestWait())
+
+    # Verify that the OM log indicates successful DMT migration to this point.
+    suite.addTest(testcases.TestFDSSysMgt.TestInitial1NodeDMTMigration())
+
+    # Now bring up transient nodes.
+    suite.addTest(testcases.TestFDSModMgt.TestPMForTransientBringUp())
+    suite.addTest(testcases.TestFDSModMgt.TestPMForTransientWait())
+
+    # Give the transient PMs some time to initialize.
+    suite.addTest(testcases.TestFDSSysMgt.TestWait())
+
+    # Activate the cluster to include the transient nodes.
+    suite.addTest(testcases.TestFDSSysMgt.TestClusterActivate())
+
+    # Check that *all* nodes are up.
+    suite.addTest(nodeUpSuite)
+    transientNodeUpSuite = TransientNodeWaitSuite.suiteConstruction()
+    suite.addTest(transientNodeUpSuite)
+
+    # Give the transient nodes some time to initialize.
+    suite.addTest(testcases.TestFDSSysMgt.TestWait())
+
+    # Verify that the OM log indicates successful DMT migration to this point.
+    suite.addTest(testcases.TestFDSSysMgt.TestTransientAddNodeDMTMigration())
+
+    # Remove the services of the transient nodes.
+    suite.addTest(testcases.TestFDSSysMgt.TestTransientRemoveService())
+
+    # Give the OM some time to manage the node removal.
+    suite.addTest(testcases.TestFDSSysMgt.TestWait())
+
+    # Verify that the OM log indicates successful DMT migration to this point.
+    suite.addTest(testcases.TestFDSSysMgt.TestTransientRemoveNodeDMTMigration())
+
+    # Since out cluster is all on one machine, this will actually shutdown the cluster.
+    suite.addTest(testcases.TestFDSSysMgt.TestNodeShutdown())
+
+    # Cleanup FDS installation directory.
+    suite.addTest(testcases.TestFDSEnvMgt.TestFDSDeleteInstDir())
 
     return suite
 
--- source/test/testsuites/testcases/TestFDSSysMgt.py
+++ source/test/testsuites/testcases/TestFDSSysMgt.py
@@ -12,6 +12,37 @@ import TestCase
 import sys
 import time
 import os
+import logging
+
+
+def fileSearch(searchFile, searchString, occurrences):
+    """
+    Search the given file for the given search string
+    verifying whether it occurs the specified number
+    of times.
+    """
+    log = logging.getLogger('TestFDSSysMgt' + '.' + 'fileSearch')
+
+    # Verify the file exists.
+    if not os.path.isfile(searchFile):
+        log.error("File %s is not found." % (searchFile))
+        return False
+
+    f = open(searchFile, "r")
+    searchLines = f.readlines()
+    f.close()
+
+    occur = 0
+    log.info("Searching for exactly %d occurrence(s) of string [%s]." % (occurrences, searchString))
+    for line in searchLines:
+        if searchString in line:
+            occur = occur + 1
+            log.info("Found occurrence %d: %s." % (occur, line))
+
+    if occur == occurrences:
+        return True
+    else:
+        return False
 
 
 # This class contains the attributes and methods to test
@@ -84,9 +115,9 @@ class TestClusterActivate(TestCase.FDSTestCase):
 # This class contains the attributes and methods to give
 # a node some time to initialize. Once proper boot-up
 # coordination has been implemented, this can be removed.
-class TestNodeWait(TestCase.FDSTestCase):
+class TestWait(TestCase.FDSTestCase):
     def __init__(self, parameters=None):
-        super(TestNodeWait, self).__init__(parameters)
+        super(TestWait, self).__init__(parameters)
 
 
     def runTest(self):
@@ -100,10 +131,10 @@ class TestNodeWait(TestCase.FDSTestCase):
             self.log.info("Running Case %s." % self.__class__.__name__)
 
         try:
-            if not self.test_NodeWait():
+            if not self.test_Wait():
                 test_passed = False
         except Exception as inst:
-            self.log.error("Node wait caused exception:")
+            self.log.error("Wait caused exception:")
             self.log.error(traceback.format_exc())
             test_passed = False
 
@@ -117,7 +148,7 @@ class TestNodeWait(TestCase.FDSTestCase):
             return test_passed
 
 
-    def test_NodeWait(self):
+    def test_Wait(self):
         """
         Test Case:
         Give a node some time to initialize.
@@ -196,6 +227,270 @@ class TestNodeShutdown(TestCase.FDSTestCase):
             #    return False
 
         return True
+
+
+# This class contains the attributes and methods to test
+# service removal of transient nodes.
+class TestTransientRemoveService(TestCase.FDSTestCase):
+    def __init__(self, parameters=None):
+        super(TestTransientRemoveService, self).__init__(parameters)
+
+
+    def runTest(self):
+        test_passed = True
+
+        if TestCase.pyUnitTCFailure:
+            self.log.warning("Skipping Case %s. stop-on-fail/failfast set and a previous test case has failed." %
+                             self.__class__.__name__)
+            return unittest.skip("stop-on-fail/failfast set and a previous test case has failed.")
+        else:
+            self.log.info("Running Case %s." % self.__class__.__name__)
+
+        try:
+            if not self.test_TransientRemoveService():
+                test_passed = False
+        except Exception as inst:
+            self.log.error("Transient node remove service caused exception:")
+            self.log.error(traceback.format_exc())
+            test_passed = False
+
+        super(self.__class__, self).reportTestCaseResult(test_passed)
+
+        # If there is any test fixture teardown to be done, do it here.
+
+        if self.parameters["pyUnit"]:
+            self.assertTrue(test_passed)
+        else:
+            return test_passed
+
+
+    def test_TransientRemoveService(self):
+        """
+        Test Case:
+        Attempt to remove the services of transient nodes.
+        """
+
+        # Get the FdsConfigRun object for this test.
+        fdscfg = self.parameters["fdscfg"]
+        bin_dir = fdscfg.rt_env.get_bin_dir(debug=False)
+        log_dir = fdscfg.rt_env.get_log_dir()
+        om_node = fdscfg.rt_om_node
+        fds_dir = om_node.nd_conf_dict['fds_root']
+
+        nodes = fdscfg.rt_obj.cfg_nodes
+        for n in nodes:
+            # Skip the PM for the OM's node.
+            if n.nd_conf_dict['node-name'] == om_node.nd_conf_dict['node-name']:
+                self.log.info("Skipping OM's PM on %s." % n.nd_conf_dict['node-name'])
+                continue
+
+            # Skip the PM for non-transient nodes. Those are handled by TestPMWait()
+            if not n.nd_transient:
+                self.log.info("Skipping PM on non-transient node %s." % n.nd_conf_dict['node-name'])
+                continue
+
+            self.log.info("Remove services for transient node %s." % n.nd_conf_dict['node-name'])
+
+            cur_dir = os.getcwd()
+            os.chdir(bin_dir)
+
+            status = om_node.nd_agent.exec_wait('bash -c \"(nohup ./fdscli --fds-root %s --remove-services %s > '
+                                          '%s/cli.out 2>&1 &) \"' %
+                                          (fds_dir, n.nd_conf_dict['node-name'],
+                                           log_dir if n.nd_agent.env_install else "."))
+
+            os.chdir(cur_dir)
+
+            if status != 0:
+                self.log.error("Service removal of transient node %s returned status %d." %
+                               (n.nd_conf_dict['node-name'], status))
+                return False
+
+        return True
+
+
+# This class contains the attributes and methods to test
+# whether there exists the expected initial DMT migration
+# completion OM log entries for a 1-node cluster.
+class TestInitial1NodeDMTMigration(TestCase.FDSTestCase):
+    def __init__(self, parameters=None):
+        super(TestInitial1NodeDMTMigration, self).__init__(parameters)
+
+
+    def runTest(self):
+        test_passed = True
+
+        if TestCase.pyUnitTCFailure:
+            self.log.warning("Skipping Case %s. stop-on-fail/failfast set and a previous test case has failed." %
+                             self.__class__.__name__)
+            return unittest.skip("stop-on-fail/failfast set and a previous test case has failed.")
+        else:
+            self.log.info("Running Case %s." % self.__class__.__name__)
+
+        try:
+            if not self.test_Initial1NodeDMTMigration():
+                test_passed = False
+        except Exception as inst:
+            self.log.error("Initial 1-node DMT migration caused exception:")
+            self.log.error(traceback.format_exc())
+            test_passed = False
+
+        super(self.__class__, self).reportTestCaseResult(test_passed)
+
+        # If there is any test fixture teardown to be done, do it here.
+
+        if self.parameters["pyUnit"]:
+            self.assertTrue(test_passed)
+        else:
+            return test_passed
+
+
+    def test_Initial1NodeDMTMigration(self):
+        """
+        Test Case:
+        Attempt locate in OM's log logged statements indicating
+        successful completion of the initial DMT migration for
+        a 1-node cluster.
+        """
+
+        # Get the FdsConfigRun object for this test.
+        fdscfg = self.parameters["fdscfg"]
+        om_node = fdscfg.rt_om_node
+        fds_dir = om_node.nd_conf_dict['fds_root']
+        log_file = fds_dir + "/var/logs/om.log_0.log"
+
+        if fileSearch(log_file, "OM_PmAgent: will send node activate message to Node-1", 1) and\
+                fileSearch(log_file, "Sent DMT to 1 DM services successfully", 1) and\
+                fileSearch(log_file, "Sent DMT to 1 AM services successfully", 1) and\
+                fileSearch(log_file, "Sent DMT to 1 SM services successfully", 1) and\
+                fileSearch(log_file, "OM deployed DMT with 1 DMs", 1):
+            return True
+        else:
+            return False
+
+
+# This class contains the attributes and methods to test
+# whether there exists the expected DMT migration
+# completion OM log entries for a 1-node cluster with a node added.
+class TestTransientAddNodeDMTMigration(TestCase.FDSTestCase):
+    def __init__(self, parameters=None):
+        super(TestTransientAddNodeDMTMigration, self).__init__(parameters)
+
+
+    def runTest(self):
+        test_passed = True
+
+        if TestCase.pyUnitTCFailure:
+            self.log.warning("Skipping Case %s. stop-on-fail/failfast set and a previous test case has failed." %
+                             self.__class__.__name__)
+            return unittest.skip("stop-on-fail/failfast set and a previous test case has failed.")
+        else:
+            self.log.info("Running Case %s." % self.__class__.__name__)
+
+        try:
+            if not self.test_Transient2NodeDMTMigration():
+                test_passed = False
+        except Exception as inst:
+            self.log.error("Transient 2-node DMT migration caused exception:")
+            self.log.error(traceback.format_exc())
+            test_passed = False
+
+        super(self.__class__, self).reportTestCaseResult(test_passed)
+
+        # If there is any test fixture teardown to be done, do it here.
+
+        if self.parameters["pyUnit"]:
+            self.assertTrue(test_passed)
+        else:
+            return test_passed
+
+
+    def test_Transient2NodeDMTMigration(self):
+        """
+        Test Case:
+        Attempt locate in OM's log logged statements indicating
+        successful completion of the DMT migration following the
+        addition of a second node to a 1-node cluster.
+        """
+
+        # Get the FdsConfigRun object for this test.
+        fdscfg = self.parameters["fdscfg"]
+        om_node = fdscfg.rt_om_node
+        fds_dir = om_node.nd_conf_dict['fds_root']
+        log_file = fds_dir + "/var/logs/om.log_0.log"
+
+        # Some of these message will have been logged before for other reasons, so account for them.
+        if fileSearch(log_file, "OM_PmAgent: will send node activate message to Node-1", 2) and\
+                fileSearch(log_file, "OM_PmAgent: will send node activate message to Node-2", 1) and\
+                fileSearch(log_file, "Sent DMT to 2 DM services successfully", 1) and\
+                fileSearch(log_file, "Sent DMT to 1 AM services successfully", 2) and\
+                fileSearch(log_file, "Sent DMT to 2 SM services successfully", 1) and\
+                fileSearch(log_file, "OM deployed DMT with 2 DMs", 1):
+            return True
+        else:
+            return False
+
+
+# This class contains the attributes and methods to test
+# whether there exists the expected DMT migration
+# completion OM log entries for a 2-node cluster following a node removed.
+class TestTransientRemoveNodeDMTMigration(TestCase.FDSTestCase):
+    def __init__(self, parameters=None):
+        super(TestTransientRemoveNodeDMTMigration, self).__init__(parameters)
+
+
+    def runTest(self):
+        test_passed = True
+
+        if TestCase.pyUnitTCFailure:
+            self.log.warning("Skipping Case %s. stop-on-fail/failfast set and a previous test case has failed." %
+                             self.__class__.__name__)
+            return unittest.skip("stop-on-fail/failfast set and a previous test case has failed.")
+        else:
+            self.log.info("Running Case %s." % self.__class__.__name__)
+
+        try:
+            if not self.test_Transient1NodeDMTMigration():
+                test_passed = False
+        except Exception as inst:
+            self.log.error("Transient 1-node DMT migration caused exception:")
+            self.log.error(traceback.format_exc())
+            test_passed = False
+
+        super(self.__class__, self).reportTestCaseResult(test_passed)
+
+        # If there is any test fixture teardown to be done, do it here.
+
+        if self.parameters["pyUnit"]:
+            self.assertTrue(test_passed)
+        else:
+            return test_passed
+
+
+    def test_Transient1NodeDMTMigration(self):
+        """
+        Test Case:
+        Attempt locate in OM's log logged statements indicating
+        successful completion of the DMT migration following the
+        removal of a second node from a 2-node cluster.
+        """
+
+        # Get the FdsConfigRun object for this test.
+        fdscfg = self.parameters["fdscfg"]
+        om_node = fdscfg.rt_om_node
+        fds_dir = om_node.nd_conf_dict['fds_root']
+        log_file = fds_dir + "/var/logs/om.log_0.log"
+
+        # Some of these message will have been logged before for other reasons, so account for them.
+        if fileSearch(log_file, "Received remove services for nodeNode-2 remove am ? true remove sm ? true remove dm ? true", 1) and\
+                fileSearch(log_file, "Sent DMT to 1 DM services successfully", 2) and\
+                fileSearch(log_file, "Sent DMT to 1 AM services successfully", 3) and\
+                fileSearch(log_file, "Sent DMT to 1 SM services successfully", 2) and\
+                fileSearch(log_file, "OM deployed DMT with 1 DMs", 2):
+            return True
+        else:
+            return False
+
 
 if __name__ == '__main__':
     TestCase.FDSTestCase.fdsGetCmdLineConfigs(sys.argv)
