9ed57b937fa4f7c14f17a96cc1293610b8e7ebe1
Fabrice Medio <fmedio@gmail.com>
2015-01-19 17:20:58 -0800

    [FS-745] Return 404 on missing S3 buckets

--- source/java/src/com/formationds/smoketest/S3SmokeTest.java
+++ source/java/src/com/formationds/smoketest/S3SmokeTest.java
@@ -2,7 +2,6 @@ package com.formationds.smoketest;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.redshift.model.BucketNotFoundException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.S3ClientOptions;
 import com.amazonaws.services.s3.model.*;
@@ -226,10 +225,20 @@ public class S3SmokeTest {
         }
     }
 
-    @Test(expected = BucketNotFoundException.class)
+    @Test
     public void testMissingBucketReturnsFourOfFour() {
         String missingBucket = UUID.randomUUID().toString();
-        userClient.listObjects(missingBucket);
+        try {
+            userClient.listObjects(missingBucket);
+        } catch (AmazonClientException e) {
+            // Not very nice, but AmazonClientExceptions don't expose any details of the underlying HTTP transaction
+            String error = e.toString();
+            assertTrue(error.contains("404"));
+            assertTrue(error.contains("NoSuchBucket"));
+            return;
+        }
+
+        fail("Should have gotten an AmazonClientException!");
     }
 
     @Test
