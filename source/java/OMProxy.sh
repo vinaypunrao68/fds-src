5f4a8cec9df04fba1884db95ca9e8f006ce6ec9c
paul-tinius <paul@formationds.com>
2015-02-02 14:49:39 -0700

    Script of stand-a-lone execution of the OMProxy code.

--- source/java/OMProxy.sh
+++ source/java/OMProxy.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+export CLASSPATH=.
+for i in `ls /fds/lib/java/fds-1.0-bin/fds-1.0/*.jar`
+do
+    export CLASSPATH=${CLASSPATH}:${i}
+done
+
+java -cp ${CLASSPATH} com.formationds.spike.om.OMProxy
