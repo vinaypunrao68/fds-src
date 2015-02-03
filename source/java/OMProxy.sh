8e8e6ea1778f43ea1a502d5406c3cb1fc64328fa
Paul Tinius <paul-tinius@users.noreply.github.com>
2015-02-03 09:20:49 -0700

    Update OMProxy.sh

--- source/java/OMProxy.sh
+++ source/java/OMProxy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export CLASSPATH=.
-for i in `ls /fds/lib/java/fds-1.0-bin/fds-1.0/*.jar`
-do
-    export CLASSPATH=${CLASSPATH}:${i}
-done
-
+export CLASSPATH=${CLASSPATH}:/fds/lib/java/fds-1.0-bin/fds-1.0/*
+export CLASSPATH=${CLASSPATH}:/fds/lib/java/classes
 java -cp ${CLASSPATH} com.formationds.spike.om.OMProxy
