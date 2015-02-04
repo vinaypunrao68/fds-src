5d2b50c302db8be3f35f7f509106143711b234c6
Sean Kim <sean@formationds.com>
2015-02-04 07:40:44 -0700

    Removing SyncMeta from ObjMeta.

--- source/stor-mgr/smlib/ObjMeta.cpp
+++ source/stor-mgr/smlib/ObjMeta.cpp
@@ -12,81 +12,6 @@
 
 namespace fds {
 
-SyncMetaData::SyncMetaData()
-{
-    reset();
-}
-
-/**
- * Resets sync metadata
- */
-void SyncMetaData::reset()
-{
-    born_ts = 0;
-    mod_ts = 0;
-    assoc_entries.clear();
-}
-
-/**
- * Serialization routine
- * @param serializer
- * @return
- */
-uint32_t SyncMetaData::write(serialize::Serializer* serializer) const
-{
-    uint32_t sz = 0;
-    sz += serializer->writeI64((int64_t&)born_ts);
-    sz += serializer->writeI64((int64_t&)mod_ts);
-    sz += serializer->writeVector(assoc_entries);
-    return sz;
-}
-
-/**
- * Deserialization routine
- * @param deserializer
- * @return
- */
-uint32_t SyncMetaData::read(serialize::Deserializer* deserializer)
-{
-    uint32_t sz = 0;
-    sz += deserializer->readI64((int64_t&)born_ts);
-    sz += deserializer->readI64((int64_t&)mod_ts);
-    sz += deserializer->readVector(assoc_entries);
-    return sz;
-}
-
-/**
- * Return the the size of serialization buffer
- * @return
- */
-uint32_t SyncMetaData::getEstimatedSize() const
-{
-    uint32_t sz = 0;
-    sz += sizeof(born_ts) +
-            sizeof(mod_ts) +
-            serialize::getEstimatedSize(assoc_entries);
-    return sz;
-}
-
-bool SyncMetaData::operator==(const SyncMetaData &rhs) const
-{
-    if (born_ts == rhs.born_ts && mod_ts == rhs.mod_ts &&
-        assoc_entries.size() == rhs.assoc_entries.size()) {
-        return (memcmp(assoc_entries.data(),
-                rhs.assoc_entries.data(),
-                sizeof(obj_assoc_entry_t) * assoc_entries.size()) == 0);
-    }
-    return false;
-}
-
-SyncMetaData&
-SyncMetaData::operator=(const SyncMetaData &rhs) {
-    born_ts = rhs.born_ts;
-    mod_ts  = rhs.mod_ts;
-    assoc_entries = rhs.assoc_entries;
-    return *this;
-}
-
 ObjMetaData::ObjMetaData()
 {
     memset(&obj_map, 0, sizeof(obj_map));
@@ -116,7 +41,6 @@ ObjMetaData::ObjMetaData(const ObjMetaData::const_ptr &rhs) {
 
     phy_loc = &obj_map.loc_map[0];
     assoc_entry = rhs->assoc_entry;
-    sync_data   = rhs->sync_data;
 }
 
 ObjMetaData::ObjMetaData(const ObjMetaData &rhs) {
@@ -127,7 +51,6 @@ ObjMetaData::ObjMetaData(const ObjMetaData &rhs) {
 
     phy_loc = &obj_map.loc_map[0];
     assoc_entry = rhs.assoc_entry;
-    sync_data   = rhs.sync_data;
 }
 
 ObjMetaData::~ObjMetaData()
@@ -187,9 +110,6 @@ uint32_t ObjMetaData::write(serialize::Serializer* serializer) const
     fds_verify(obj_map.obj_num_assoc_entry == assoc_entry.size());
     sz += serializer->writeVector(assoc_entry);
 
-    if (syncDataExists()) {
-        sz += sync_data.write(serializer);
-    }
     fds_assert(sz == getEstimatedSize());
     return sz;
 }
@@ -213,10 +133,6 @@ uint32_t ObjMetaData::read(serialize::Deserializer* deserializer)
     sz += deserializer->readVector(assoc_entry);
     fds_assert(assoc_entry.size() == obj_map.obj_num_assoc_entry);
 
-    if (syncDataExists()) {
-        sz += sync_data.read(deserializer);
-    }
-
     fds_assert(sz == getEstimatedSize());
     return sz;
 }
@@ -229,10 +145,6 @@ uint32_t ObjMetaData::getEstimatedSize() const
 {
     uint32_t sz = sizeof(obj_map) +
             serialize::getEstimatedSize(assoc_entry);
-
-    if (syncDataExists()) {
-        sz += sync_data.getEstimatedSize();
-    }
 
     return sz;
 }
@@ -531,79 +443,6 @@ void ObjMetaData::updatePhysLocation(obj_phy_loc_t *in_phy_loc) {
 void ObjMetaData::removePhyLocation(diskio::DataTier tier) {
     phy_loc[tier].obj_tier = -1;
 }
-/**
- * Applies incoming data to metadata.
- * @param data
- */
-void ObjMetaData::apply(const fpi::FDSP_MigrateObjectMetadata& data)
-{
-    fds_assert(!syncDataExists());
-
-    /* Of sync metadata Object size and object id don't require a merge.
-     * They can directly be applied to meta data. NOTE: If obj_map has
-     * these fields set, incoming sync entry must match with existing.
-     */
-    if (obj_map.obj_size == 0) {
-        obj_map.obj_size = data.obj_len;
-        memcpy(obj_map.obj_id.metaDigest, data.object_id.digest.data(),
-                sizeof(obj_map.obj_id.metaDigest));
-    } else {
-        fds_assert(obj_map.obj_size == static_cast<uint32_t>(data.obj_len));
-        fds_assert(memcmp(obj_map.obj_id.metaDigest, data.object_id.digest.data(),
-                sizeof(obj_map.obj_id.metaDigest)) == 0);
-    }
-    /* Creation time */
-    obj_map.obj_create_time = data.born_ts;
-
-    /* Modification time */
-    obj_map.assoc_mod_time = data.modification_ts;
-
-    /* association entries */
-    assoc_entry.clear();
-    obj_map.obj_refcnt = 0;
-    for (auto itr : data.associations) {
-        obj_assoc_entry_t e;
-        e.ref_cnt = itr.ref_cnt;
-        e.vol_uuid = itr.vol_id.uuid;
-        assoc_entry.push_back(e);
-
-        obj_map.obj_refcnt += itr.ref_cnt;
-    }
-    obj_map.obj_num_assoc_entry = assoc_entry.size();
-}
-/**
- * Extracts sync entry data.  Metadata that is node specific is skipped
- * @param md
- */
-void ObjMetaData::extractSyncData(fpi::FDSP_MigrateObjectMetadata &md) const
-{
-    /* Object id */
-    fds::assign(md.object_id, obj_map.obj_id);
-
-    /* Object len */
-    md.obj_len = static_cast<int32_t>(getObjSize());
-
-    /* Born timestamp */
-    md.born_ts = obj_map.obj_create_time;
-
-    /* Modification timestamp */
-    md.modification_ts = getModificationTs();
-
-    if (obj_map.obj_refcnt == 0)  {
-        return;
-    }
-    /* Association entries */
-    fds_assert(obj_map.obj_num_assoc_entry == assoc_entry.size());
-    for (fds_uint32_t i = 0; i < obj_map.obj_num_assoc_entry; ++i) {
-        if (assoc_entry[i].ref_cnt > 0L) {
-            fds_assert(assoc_entry[i].vol_uuid != 0);
-            fpi::FDSP_ObjectVolumeAssociation a;
-            a.vol_id.uuid = assoc_entry[i].vol_uuid;
-            a.ref_cnt = assoc_entry[i].ref_cnt;
-            md.associations.push_back(a);
-        }
-    }
-}
 
 struct AssocEntryLess {
     bool operator() (const obj_assoc_entry_t &assocEntry1,
@@ -839,190 +678,8 @@ ObjMetaData::updateFromRebalanceDelta(const fpi::CtrlObjectMetaDataPropagate& ob
     return ERR_OK;
 }
 
-/**
- * While sync is in progress, existin metadata prior to sync point needs
- * to demoted as the sync data.  We do this process here.  While demoting
- * any replicable data is demoted to sync data.
- * @param syncTs
- */
-void ObjMetaData::checkAndDemoteUnsyncedData(const uint64_t& syncTs)
-{
-    if (!syncDataExists() &&
-            obj_map.assoc_mod_time != 0 &&
-            obj_map.assoc_mod_time < syncTs) {
-        /* sync_data will inherit fields from current metadata.  We will do
-         * this one field at a time.
-         * NOTE: For each field here, there should be an association resolve in
-         * mergeNewAndUnsyncedData
-         */
-        obj_map.obj_flags |= SYNCMETADATA_MASK;
 
-        /* Creation time */
-        sync_data.born_ts = obj_map.obj_create_time;
-        /* Modification time */
-        sync_data.mod_ts = obj_map.assoc_mod_time;
-        /* association entries */
-        sync_data.assoc_entries = assoc_entry;
 
-        /* Clear existing modification time.  NOTE: We keep the creation
-         * time.  However, during merge we'll use creation time from replica
-         */
-        obj_map.assoc_mod_time = 0;
-        /* Clear association information from existing metadata */
-        obj_map.obj_refcnt = 0;
-        obj_map.obj_num_assoc_entry = 0;
-        assoc_entry.clear();
-    }
-}
-
-/**
- * Applies data to sync metadata.  Normal metadata is unaffected.
- * @param data
- */
-void ObjMetaData::applySyncData(const fpi::FDSP_MigrateObjectMetadata& data)
-{
-    if (!syncDataExists()) {
-        obj_map.obj_flags |= SYNCMETADATA_MASK;
-        LOGWARN << "syncDataExists is false " << logString();
-    }
-
-    /* Of sync metadata Object size and object id don't require a merge.
-     * They can directly be applied to meta data. NOTE: If obj_map has
-     * these fields set, incoming sync entry must match with existing.
-     */
-    if (obj_map.obj_size == 0) {
-        obj_map.obj_size = data.obj_len;
-        memcpy(obj_map.obj_id.metaDigest, data.object_id.digest.data(),
-                sizeof(obj_map.obj_id.metaDigest));
-    } else {
-        fds_assert(obj_map.obj_size == static_cast<uint32_t>(data.obj_len));
-        fds_assert(memcmp(obj_map.obj_id.metaDigest, data.object_id.digest.data(),
-                sizeof(obj_map.obj_id.metaDigest)) == 0);
-    }
-
-    fds_assert(sync_data.mod_ts <= static_cast<uint64_t>(data.modification_ts));
-
-    /* Creation time */
-    sync_data.born_ts = data.born_ts;
-
-    /* Modification time */
-    sync_data.mod_ts = data.modification_ts;
-
-    /* association entries */
-    sync_data.assoc_entries.clear();
-    for (auto itr : data.associations) {
-        obj_assoc_entry_t e;
-        e.ref_cnt = itr.ref_cnt;
-        e.vol_uuid = itr.vol_id.uuid;
-        sync_data.assoc_entries.push_back(e);
-    }
-}
-
-/**
- * Merges sync data with current metadata.  This is done after
- * sync transfer.
- */
-void ObjMetaData::mergeNewAndUnsyncedData()
-{
-    fds_assert(syncDataExists());
-    /* We will resolve one field at a time*/
-
-#if 0
-    if (sync_data.mod_ts >= obj_map.assoc_mod_time) {
-        /* Sync metadata is more recent compared to object metadata.
-         * Clear sync-able fields.  We'll just use them from sync entry
-         * See merge code after this if statement.
-         */
-        obj_map.obj_create_time = 0;
-        obj_map.assoc_mod_time = 0;
-        obj_map.obj_num_assoc_entry = 0;
-        assoc_entry.clear();
-        obj_map.obj_refcnt = 0;
-
-        /* Merge happens below */
-    }
-#endif
-
-    /* Creation timestamp.  We will honor the one from sync entry */
-    obj_map.obj_create_time = sync_data.born_ts;
-
-    /* Modification timestamp.  We'll use the recent of two */
-    obj_map.assoc_mod_time = (sync_data.mod_ts > obj_map.assoc_mod_time) ?
-            sync_data.mod_ts : obj_map.assoc_mod_time;
-
-    /* Association entries.  We will merge these */
-    mergeAssociationArrays_();
-
-    /* Other adjustments/accounting of obj_map based on above changes */
-    obj_map.obj_num_assoc_entry = assoc_entry.size();
-    obj_map.obj_refcnt = 0;
-    for (auto e : assoc_entry)  {
-        obj_map.obj_refcnt += e.ref_cnt;
-    }
-    /* Sync data isn't needed anymore */
-    obj_map.obj_flags &= ~SYNCMETADATA_MASK;
-    sync_data.reset();
-}
-
-/**
- * Less than operator
- */
-struct obj_assoc_entryLess {
-    bool operator() (const obj_assoc_entry_t &a, const obj_assoc_entry_t& b)
-    {
-        return a.vol_uuid < b.vol_uuid;
-    }
-};
-
-/**
- * Merges the sync association array with current metadata association array
- * This is done after sync transfer completes
- */
-void ObjMetaData::mergeAssociationArrays_()
-{
-    if (sync_data.assoc_entries.size() == 0) {
-        /* nothing to do */
-        return;
-    } else if (assoc_entry.size() == 0) {
-        assoc_entry = sync_data.assoc_entries;
-        return;
-    }
-    std::sort(sync_data.assoc_entries.begin(),
-            sync_data.assoc_entries.end(), obj_assoc_entryLess());
-    std::sort(assoc_entry.begin(), assoc_entry.end(), obj_assoc_entryLess());
-
-    // TODO(Rao): consider doing a move here
-    auto temp = assoc_entry;
-    assoc_entry.clear();
-
-    auto itr1 = sync_data.assoc_entries.begin();
-    auto itr2 = temp.begin();
-    while (itr1 != sync_data.assoc_entries.end() &&
-           itr2 != temp.end()) {
-        obj_assoc_entry_t e;
-        if (itr1->vol_uuid == itr2->vol_uuid) {
-            e = *itr1;
-            e.ref_cnt += itr2->ref_cnt;
-            ++itr1;
-            ++itr2;
-        } else if (itr1->vol_uuid < itr2->vol_uuid) {
-            e = *itr1;
-            ++itr1;
-        } else {
-            e = *itr2;
-            ++itr2;
-        }
-        assoc_entry.push_back(e);
-    }
-    if (itr1 == sync_data.assoc_entries.end() &&
-        itr2 == temp.end()) {
-        return;
-    } else if (itr1 != sync_data.assoc_entries.end()) {
-        assoc_entry.insert(assoc_entry.end(), itr1, sync_data.assoc_entries.end());
-    } else {
-        assoc_entry.insert(assoc_entry.end(), itr2, temp.end());
-    }
-}
 
 bool ObjMetaData::dataPhysicallyExists() const
 {
@@ -1033,22 +690,12 @@ bool ObjMetaData::dataPhysicallyExists() const
     return false;
 }
 
-void ObjMetaData::setSyncMask()
-{
-    obj_map.obj_flags |= SYNCMETADATA_MASK;
-}
-
 void ObjMetaData::setObjCorrupted() {
     obj_map.obj_flags |= OBJ_FLAG_CORRUPTED;
 }
 
 fds_bool_t ObjMetaData::isObjCorrupted() const {
     return (obj_map.obj_flags & OBJ_FLAG_CORRUPTED);
-}
-
-bool ObjMetaData::syncDataExists() const
-{
-    return (obj_map.obj_flags & SYNCMETADATA_MASK) != 0;
 }
 
 /**
