57e29c576fd412e440912591c29bbbbfa91acb01
Fabrice Medio <fmedio@gmail.com>
2015-02-04 15:22:11 -0800

    Remove Netty dep

--- source/java/src/com/formationds/spike/VertX.java
+++ source/java/src/com/formationds/spike/VertX.java
@@ -1,12 +1,14 @@
 
 package com.formationds.spike;
+
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.VertxFactory;
 import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.http.HttpServerResponse;
 import org.vertx.java.platform.Verticle;
 
-import java.util.Map;
 import java.util.Random;
 
 public class VertX extends Verticle {
@@ -19,8 +21,8 @@ public class VertX extends Verticle {
     public static void main(String[] args) {
         new VertX().start();
     }
-
     public void start() {
+        Vertx vertx = VertxFactory.newVertx();
         vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
             public void handle(HttpServerRequest req) {
                 HttpServerResponse response = req.response();
