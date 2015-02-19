15862f33f01651b2db24cf559679d92a33d6ca68
Luke Guildner <luke@formationds.com>
2015-02-19 09:57:35 -0800

    Final changes to Test300VolsObj.py testcase.

--- source/test/integration-framework/testsets/testcases/Test300VolsObj.py
+++ source/test/integration-framework/testsets/testcases/Test300VolsObj.py
@@ -10,6 +10,9 @@ import requests
 import json
 import testsets.testcase as testcase
 
+from boto.s3.key import Key
+from boto.s3.bucket import Bucket
+
 class TestCreateThreehundredObjectVolumes(testcase.FDSTestCase):
 
     '''
@@ -40,10 +43,15 @@ class TestCreateThreehundredObjectVolumes(testcase.FDSTestCase):
             header = {'FDS-Auth' : userToken}
             self.log.info("header = %s", header)
 
+            #Get the s3 connection from the testcase parameters
+            conn = self.parameters['s3'].get_s3_connection()
+
             #Get number of volumes currently?
 
-            #
             for i in range(0, 300):
+
+                #bucket name
+                bucket_name = "test-" + str(i).zfill(3) + "_300"
 
                 #prep data
                 data = {"sla":0,"limit":0,"priority":10,"snapshotPolicies":[],
@@ -51,8 +59,8 @@ class TestCreateThreehundredObjectVolumes(testcase.FDSTestCase):
                         {"retention":1209600,"recurrenceRule":{"FREQ":"WEEKLY"}},{"retention":5184000,
                         "recurrenceRule":{"FREQ":"MONTHLY"}},{"retention":31622400,
                         "recurrenceRule":{"FREQ":"YEARLY"}}],"commit_log_retention":86400,
-                        "data_connector":{"type":"OBJECT","api":"S3, Swift"},"name":"m-"
-                        + str(i).zfill(3) + "_300", "mediaPolicy":"HDD_ONLY"}
+                        "data_connector":{"type":"OBJECT","api":"S3, Swift"},"name":bucket_name,
+                        "mediaPolicy":"HDD_ONLY"}
 
                 json_data = json.dumps(data)
 
@@ -65,9 +73,23 @@ class TestCreateThreehundredObjectVolumes(testcase.FDSTestCase):
                 #Check return code
                 self.assertTrue(200 == r.status_code)
 
-                #Write to volume
+                #Get the bucket via boto
+                #bucket = conn.lookup(bucket_name)
+                #self.assertTrue(bucket != None)
+                #self.log.info("bucket = %s", bucket)
+
+                #Write to the volume
+                #k = Key(bucket)
+                #self.log.info("key = %s", k)
+                #k.key = "test"
+                #in_string = "This is a test."
+                #k.set_contents_from_string = in_string
 
                 #Read from volume
+                #self.log.info("keys = %s", bucket.get_all_keys())
+                #k = bucket.get_key("test")
+                #out_string = k.get_contents_as_string()
+                #self.assertTrue(in_string == out_string)
 
             #Get Volumes
             r = requests.get(url, headers=header)
