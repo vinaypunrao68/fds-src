0f39d86c4705af4fa89da40a093be035c0d36c66
Matteo Monchiero <matteo@formationds.com>
2015-02-26 15:06:17 -0800

    Fixing nbd_setup.sh.. need to ssh to run fdscli

--- source/test/perf/dell_test/nbd_setup.sh
+++ source/test/perf/dell_test/nbd_setup.sh
@@ -1,6 +1,6 @@
-/fds/bin/fdscli --volume-create blockVolume -i 1 -s 10240 -p 50 -y blk
+ssh perf1-node1 '/fds/bin/fdscli --volume-create blockVolume -i 1 -s 10240 -p 50 -y blk'
 sleep 10
-/fds/bin/fdscli --volume-modify blockVolume -s 10240 -g 0 -m 0 -r 10
+ssh perf1-node1 '/fds/bin/fdscli --volume-modify blockVolume -s 10240 -g 0 -m 0 -r 10'
 sleep 10
-../../../cinder/nbdadm.py attach c3po blockVolume
+../../../cinder/nbdadm.py attach perf1-node1 blockVolume
 
