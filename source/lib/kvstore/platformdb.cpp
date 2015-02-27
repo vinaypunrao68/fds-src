bb1f7b9c672589755b59c2f33e21e28298addd67
Premkumar <prem@formationds.com>
2015-02-27 15:30:23 -0800

    store/retrieve funcs for platform db

--- source/lib/kvstore/platformdb.cpp
+++ source/lib/kvstore/platformdb.cpp
@@ -17,5 +17,56 @@ PlatformDB::~PlatformDB() {
     LOGNORMAL << "destroying platformdb";
 }
 
+bool PlatformDB::setNodeInfo(const fpi::NodeInfo& nodeInfo) {
+    try {
+        boost::shared_ptr<std::string> serialized;
+        fds::serializeFdspMsg(nodeInfo, serialized);
+        r.set("node.info", *serialized);
+    } catch(const RedisException& e) {
+        LOGCRITICAL << "error with redis " << e.what();
+        return false;
+    }
+    return true;
+}
+
+bool PlatformDB::getNodeInfo(fpi::NodeInfo& nodeInfo) {
+    try {
+        Reply reply = r.get("node.info");
+        if (reply.isNil()) return false;
+        std::string value = reply.getString();
+        fds::deserializeFdspMsg(value, nodeInfo);
+    } catch(const RedisException& e) {
+        LOGCRITICAL << "error with redis " << e.what();
+        return false;
+    }
+    return true;
+}
+
+bool setNodeDiskCapability(const fpi::FDSP_AnnounceDiskCapability& diskCapability) {
+    try {
+        boost::shared_ptr<std::string> serialized;
+        fds::serializeFdspMsg(diskCapability, serialized);
+        r.set("node.disk.capability", *serialized);
+    } catch(const RedisException& e) {
+        LOGCRITICAL << "error with redis " << e.what();
+        return false;
+    }
+    return true;
+}
+
+bool getNodeDiskCapability(fpi::FDSP_AnnounceDiskCapability& diskCapability) {
+    try {
+        Reply reply = r.get("node.disk.capability");
+        if (reply.isNil()) return false;
+        std::string value = reply.getString();
+        fds::deserializeFdspMsg(value, diskCapability);
+    } catch(const RedisException& e) {
+        LOGCRITICAL << "error with redis " << e.what();
+        return false;
+    }
+    return true;
+
+}
+
 }  // namespace kvstore
 }  // namespace fds
