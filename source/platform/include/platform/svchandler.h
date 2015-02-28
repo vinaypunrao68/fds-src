d51e18c2eda9c7cdddd52cd23f4fdea2bf59504f
Premkumar <prem@formationds.com>
2015-02-27 20:43:34 -0800

    handlers for pm apis

---
+++ source/platform/include/platform/svchandler.h
@@ -0,0 +1,15 @@
+
+#ifndef SOURCE_PLATFORM_INCLUDE_PLATFORM_SVCHANDLER_H_
+#define SOURCE_PLATFORM_INCLUDE_PLATFORM_SVCHANDLER_H_
+#include <fdsp/fds_service_types.h>
+#include <net/PlatNetSvcHandler.h>
+namespace fds {
+namespace pm {
+class SvcHandler : public PlatNetSvcHandler {
+  public:
+    explicit SvcHandler(CommonModuleProviderIf *provider);
+};
+}  // namespace pm
+}  // namespace fds
+
+#endif // SOURCE_PLATFORM_INCLUDE_PLATFORM_SVCHANDLER_H_
