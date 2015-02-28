1e228ed01c8c6a56846b446458095ce8313716a1
Premkumar <prem@formationds.com>
2015-02-27 20:50:26 -0800

    setting up new pm namespace

--- source/platform/source/process.cpp
+++ source/platform/source/process.cpp
@@ -8,7 +8,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
-#include <process.h>
+#include <platform/process.h>
 #include <vector>
 #include <cstdio>
 
@@ -16,150 +16,150 @@
 #include "shared/fds_types.h"
 
 namespace fds {
-    int fds_get_fd_limit(void) {
-        fds_int64_t      slim, rlim;
-        struct rlimit    rl;
+namespace pm {
+
+int fds_get_fd_limit(void) {
+    fds_int64_t      slim, rlim;
+    struct rlimit    rl;
     
-        slim = sysconf(_SC_OPEN_MAX);
+    slim = sysconf(_SC_OPEN_MAX);
     
-        if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
-            rlim = 0;
-        } else {
-            rlim = rl.rlim_max;
-        }
-    
-        if (slim > rlim) {
-            return (slim);
-        }
-    
-        if (slim < 0) {
-            return (10000);
-        }
-    
-        return (rlim);
+    if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
+        rlim = 0;
+    } else {
+        rlim = rl.rlim_max;
     }
     
-    /*
-     * fds_spawn
-     * ---------
-     */
-    pid_t fds_spawn(char *const argv[], int daemonize) {
-        int      flim, fd, res;
-        pid_t    child_pid;
-        pid_t    res_pid;
+    if (slim > rlim) {
+        return (slim);
+    }
     
-        child_pid = fork();
+    if (slim < 0) {
+        return (10000);
+    }
     
-        if (child_pid != 0) {
-            if (daemonize) {
-                res_pid = waitpid(child_pid, NULL, 0);
-                // TODO(bao): check for 0 and -1
-                fds_assert(res_pid == child_pid);
-                return (0);
-            } else {
-                return (child_pid);
-            }
-        }
+    return (rlim);
+}
     
-        /* Child process, close all file descriptors. */
-        flim = fds_get_fd_limit();
-        printf("Close fd up to %d\n", flim);
+/*
+ * fds_spawn
+ * ---------
+ */
+pid_t fds_spawn(char *const argv[], int daemonize) {
+    int      flim, fd, res;
+    pid_t    child_pid;
+    pid_t    res_pid;
     
-        for (fd = 0; fd < flim; fd++) {
-            close(fd);
-        }
-   
-        // There is probably a better way, but for now, create a dummy variable to capture the
-        // return value from dup().  This prevents a compiler warning when compiling with -O2 
-        fd = open("/dev/null", O_RDWR);  // will be 0
-        int unused_discard = dup(fd); // will be 1
-        unused_discard = dup(fd);     // will be 2
+    child_pid = fork();
     
+    if (child_pid != 0) {
         if (daemonize) {
-            res = daemon(1, 1);
-    
-            if (res != 0) {
-                printf("Fatal error, can't daemonize %s\n", argv[0]);
-                abort();
-            }
+            res_pid = waitpid(child_pid, NULL, 0);
+            // TODO(bao): check for 0 and -1
+            fds_assert(res_pid == child_pid);
+            return (0);
+        } else {
+            return (child_pid);
         }
-    
-        /* actual child process */
-        execvp(argv[0], argv);
-        printf("Fatal error, can't spawn %s\n", argv[0]);
-        abort();
     }
-     
-    pid_t fds_spawn_service(const std::string& prog,
-                            const std::string& fds_root,
-                            const std::vector<std::string>& args,
-                            int daemonize) {
-        std::vector<const char*>    c_args;
-        for (const auto& arg : args) {
-            c_args.push_back(arg.c_str());
-        }
-        c_args.push_back(NULL);
-        return fds_spawn_service(prog.c_str() , fds_root.c_str(), &c_args[0], daemonize);
+    
+    /* Child process, close all file descriptors. */
+    flim = fds_get_fd_limit();
+    printf("Close fd up to %d\n", flim);
+    
+    for (fd = 0; fd < flim; fd++) {
+        close(fd);
     }
-    /*
-     * fds_spawn_service
-     * -----------------
-     */
-    pid_t fds_spawn_service(const char *prog,
-                            const char *fds_root,
-                            const char** extra_args,
-                            int daemonize) {
-        size_t    len, ret;
-        char      exec[1024];
-        char      root[1024];
-        char     *argv[12];
-        int       i = 0;
-        int       j = 0;
-        argv[i++] = exec;
-        argv[i++] = root;
-        for (; extra_args[j] != NULL && j < 10; i++, j++)
-        {
-            argv[i] = (char*) extra_args[j];
-        }
+   
+    // There is probably a better way, but for now, create a dummy variable to capture the
+    // return value from dup().  This prevents a compiler warning when compiling with -O2 
+    fd = open("/dev/null", O_RDWR);  // will be 0
+    int unused_discard = dup(fd); // will be 1
+    unused_discard = dup(fd);     // will be 2
     
-        /* Only allow 10 args for now */
-        fds_verify(extra_args[j] == NULL);
-        argv[i] = NULL;
+    if (daemonize) {
+        res = daemon(1, 1);
     
-        if (getcwd(exec, sizeof (exec)) == NULL)
-        {
-            perror("Fatal from getcwd()");
+        if (res != 0) {
+            printf("Fatal error, can't daemonize %s\n", argv[0]);
             abort();
         }
-    
-        len = strlen(exec);
-        ret = snprintf(exec + len, sizeof (exec) - len, "/%s", prog);
-    
-        if (ret == (sizeof (exec) - len))
-        {
-            printf("The exec path name is too long: %s\n", exec);
-            exit(1);
-        }
-    
-        ret = snprintf(root, sizeof (root), "--fds-root=%s", fds_root);
-    
-        if (ret >= sizeof (root))
-        {
-            printf("The fds-root path name is too long: %s\n", root);
-            exit(1);
-        }
-    
-        /*
-         * XXX(Vy): we're using fds_root as prefix to config DB, strip out the
-         * ending '/' so that the child process can use the correct key.
-         */
-        for (ret--; ret > 0 && root[ret] == '/'; ret--)
-        {
-            ;
-        }
-        root[ret + 1] = '\0';
-    
-        printf("Spawn %s %s\n", exec, root);
-        return (fds_spawn(argv, daemonize));
     }
+    
+    /* actual child process */
+    execvp(argv[0], argv);
+    printf("Fatal error, can't spawn %s\n", argv[0]);
+    abort();
+}
+     
+pid_t fds_spawn_service(const std::string& prog,
+                        const std::string& fds_root,
+                        const std::vector<std::string>& args,
+                        bool daemonize) {
+    std::vector<const char*>    c_args;
+    for (const auto& arg : args) {
+        c_args.push_back(arg.c_str());
+    }
+    c_args.push_back(NULL);
+    return fds_spawn_service(prog.c_str() , fds_root.c_str(), &c_args[0], daemonize?1:0);
+}
+/*
+ * fds_spawn_service
+ * -----------------
+ */
+pid_t fds_spawn_service(const char *prog,
+                        const char *fds_root,
+                        const char** extra_args,
+                        int daemonize) {
+    size_t    len, ret;
+    char      exec[1024];
+    char      root[1024];
+    char     *argv[12];
+    int       i = 0;
+    int       j = 0;
+    argv[i++] = exec;
+    argv[i++] = root;
+    for (; extra_args[j] != NULL && j < 10; i++, j++)
+    {
+        argv[i] = (char*) extra_args[j];
+    }
+    
+    /* Only allow 10 args for now */
+    fds_verify(extra_args[j] == NULL);
+    argv[i] = NULL;
+    
+    if (getcwd(exec, sizeof (exec)) == NULL)
+    {
+        perror("Fatal from getcwd()");
+        abort();
+    }
+    
+    len = strlen(exec);
+    ret = snprintf(exec + len, sizeof (exec) - len, "/%s", prog);
+    
+    if (ret == (sizeof (exec) - len))
+    {
+        printf("The exec path name is too long: %s\n", exec);
+        exit(1);
+    }
+    
+    ret = snprintf(root, sizeof (root), "--fds-root=%s", fds_root);
+    
+    if (ret >= sizeof (root))
+    {
+        printf("The fds-root path name is too long: %s\n", root);
+        exit(1);
+    }
+    
+    /*
+     * XXX(Vy): we're using fds_root as prefix to config DB, strip out the
+     * ending '/' so that the child process can use the correct key.
+     */
+    for (ret--; ret > 0 && root[ret] == '/'; ret--) {}
+    root[ret + 1] = '\0';
+    
+    printf("Spawn %s %s\n", exec, root);
+    return (fds_spawn(argv, daemonize));
+}
+}  // namespace pm
 }  // namespace fds
