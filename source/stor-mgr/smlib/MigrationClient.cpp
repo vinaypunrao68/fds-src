c768d912fc1de46db5d7c65eb396613758dcdda1
Anna Povzner <anna@formationds.com>
2015-03-05 14:46:48 -0800

    FS-1228 fixed mismatch of thrift message ID and type.

--- source/stor-mgr/smlib/MigrationClient.cpp
+++ source/stor-mgr/smlib/MigrationClient.cpp
@@ -777,7 +777,7 @@ MigrationClient::handleMigrationError(const Error& error) {
                    << " reporting to OM to abort token migration";
         fpi::CtrlTokenMigrationAbortPtr msg(new fpi::CtrlTokenMigrationAbort());
         auto req = gSvcRequestPool->newEPSvcRequest(gl_OmUuid.toSvcUuid());
-        req->setPayload(FDSP_MSG_TYPEID(fpi::CtrlSvcEvent), msg);
+        req->setPayload(FDSP_MSG_TYPEID(fpi::CtrlTokenMigrationAbort), msg);
         req->invoke();
     }
 }
