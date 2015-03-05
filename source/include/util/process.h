a8f5440fff3a60d5b8bc9aac72112d9f968507f7
Premkumar <prem@formationds.com>
2015-03-05 03:09:16 -0800

    process utils

---
+++ source/include/util/process.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2013 Formation Data Systems, Inc.
+ */
+
+#ifndef SOURCE_UTIL_PROCESS_H_
+#define SOURCE_UTIL_PROCESS_H_
+
+#include <util/Log.h>
+namespace fds {
+namespace util {
+
+void printBackTrace();
+
+} // namespace util
+} // namespace fds
+
+#endif // SOURCE_UTIL_PROCESS_H_
