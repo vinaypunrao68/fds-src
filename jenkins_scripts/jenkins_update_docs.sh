345b33aaa4455dcbaae9f96bef2d285804133742
Andrew Leung <andrew@formationds.com>
2015-03-09 16:16:12 -0700

    FS-958 Fix path in Jenkins script.

--- jenkins_scripts/jenkins_update_docs.sh
+++ jenkins_scripts/jenkins_update_docs.sh
@@ -10,7 +10,7 @@ function message
 function gen_api_docs
 {
    message "GENERATE API THRIFT DOCUMENTS"
-   ../Build/linux-x86_64.debug/bin/thrift --gen html -r ../source/fdsp/doc_api.thrift
+   ./Build/linux-x86_64.debug/bin/thrift --gen html -r source/fdsp/doc_api.thrift
 }
 
 function copy_to_webserver
