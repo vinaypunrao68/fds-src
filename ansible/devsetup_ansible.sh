9a338ce1ff27c61a3a417ea10a13aa0f651bff08
Aaron Nichols <aaron@formationds.com>
2015-03-14 11:47:06 -0600

    add -H to sudo call to pip to fix errors

--- ansible/devsetup_ansible.sh
+++ ansible/devsetup_ansible.sh
@@ -23,7 +23,7 @@ case $? in
     0)
         if [ ! -f /usr/local/bin/ansible-playbook ]; then
             sudo apt-get install -y --force-yes python-pip python-dev
-            sudo pip install -q ansible
+            sudo -H pip install -q ansible
         fi
 
         rm -f ${script_dir}/.devsetup-is-up-to-date
