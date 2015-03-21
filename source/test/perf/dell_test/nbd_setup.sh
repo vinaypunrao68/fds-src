f4753f5ca24970345c6ef9e7c66377c49339729d
Matteo Monchiero <matteo@formationds.com>
2015-03-21 11:39:36 -0700

    More fixes

--- source/test/perf/dell_test/nbd_setup.sh
+++ source/test/perf/dell_test/nbd_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 node=$1
 echo "Setting up nbd on $node"
-ssh $node 'bash -c "pushd /fds/sbin && ./fdsconsole.py accesslevel debug && ./fdsconsole.py volume create blockVolume --vol-type block --blk-dev-size 10485760 && popd"'
+pushd /fds/sbin && ./fdsconsole.py accesslevel debug && ./fdsconsole.py volume create blockVolume --vol-type block --blk-dev-size 10485760 && popd
 sleep 10
-ssh $node 'bash -c "pushd /fds/sbin && ./fdsconsole.py volume modify blockVolume --minimum 0 --maximum 0 --priority 1 && popd"'
+pushd /fds/sbin && ./fdsconsole.py volume modify blockVolume --minimum 0 --maximum 0 --priority 1 && popd
 sleep 10
 disk=`../../../cinder/nbdadm.py attach $node blockVolume`
 echo $disk > .disk
