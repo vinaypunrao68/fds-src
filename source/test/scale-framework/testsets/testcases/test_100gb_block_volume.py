180ea228b765c1ef90cab396bc3a0a4b63a0bddc
Philippe Ribeiro <philippe@formationds.com>
2015-03-23 11:49:07 -0700

    Test100GBVolume separated into two test cases
    * First test create a block volume and tries to upload 100GB of data
    * Second test creates a S3 volume and tried to upload 100GB of data

--- source/test/scale-framework/testsets/testcases/test_100gb_block_volume.py
+++ source/test/scale-framework/testsets/testcases/test_100gb_block_volume.py
@@ -18,63 +18,18 @@ from boto.s3.key import Key
 import testsets.testcase as testcase
 
 
-class Test100GBVolume(testcase.FDSTestCase):
+class Test100GBBlockVolume(testcase.FDSTestCase):
     '''
     Create one block volume with 100GB capacity, and populate first with 100GB;
     After that, add more 2GB of data, to ensure correctness.
     '''
     def __init__(self, parameters=None, config_file=None, om_ip_address=None):
-        super(Test100GBVolume, self).__init__(parameters=parameters,
+        super(Test100GBBlockVolume, self).__init__(parameters=parameters,
                                                config_file=config_file,
                                                om_ip_address=om_ip_address)
     
     def runTest(self):
-        self.upload_s3_volume()
-    
-    def upload_s3_volume(self):
-        utils.create_dir(config.TEST_DIR)
-        # utils.create_dir(config.DOWNLOAD_DIR)
-        s3conn = utils.create_s3_connection(self.om_ip_address, self.om_ip_address)
-        bucket_name = "volume_100gb"
-        bucket = s3conn.conn.create_bucket(bucket_name)
-        if bucket == None:
-            raise Exception("Invalid bucket.")
-            # We won't be waiting for it to complete, short circuit it.
-            self.test_passed = False
-            self.reportTestCaseResult(self.test_passed)
-        self.log.info("Volume %s created..." % bucket.name)
-        # Store all the files created to the bucket
-        self.store_file_to_volume(bucket)
-        # self.log.info("Trying to Download the blobs now.")
-        # self.download_files(bucket)
-        # self.check_files_hash()
-        # Delete the bucket
-        # self.destroy_volume(bucket, s3conn)
-        # remove the existing file
-        utils.remove_dir(config.DOWNLOAD_DIR)
-        
-    def store_file_to_volume(self, bucket):
-        '''
-        Given the list of files to be uploaded, presented in sample_files list,
-        upload them to the corresponding volume
-        
-        Attributes:
-        -----------
-        bucket : bucket
-            the S3 bucket (volume) where the data files will to uploaded to.
-        '''
-        # add the data files to the bucket.
-        k = Key(bucket)
-        sample = "test_sample_200M"
-        path = os.path.join(config.TEST_DIR, sample)
-        self.log.info("Uploading: %s" % path)
-        if os.path.exists(path):
-            k.key = sample
-            k.set_contents_from_filename(path,
-                                         cb=utils.percent_cb,
-                                         num_cb=10)
-            self.log.info("Uploaded file %s to bucket %s" % 
-                         (sample, bucket.name))
+        self.upload_block_volume()
                 
     def upload_block_volume(self):
         test_passed = False
@@ -99,7 +54,7 @@ class Test100GBVolume(testcase.FDSTestCase):
                 test_passed = False
     
             #Get number of volumes currently?
-            volume_name = "test-block-100GB"
+            volume_name = "test-block-100GB-block-volume"
             #prep data
             data = {"sla":0,"limit":0,"priority":10,"snapshotPolicies":[],
                     "timelinePolicies":[{"retention":604800,
@@ -138,6 +93,7 @@ class Test100GBVolume(testcase.FDSTestCase):
                     'mv sample_file /fdsmount',
                     'umount /fdsmount',
                     'rm -rf /fdsmount',
+                    'rm -rf sample_file',
                     './nbdadm.py detach %s' % (volume_name),
                 )
                 
