63638953ee5783fa8d952333d3862797dab44058
Anna Povzner <anna@formationds.com>
2015-03-23 18:41:17 -0700

    FS-879 fixed few more places using old ports

--- source/access-mgr/test/AmFunctionalTest.cpp
+++ source/access-mgr/test/AmFunctionalTest.cpp
@@ -66,8 +66,8 @@ class AmLoadProc : public boost::enable_shared_from_this<AmLoadProc>,
             : domainName(new std::string("Test Domain")),
               volumeName(new std::string("Test Volume")),
               serverIp("127.0.0.1"),
-              serverPort(8899),
-              responsePort(9876) {
+              serverPort(7025),
+              responsePort(7053) {
         // register and populate volumes
         VolumeDesc volDesc(*volumeName, 5);
         volDesc.iops_min = 0;
