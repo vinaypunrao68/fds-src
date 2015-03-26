7002e00fc82d3243d1d64b39f62b98f9644bc877
James LaMar <james@formationds.com>
2015-03-25 17:24:08 -0700

    [fs-1449] add test for the nbdadmd.py rest server

--- source/cinder/fds_common_tests.py
+++ source/cinder/fds_common_tests.py
@@ -12,6 +12,7 @@ import uuid
 am_endpoint = ("localhost", 9988)
 cs_endpoint = ("localhost", 9090)
 nbd_endpoint = ("localhost", 10809)
+nbdadmd_endpoint = ("localhost", 10511)
 
 
 class TestExecutor:
@@ -57,11 +58,24 @@ class TestFdsCommonOpenstack(unittest.TestCase):
     @contextmanager
     def mount(self, dev):
         tempdir = tempfile.mkdtemp()
-        print "mount {0} {1}".format(dev, tempdir)
         self.executor("mount", dev, tempdir)
         yield tempdir
         self.executor("umount", tempdir)
         shutil.rmtree(tempdir)
+
+    @contextmanager
+    def runNbdRestServer(self):
+        nbdadmd = os.path.abspath("nbdadmd.py")
+        process = Popen(nbdadmd, stdout=PIPE, stderr=PIPE)
+        sleep(1)
+        yield "http://" + nbdadmd_endpoint[0] + ":" + str(nbdadmd_endpoint[1])
+        process.kill()
+
+    @contextmanager
+    def useRemote(self, url, volume):
+        yield self.nbd_mgr.attach_nbd_remote(url, self.nbd_server, volume)
+        self.nbd_mgr.detach_nbd_remote(url, self.nbd_server, volume)
+
 
     def randomBits(self, length):
         with open("/dev/urandom") as random:
@@ -105,6 +119,12 @@ class TestFdsCommonOpenstack(unittest.TestCase):
                     with open(hello_file) as hello_handle:
                         read_line = hello_handle.readline()
                         self.assertEqual(hello_output, read_line)
+
+    def testRemote(self):
+        with self.volume(1) as vol:
+            with self.runNbdRestServer() as url:
+                with self.useRemote(url, vol) as nbd_device:
+                    pass
 
 if __name__ == '__main__':
     unittest.main()
