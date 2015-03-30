e86ab2e4f1eab255df228c170164b8f3e11eb728
Matteo Monchiero <matteo@formationds.com>
2015-03-30 16:02:19 -0700

    Tiering perf test scripts

--- source/test/perf/tiering_test/test.sh
+++ source/test/perf/tiering_test/test.sh
@@ -2,6 +2,9 @@
 outdir=$1
 tools=/fds/sbin
 policies="hdd ssd hybrid"
+# policies="hybrid"
+
+machines="luke han c3po chewie"
 
 pushd $tools
 ./fdsconsole.py accesslevel admin
@@ -9,13 +12,14 @@ popd
 
 for p in $policies ; do
     pushd $tools
-    ./fdsconsole.py volume create volume_$p --vol-type block --blk-dev-size 10737418240 --media-policy $p
+    ./fdsconsole.py volume create volume_$p --vol-type block --blk-dev-size 10737418240 --media-policy $p --max-obj-size 4096
     popd
     sleep 5
     disk=`$tools/nbdadm.py attach luke volume_$p`
     fio --name=write --rw=write --filename=$disk --bs=4096 --numjobs=1 --iodepth=32 --ioengine=libaio --direct=1 --size=8g
-    sync
-    echo 3 > /proc/sys/vm/drop_caches
+    for m in $machines ; do
+        ssh $m 'sync && echo 3 > /proc/sys/vm/drop_caches'
+    done
     fio --name=randread --rw=randread --filename=$disk --bs=4096 --numjobs=4 --iodepth=128 --ioengine=libaio --direct=1 --size=8g -time_based -runtime 120 | tee $outdir/out.$p
     sleep 5
 done
