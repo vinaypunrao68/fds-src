528c0b548693a2a46d769e893ce107cca903f795
Matteo Monchiero <matteo@formationds.com>
2015-04-01 16:23:17 -0700

    Error handling

--- source/test/perf/tiering_test/get_results.sh
+++ source/test/perf/tiering_test/get_results.sh
@@ -3,11 +3,14 @@ for f in `ls -d $1` ; do
     iops=`grep iops $f | sed -e 's/[ ,=:]/ /g' | awk '{e+=$7}END{print e}'`
     frequency=`echo $f | awk  -F'[./]' '{print $2}'`
     policy=`echo $f | awk  -F'[./]' '{print $4}'`
-    echo policy=$policy frequency=$frequency iops=$iops
+    latency=`grep clat $f | grep avg| awk -F '[,=:()]' '{print $9}' | awk '{i+=1; e+=$1}END{print e/i}'`
+
+    echo policy=$policy frequency=$frequency iops=$iops latency=$latency
     echo file=$f > .data
     echo iops=$iops >> .data
     echo frequency=$frequency >> .data
     echo policy=$policy >> .data
+    echo latency=$latency >> .data
     ../common/push_to_influxdb.py tiering_test .data
 done
 
