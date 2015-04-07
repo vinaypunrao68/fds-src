74fc6ad1870e98499633876e3a8413afede9669c
Luke Guildner <luke@formationds.com>
2015-04-07 10:21:14 -0700

    More changes for stress test setup.

--- source/test/scale-framework/testsets/testcases/test_stress_setup.py
+++ source/test/scale-framework/testsets/testcases/test_stress_setup.py
@@ -2,14 +2,21 @@
 #
 # Copyright 2014 by Formation Data Systems, Inc.
 
+import ConfigParser
+
 import s3
 import utils
 import config
 import unittest
 import json
 import testsets.testcase as testcase
+import block_volumes
+import s3_volumes
 
 class TestStressSetup(testcase.FDSTestCase):
+
+    logging.basicConfig(level=logging.INFO)
+    log = logging.getLogger(__name__)
 
     '''
     Setup volumes for the stress test
@@ -18,6 +25,16 @@ class TestStressSetup(testcase.FDSTestCase):
     def __init__(self, parameters=None, config_file=None, om_ip_address=None):
         super(TestCreateThreehundredObjectVolumes, self).__init__(parameters=parameters,
                 config_file=config_file, om_ip_address=om_ip_address)
+
+        self.s3_vol_count = 500
+        self.block_vol_count = 500
+        if(config_file != None)
+            self.log.info("config_file = %s", config_file)
+            config = ConfigParser.ConfigParser()
+            config.readfp(open(config_file))
+            self.s3_vol_count = config.getInt(stress, s3_vols)
+
+        self.log.info("s3_vol_count = %s", self.s3_vols_count)
 
     '''
     Do it.
