e877ad13c30c09b6baf4f3ecf244a40285305790
Matteo Monchiero <matteo@formationds.com>
2015-04-15 15:21:10 -0700

    Single node fio regression test

--- source/test/perf/dell_test/run_fio_regr.sh
+++ source/test/perf/dell_test/run_fio_regr.sh
@@ -13,6 +13,28 @@ workloads="randread"
 #workers="4"
 #workloads="randread"
 
+function process_results {
+    local f=$1
+    local numjobs=$2
+    local workload=$3
+    local bs=$4
+    local iodepth=$5
+    local disksize=$6
+
+    iops=`grep iops $f | sed -e 's/[ ,=:]/ /g' | awk '{e+=$7}END{print e}'`
+    latency=`grep clat $f | grep avg| awk -F '[,=:()]' '{print $9}' | awk '{i+=1; e+=$1}END{print e/i}'`
+
+    echo file=$f > .data
+    echo numjobs=$numjobs >> .data
+    echo workload=$workload >> .data
+    echo bs=$bs >> .data
+    echo iodepth=$iodepth >> .data
+    echo disksize=$disksize >> .data
+    echo iops=$iops >> .data
+    echo latency=$latency >> .data
+    ../common/push_to_influxdb.py dell_test .data
+}
+
 if [ ! -e .disk ] ; then
     echo cannot find nbd disk definition file
     exit 1
@@ -27,6 +49,7 @@ for worker in $workers ; do
             #sync
             #echo 3 > /proc/sys/vm/drop_caches
             fio --name=test --rw=$workload --filename=$nbd_disk --bs=$bs --numjobs=$worker --iodepth=$d --ioengine=libaio --direct=1 --size=$size --time_based --runtime=60 | tee $outdir/out.numjobs=$worker.workload=$workload.bs=$bs.iodepth=$d.disksize=$size
+            process_results $outfile $worker $workload $bs $d $size
             done
         done
     done
