ff3a999d92dd722783b41250f92e596db94756dc
Nate Bever <nate@formationds.com>
2015-04-16 15:12:04 -0600

    adding the first phase of create volume to the CLI

--- source/cli/src/fds/model/volume.py
+++ source/cli/src/fds/model/volume.py
@@ -1,12 +1,12 @@
-'''
-Created on Apr 3, 2015
-
-Model object representing a volume
-
-@author: nate
-'''
 class Volume( object ):
     
+    '''
+    Created on Apr 3, 2015
+    
+    Model object representing a volume
+    
+    @author: nate
+    '''
     __id = -1
     __name = ""
     __priority = 7
@@ -18,6 +18,9 @@ class Volume( object ):
     __state = "Inactive"
     __current_size = 10
     __current_units = "GB"
+    __last_performance_firebreak = 0
+    __last_capacity_firebreak = 0
+    __tenant_id = 0
     
     @property
     def id(self):
@@ -85,7 +88,7 @@ class Volume( object ):
         
     @property
     def state(self):
-        self.__state
+        return self.__state
         
     @state.setter
     def state(self, aState):
@@ -106,3 +109,27 @@ class Volume( object ):
     @current_units.setter
     def current_units(self, someUnits):
         self.__current_units = someUnits
+        
+    @property
+    def tenant_id(self):
+        return self.__tenant_id
+    
+    @tenant_id.setter
+    def tenant_id(self, anId):
+        self.__tenant_id = anId
+        
+    @property
+    def last_performance_firebreak(self):
+        return self.__last_performance_firebreak
+    
+    @last_performance_firebreak.setter
+    def last_performance_firebreak(self, aTime):
+        self.__last_performance_firebreak = aTime
+        
+    @property
+    def last_capacity_firebreak(self):
+        return self.__last_capacity_firebreak
+    
+    @last_capacity_firebreak.setter
+    def last_capacity_firebreak(self, aTime):
+        self.__last_capacity_firebreak = aTime
