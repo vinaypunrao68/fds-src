8abb307e47986c44d5af519985fba2b124dd61ae
Matteo Monchiero <matteo@formationds.com>
2015-04-17 16:07:12 -0700

    More tests

--- source/test/perf/s3_test/run_s3_regr.sh
+++ source/test/perf/s3_test/run_s3_regr.sh
@@ -4,12 +4,15 @@
 function s3_setup {
     local node=$1
     echo "Setting up s3 on $node"
-    pushd /fds/sbin && ./fdsconsole.py accesslevel debug && ./fdsconsole.py volume create volume0 --vol-type object && popd
+    pushd /fds/sbin
+    ./fdsconsole.py accesslevel debug
+    ./fdsconsole.py volume create volume0 --vol-type object
+    popd
     sleep 10
 }
-# process_results $outfile $n_reqs $n_files $outs $test_type $object_size $hostname $n_conns $n_jobs
+
 function process_results {
-    local f=$1
+    local files=$1
     local n_reqs=$2
     local n_files=$3
     local outs=$4
@@ -19,8 +22,8 @@ function process_results {
     local n_conns=$8
     local n_jobs=$9
 
-    iops=`grep iops $f | sed -e 's/[ ,=:]/ /g' | awk '{e+=$7}END{print e}'`
-    latency=`grep clat $f | grep avg| awk -F '[,=:()]' '{print $9}' | awk '{i+=1; e+=$1}END{print e/i}'`
+    iops=`echo $files | xargs grep IOPs |awk '{e+=$2}END{print e}'`
+    latency=`echo $files | xargs grep latency | awk '{print $4*1e-6}'|awk '{i+=1; e+=$1}END{print e/i}'`
 
     echo file=$f > .data
     echo n_reqs=$n_reqs >> .data
@@ -31,6 +34,8 @@ function process_results {
     echo hostname=$hostname >> .data
     echo n_conns=$n_conns >> .data
     echo n_jobs=$n_jobs >> .data
+    echo iops=$iops >>.data
+    echo latency=$latency >>.data
     ../common/push_to_influxdb.py s3_test .data
 }
 
@@ -52,16 +57,17 @@ n_conns=100
 n_jobs=4
 
 test_types="GET"
-object_sizes="4096"
-concurrencies="100"
+object_sizes="4096 65536 262144"
+concurrencies="25 100"
 
 s3_setup luke
 
-cd $workspace/source/Build/linux-x86_64.release
-tar czvf java_tools.tgz tools lib/java
-
-scp $workspace/source/Build/linux-x86_64.release/java_tools.tgz $client:/root/java_tools.tgz
-ssh $client 'tar xzvf java_tools.tgz'
+#FIXME: assuming trafficgen is installed on the client
+#pushd $workspace/source/Build/linux-x86_64.release
+#tar czvf java_tools.tgz tools lib/java
+#popd
+#scp $workspace/source/Build/linux-x86_64.release/java_tools.tgz $client:/root/java_tools.tgz
+#ssh $client 'tar xzvf java_tools.tgz'
 
 echo "loading dataset"
 cmd="cd /root/tools; ./trafficgen --n_reqs 20000 --n_files 1000 --outstanding_reqs 50 --test_type PUT --object_size 4096 --hostname luke --n_conns 50"
@@ -80,13 +86,16 @@ for t in $test_types ; do
             cmd="cd /root/tools; ./trafficgen --n_reqs $n_reqs --n_files $n_files --outstanding_reqs $outs --test_type $test_type --object_size $object_size --hostname $hostname --n_conns $n_conns"
 
             pids=""
-            for j in `seq $njobs` ; do
-                ssh $client "$cmd"  | tee $outdir/out.n_reqs=$n_reqs.n_files=$n_files.outstanding_reqs=$outs.test_type=$test_type.object_size=$object_size.hostname=$hostname.n_conns=$nconns.job=$j &
+            outfiles=""
+            for j in `seq $n_jobs` ; do
+                f=$outdir/out.n_reqs=$n_reqs.n_files=$n_files.outstanding_reqs=$outs.test_type=$test_type.object_size=$object_size.hostname=$hostname.n_conns=$n_conns.job=$j
+                outfiles="$outfiles $f"
+                ssh $client "$cmd"  | tee $f &
                 pid="$pid $!"
                 pids="$pids $!"
             done
             wait $pids
-            process_results $outfile $n_reqs $n_files $outs $test_type $object_size $hostname $n_conns $n_jobs
+            process_results "$outfiles" $n_reqs $n_files $outs $test_type $object_size $hostname $n_conns $n_jobs
         done
     done
 done
