357eef71bc9b4c3c1d143d26aa29f99cbb7a98db
Matteo Monchiero <matteo@formationds.com>
2015-04-17 16:24:28 -0700

    Cleanup

--- source/test/perf/dell_test/run_fio_regr.sh
+++ source/test/perf/dell_test/run_fio_regr.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-outdir=$1
-size="16m"
-bsizes="4096"
-iodepths="50"
-workers="8"
-workloads="randread"
+#####################
 
-#bsizes="4096"
-#iodepths="1 2 4 8 16 32 64 128 256"
-#iodepths="32"
-#workers="4"
-#workloads="randread"
+function volume_setup {
+    local max_obj_size=$1
+    local node=$2
+    local vol=$3
+    pushd /fds/sbin 
+    ./fdsconsole.py accesslevel admin 
+    ./fdsconsole.py volume create $vol --vol-type block --blk-dev-size 10485760 --max-obj-size $max_obj_size
+    sleep 10
+    ./fdsconsole.py volume modify $vol --minimum 0 --maximum 0 --priority 1 
+    popd
+    sleep 10
+    nbd_disk=`../../../cinder/nbdadm.py attach $node $vol`
+}
+
+function volume_detach {
+    local vol=$1
+    ../../../cinder/nbdadm.py detach $vol
+}
 
 function process_results {
     local f=$1
@@ -35,22 +43,23 @@ function process_results {
     ../common/push_to_influxdb.py dell_test .data
 }
 
-if [ ! -e .disk ] ; then
-    echo cannot find nbd disk definition file
-    exit 1
-fi
-nbd_disk=`cat .disk`
+#####################
+
+outdir=$1
+size="16m"
+worker=8
+workload=randomread
+bs=4096
+d=50
+
+nbd_disk=""
+volume_setup $bs $node volume_$bs
+echo "nbd disk: $nbd_disk"
+
 fio --name=write --rw=write --filename=$nbd_disk --bs=4096 --numjobs=1 --iodepth=32 --ioengine=libaio --direct=1 --size=$size
 
-for worker in $workers ; do
-    for workload in $workloads ; do
-        for bs in $bsizes ; do
-            for d in $iodepths ; do
-            #sync
-            #echo 3 > /proc/sys/vm/drop_caches
-            fio --name=test --rw=$workload --filename=$nbd_disk --bs=$bs --numjobs=$worker --iodepth=$d --ioengine=libaio --direct=1 --size=$size --time_based --runtime=60 | tee $outdir/out.numjobs=$worker.workload=$workload.bs=$bs.iodepth=$d.disksize=$size
-            process_results $outfile $worker $workload $bs $d $size
-            done
-        done
-    done
-done
+#sync
+#echo 3 > /proc/sys/vm/drop_caches
+fio --name=test --rw=$workload --filename=$nbd_disk --bs=$bs --numjobs=$worker --iodepth=$d --ioengine=libaio --direct=1 --size=$size --time_based --runtime=60 | tee $outdir/out.numjobs=$worker.workload=$workload.bs=$bs.iodepth=$d.disksize=$size
+process_results $outfile $worker $workload $bs $d $size
+volume_detach volume_$bs
--- source/test/perf/dell_test/s3_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-node=$1
-echo "Setting up s3 on $node"
-pushd /fds/sbin && ./fdsconsole.py accesslevel debug && ./fdsconsole.py volume create volume0 --vol-type object && popd
-sleep 10
