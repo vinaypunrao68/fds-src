74355c3564c6c8b76bff0655f67d8c3ca5097fd4
Brian Szmyd <szmyd@formationds.com>
2015-04-20 13:06:26 -0600

    Revert "Merge branch 'master' of github.com:FDS-Dev/fds-src"
    This reverts commit 6a3ed5e628ea8feb37430bbec496d46975f3fe76, reversing
    changes made to 07cba1a5d22a15c4c3aab6e33509104e309033e5.

--- source/test/testlib/keys.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-# author: Philippe Ribeiro                                                   #
-# email: philippe@formationds                                                #
-# file: keys.py                                                              #
-##############################################################################
-AWS_USER = 'fds_testing'
-AWS_PASSWORD = 'bIEYKCPzDKtQDDBomnAKTKRA'
-AWS_ACCESS_KEY_ID = ''
-AWS_SECRET_ACCESS_KEY = ''
