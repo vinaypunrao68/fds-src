36d0f79e03c27ae23a88ab00cf104f5701597972
Luke Guildner <luke@formationds.com>
2015-04-21 23:54:22 -0700

    Adding stress test clean up. And some fixes to stress test setup as well as fixes to the s3 and block volume libraries.

--- source/test/scale-framework/testsets/testcases/test_stress_cleanup.py
+++ source/test/scale-framework/testsets/testcases/test_stress_cleanup.py
@@ -14,7 +14,7 @@ import block_volumes
 import s3_volumes
 import logging
 
-class TestStressSetup(testcase.FDSTestCase):
+class TestStressCleanup(testcase.FDSTestCase):
 
     logging.basicConfig(level=logging.INFO)
     log = logging.getLogger(__name__)
@@ -24,7 +24,7 @@ class TestStressSetup(testcase.FDSTestCase):
     '''
 
     def __init__(self, parameters=None, config_file=None, om_ip_address=None):
-        super(TestStressSetup, self).__init__(parameters=parameters,
+        super(TestStressCleanup, self).__init__(parameters=parameters,
                 config_file=config_file, om_ip_address=om_ip_address)
 
         self.s3_vol_count = 500
@@ -44,19 +44,23 @@ class TestStressSetup(testcase.FDSTestCase):
     '''
     def runTest(self):
         test_passed = False
+        r = None
+        port = config.FDS_REST_PORT
         s3_vols = s3_volumes.S3Volumes(self.om_ip_address)
         block_vols = block_volumes.BlockVolumes(self.om_ip_address)
 
         try:
 
-            #Setup
-            s3_bucket_list = s3_vols.create_volumes(self.s3_vol_count,
-                    "stress_test_s3")
-            block_vol_list = block_vols.create_volumes(self.block_vol_count,
-                    "stress_test_block", "1", "GB")
+            #Delete Volumes
+            volumes = []
+            for i in xrange(0, self.s3_vol_count):
+                name = "stress_test_s3_" + `i`
+                volumes.append(name)
+            for i in xrange(0, self.block_vol_count):
+                name = "stress_test_block_" + `i`
+                volumes.append(name)
 
-            self.log.info(s3_bucket_list)
-            self.log.info(block_vol_list)
+            block_vols.delete_volumes(volumes)
 
             #Yay?
             test_passed = True
