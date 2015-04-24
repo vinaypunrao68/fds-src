affd3b614f8c250143a8538728976e991a730af9
Philippe Ribeiro <philippe@formationds.com>
2015-04-24 11:20:15 -0700

    EC2 instances can be scale up or down now
    * EC2 cluster can be created using the Domain class
    * new nodes can be added, either incrementally for by bunches
    * user can halt, resume or destroy existing nodes
    * unit test added as well

--- source/test/testlib/domain.py
+++ source/test/testlib/domain.py
@@ -3,16 +3,28 @@
 # email: philippe@formationds                                                #
 # file: ec2.py                                                               #
 ##############################################################################
+import concurrent.futures
+import logging
 import Queue as queue
 import random
 import threading
 import time
 
 import ec2
-import utils.helper as helper
 
 class Domain(object):
     
+    '''
+    Responsible for creating the FDS cluster. User can scale up or down the number of nodes,
+    terminate or halt them.
+    
+    Attributes:
+    -----------
+    name: str
+        the name of the cluster
+    cluster_size: int
+        the number of nodes created. Set the minimum to 1, default to 4
+    '''
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                         datefmt='%m-%d %H:%M')
@@ -20,19 +32,27 @@ class Domain(object):
     
     def __init__(self, name, cluster_size=4):
 
-        self.cluster_size = 0
+        assert cluster_size >= 1
+
         self.name = name
-        self.has_started = False
-        if cluster_size < 1:
-            self.cluster_size=4
-        else:
-            self.cluster_size = cluster_size
-        # in case of a multi OM cluster
-        self.nodes = []
-        self.om_ip_address=[]
+        # create the first single OM node
+        om_node = ec2.EC2(name=name, is_om_node=True)
+        
+        # start the node
+        self.__run_threads([om_node.start_instance])
+        
+        self.om_nodes = [om_node]
+        self.nodes = [om_node]
+        
+        self.cluster_size = 1
+        self.create_ec2_multiple_increment(cluster_size-1)
+        self.has_started = True
         
     
     def check_status(self):
+        '''
+        Check the status of each node in the cluster.
+        '''
         # will make use of the fabric APIs to check each node
         running = []
         halted = []
@@ -48,74 +68,106 @@ class Domain(object):
         self.log.info("%s/%s nodes running" % (len(running), self.cluster_size))
         self.log.info("%s/%s nodes halted" % (len(halted), self.cluster_size))
         self.log.info("%s/%s nodes terminated" % (len(terminated), self.cluster_size))
+    
+    def resume(self):
+        '''
+        Check if the cluster is halted (stopped). If it is, then 
+        resume and bring the nodes up and running again.
+        '''
+        if self.has_started:
+            self.log.warning("Cluster has not been created yet. Nothing to do")
+            return
         
+        q = queue.Queue()
+        targets = [ node.resume_instance for node in self.nodes ]
+        self.__run_threads(targets)
+        self.has_started = True
+        self.check_status()
+        
+    def __run_threads(self, targets):
+        '''
+        Private method for running things in parallel.The target (method) is passed as an object
+        '''
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            future_job = { executor.submit(target): target for target in targets }
+            for future in concurrent.futures.as_completed(future_job):
+                try:
+                    data = future.result()
+                except Exception as exc:
+                    self.log.exception(exc)
+                else:
+                    continue
+            
     def halt(self):
+        '''
+        Check if the cluster is running. If it is, then halt (stop) the nodes. The user can
+        resume and bring the nodes up and running again. This will not destroy the nodes.
+        '''
         if not self.has_started:
             self.log.warning("Cluster has not been created yet. Nothing to do")
             return
         q = queue.Queue()
-        threads = [ threading.Thread(target=node.stop_instance) for node in new_nodes]
-        for thread in threads:
-            thread.start()
-            thread.join()
+        targets = [ node.stop_instance for node in self.nodes ]
+        self.__run_threads(targets)
+        self.has_started = False
+        self.check_status()
 
     def terminate(self):
+        '''
+        Check if the cluster is running. If it is, then 
+        destroy the existing nodes and any volumes attached to them.
+        '''
         if not self.has_started:
             self.log.warning("Cluster has not been created yet. Nothing to do")
             return
         q = queue.Queue()
-        threads = [ threading.Thread(target=node.terminate_instance) for node in new_nodes]
-        for thread in threads:
-            thread.start()
-            thread.join()
+        targets = [ node.terminate_instance for node in self.nodes ]
+        self.__run_threads(targets)
 
         self.has_started = False
         self.cluster_size = 0
-        
-    def start(self):
-        if self.has_started:
-            self.log.warning("Cluster has already started. Nothing to do")
-            return
-        om_node = ec2.EC2(name=name, is_om_node=True)
-        om.node.start_instance()
-
-        self.om_ip_nodes = [om_node]
-        self.nodes = [om_node]
-
-        for i in xrange(1, self.cluster_size):
-            node = ec2.EC2(name=name)
-            node.start_instance()
-            self.nodes.append(node)
-        
-        self.has_started = True
-        
+        self.check_status()
+    
     def create_ec2_single_increment(self, increment):
         '''
-        Create a single node, and append each time, and append such node to the cluster
+        Add increment number of new nodes, one each time, and append such node to the cluster
         Have to check for errors in case a node failed.
+        
+        Arguments:
+        ----------
+        increment: int
+            how many additional nodes to be added to the cluster.
         '''
-        assert increment >= 1
         for i in xrange(0, increment):
-            node = ec2.EC2(name=name)
+            node = ec2.EC2(name=self.name)
             node.start_instance()
             self.nodes.append(node)
         self.cluster_size += increment
+        self.check_status()
         
     def create_ec2_multiple_increment(self, increment):
-        assert increment >= 1
+        '''
+        Add increment number of new nodes, which are scale up in parallel for efficiency.
+        Have to check for errors in case a node failed.
+        
+        Arguments:
+        ----------
+        increment: int
+            how many additional nodes to be added to the cluster.
+        '''
         new_nodes = []
+        
         # create increment number of nodes
         for i in xrange(0, increment):
             node = ec2.EC2(name=self.name)
-            new_nodes.apppend(node)
+            new_nodes.append(node)
         
         # create the new nodes in parallel
         q = queue.Queue()
-        threads = [ threading.Thread(target=node.start_instance) for node in new_nodes]
-        for thread in threads:
-            thread.start()
-            thread.join()
+        targets = [ node.start_instance for node in new_nodes ]
+        self.__run_threads(targets)
         
         self.nodes.extend(new_nodes)
         self.cluster_size += increment
+        self.check_status()
         
--- source/test/testlib/unit/test_domain.py
+++ source/test/testlib/unit/test_domain.py
@@ -2,6 +2,7 @@ import logging
 import unittest
 
 import ec2
+import domain
 
 class TestDomain(unittest.TestCase):
     
@@ -12,6 +13,77 @@ class TestDomain(unittest.TestCase):
         
     def setUp(self):
         pass
+        
+    def test_constructor(self):
+        # create a Domain with an invalid cluster size
+        dobj = domain.Domain(name="test_domain", cluster_size=1)
+        
+        self.assertEqual(dobj.cluster_size, 1)
+        self.assertEqual(len(dobj.nodes), 1)
+        self.assertEqual(len(dobj.om_nodes), 1)
+        self.assertEqual(dobj.nodes[0], dobj.om_nodes[0])
+        self.assertEqual(dobj.om_nodes[0].get_instance_status(), 'running')
+        
+        dobj.terminate()
+    
+    def test_create_ec2_multiple_increment(self):
+        dobj = domain.Domain(name="test_domain", cluster_size=4)
+        # assert cluster size is 4
+        self.assertEqual(dobj.cluster_size, 4)
+        self.assertEqual(dobj.has_started, True)
+        # assert all the 4 nodes are running
+        for node in dobj.nodes:
+            self.assertEqual(node.get_instance_status(), 'running')
+        
+        # increment the number of nodes to 16
+        dobj.create_ec2_multiple_increment(12)
+        self.assertEqual(dobj.cluster_size, 16)
+        # assert all the 16 nodes are running
+        for node in dobj.nodes:
+            self.assertEqual(node.get_instance_status(), 'running')
+        
+        dobj.terminate()
+        self.assertEqual(dobj.cluster_size, 0)
+        self.assertEqual(dobj.has_started, False)
+        
+    def test_create_ec2_single_increment(self):
+        dobj = domain.Domain(name="test_domain")
+        # assert cluster size is 4
+        self.assertEqual(dobj.cluster_size, 4)
+        self.assertEqual(dobj.has_started, True)
+        # assert all the 4 nodes are running
+        for node in dobj.nodes:
+            self.assertEqual(node.get_instance_status(), 'running')
+        
+        # increment the number of nodes to 16
+        dobj.create_ec2_single_increment(12)
+        self.assertEqual(dobj.cluster_size, 16)
+        # assert all the 16 nodes are running
+        for node in dobj.nodes:
+            self.assertEqual(node.get_instance_status(), 'running')
+        
+        dobj.terminate()
+        self.assertEqual(dobj.cluster_size, 0)
+        self.assertEqual(dobj.has_started, False)
+        
+    def test_halt(self):
+        dobj = domain.Domain(name="test_domain")
+        # assert cluster size is 4
+        self.assertEqual(dobj.cluster_size, 4)
+        self.assertEqual(dobj.has_started, True)
+        # assert all the 4 nodes are running
+        for node in dobj.nodes:
+            self.assertEqual(node.get_instance_status(), 'running')
+
+        dobj.halt()
+        for node in dobj.nodes:
+            self.assertEqual(node.get_instance_status(), 'stopped')
+            
+        dobj.resume()
+        for node in dobj.nodes:
+            self.assertEqual(node.get_instance_status(), 'running')
+            
+        dobj.terminate()
 
 if __name__ == '__main__':
     unittest.main()
