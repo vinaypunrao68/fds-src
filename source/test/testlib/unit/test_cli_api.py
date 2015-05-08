382384a8eb03e70dce362f91e04d1c69bb8d5da8
heanglim <heang@formionds.com>
2015-05-08 08:49:27 -0600

    Added test_cli_api and test_am

---
+++ source/test/testlib/unit/test_cli_api.py
@@ -0,0 +1,47 @@
+import unittest
+import logging
+import os
+import sys
+from fds.services.node_service import NodeService
+from fds.services.fds_auth import FdsAuth 
+from fds.services.users_service import UsersService 
+from fds.model.node_state import NodeState
+from fds.model.domain import Domain 
+import pdb
+
+
+#pdb.set_trace()
+fdsauth = FdsAuth()
+fdsauth.login()
+
+print fdsauth.get_hostname()
+print fdsauth.get_username()
+print fdsauth.get_token()
+print fdsauth.is_authenticated()
+
+nservice = NodeService(fdsauth)
+nodes_list = nservice.list_nodes()
+nodeS = NodeState()
+nodeS2 = NodeState()
+
+for node in nodes_list:
+	print 'node = {}'.format(node)
+	print 'node name = {}'.format(node.name)
+	print 'node ip_v4_address= {}'.format(node.ip_v4_address)
+	print 'node id = {}'.format(node.id)
+	print 'node state = {}'.format(node.state)
+	print 'node services = {}'.format(node.services)
+	#print node.services["OM"][0].auto_name + " " + node.services["OM"][0].status
+	print node.services["AM"][0].auto_name + " " + node.services["AM"][0].status
+	print node.services["SM"][0].auto_name + " " + node.services["SM"][0].status
+	print node.services["DM"][0].auto_name + " " + node.services["DM"][0].status
+	print node.services["PM"][0].auto_name + " " + node.services["PM"][0].status
+	#service.py id
+	nservice.deactivate_node(node.id, nodeS)	
+	nodeS2.am=False
+	pdb.set_trace()	
+	nservice.activate_node(node.id, nodeS2)	
+	print 'node state am = {}'.format(nodeS2.am)
+	print 'node state sm = {}'.format(nodeS2.sm)
+	print 'node state dm = {}'.format(nodeS2.dm)
+
