d475b6b59faba2fc450dfe0f7dc04d89dc066ac6
donavan nelson <donavan@formationds.com>
2015-05-21 12:50:15 -0600

    Hack one at thrift changes for primitive PM -> OM health monitoring for children services
    Signed-off-by: donavan nelson <donavan@formationds.com>

---
+++ source/fdsp/health_monitoring_types.thrift
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015 by Formation Data Systems, Inc.
+ * vim: noai:ts=8:sw=2:tw=100:syntax=cpp:et
+ */
+
+include "common.thrift"
+
+namespace cpp fds.apis
+namespace java com.formationds.protocol.om.types
+
+enum HealthState {
+  RUNNING          = 404000;
+  INITIALIZING     = 404001;
+  DEGRADED         = 404002;
+  LIMITED          = 404003;
+  SHUTTING_DOWN    = 404004;
+  ERROR            = 404005;
+  UNREACHABLE      = 404005;
+  UNEXPECTED_EXIT  = 404007;
+}
+
+/**
+ * Health Information Message
+ */
+struct HealthInfoMessage {
+  1: required common.SvcID  serviceID;
+  2: required i32           servicePort;
+  3: required HealthState   serviceStatue;
+  4: i32                    statusCode;
+  5: string                 statusInfo;
+}
+
