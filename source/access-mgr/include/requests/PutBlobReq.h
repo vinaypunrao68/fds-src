2d6bda4e04d8b5d2bdfbdf7ebff50f983e0619eb
Brian Szmyd <szmyd@formationds.com>
2015-05-22 13:25:40 -0600

    FS-1913: Removed obsolete counters from AM.

--- source/access-mgr/include/requests/PutBlobReq.h
+++ source/access-mgr/include/requests/PutBlobReq.h
@@ -38,7 +38,7 @@ struct PutBlobReq
     fds_uint64_t final_blob_size;
 
     /// Constructor used on regular putBlob requests.
-    PutBlobReq(fds_volid_t _volid,
+    inline PutBlobReq(fds_volid_t _volid,
                const std::string& _volumeName,
                const std::string& _blob_name,
                fds_uint64_t _blob_offset,
@@ -48,7 +48,7 @@ struct PutBlobReq
                CallbackPtr _cb);
 
     /// Constructor used on putBlobOnce requests.
-    PutBlobReq(fds_volid_t          _volid,
+    inline PutBlobReq(fds_volid_t          _volid,
                const std::string&   _volumeName,
                const std::string&   _blob_name,
                fds_uint64_t         _blob_offset,
@@ -64,10 +64,68 @@ struct PutBlobReq
         tx_desc = BlobTxId::ptr(new BlobTxId(txId));
     }
 
-    virtual ~PutBlobReq();
+    ~PutBlobReq() override = default;
 
-    void notifyResponse(const Error &e);
+    void notifyResponse(const Error &e) {
+        fds_verify(resp_acks > 0);
+        if (0 == --resp_acks) {
+            // Call back to processing layer
+            proc_cb(e);
+        }
+    }
 };
+
+PutBlobReq::PutBlobReq(fds_volid_t _volid,
+                       const std::string& _volName,
+                       const std::string& _blob_name,
+                       fds_uint64_t _blob_offset,
+                       fds_uint64_t _data_len,
+                       boost::shared_ptr<std::string> _data,
+                       BlobTxId::ptr _txDesc,
+                       CallbackPtr _cb) :
+AmRequest(FDS_PUT_BLOB, _volid, _volName, _blob_name, _cb, _blob_offset, _data_len),
+    AmTxReq(_txDesc),
+    resp_acks(2),
+    ret_status(ERR_OK),
+    final_meta_data(),
+    final_blob_size(0ULL),
+    dataPtr(_data)
+{
+    qos_perf_ctx.type = PerfEventType::AM_PUT_QOS;
+    hash_perf_ctx.type = PerfEventType::AM_PUT_HASH;
+    dm_perf_ctx.type = PerfEventType::AM_PUT_DM;
+    sm_perf_ctx.type = PerfEventType::AM_PUT_SM;
+
+    e2e_req_perf_ctx.type = PerfEventType::AM_PUT_OBJ_REQ;
+    fds::PerfTracer::tracePointBegin(e2e_req_perf_ctx);
+}
+
+PutBlobReq::PutBlobReq(fds_volid_t          _volid,
+                       const std::string&   _volName,
+                       const std::string&   _blob_name,
+                       fds_uint64_t         _blob_offset,
+                       fds_uint64_t         _data_len,
+                       boost::shared_ptr<std::string> _data,
+                       fds_int32_t          _blobMode,
+                       boost::shared_ptr< std::map<std::string, std::string> >& _metadata,
+                       CallbackPtr _cb) :
+    AmRequest(FDS_PUT_BLOB_ONCE, _volid, _volName, _blob_name, _cb, _blob_offset, _data_len),
+    blob_mode(_blobMode),
+    metadata(_metadata),
+    resp_acks(2),
+    ret_status(ERR_OK),
+    final_meta_data(),
+    final_blob_size(0ULL),
+    dataPtr(_data)
+{
+    qos_perf_ctx.type = PerfEventType::AM_PUT_QOS;
+    hash_perf_ctx.type = PerfEventType::AM_PUT_HASH;
+    dm_perf_ctx.type = PerfEventType::AM_PUT_DM;
+    sm_perf_ctx.type = PerfEventType::AM_PUT_SM;
+
+    e2e_req_perf_ctx.type = PerfEventType::AM_PUT_OBJ_REQ;
+    fds::PerfTracer::tracePointBegin(e2e_req_perf_ctx);
+}
 
 }  // namespace fds
 
