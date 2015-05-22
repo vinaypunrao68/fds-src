e867714f6a17ce95206bcaf98c757bf4a67380d5
Cody Boggs <cody@formationds.com>
2015-05-22 13:35:14 -0700

    restructure directories for fdscli to put things in a more pip-esque format. Update sub-package import statements to omit 'fds.' prefix, as sub-packages are now one level closer to the top-level package

---
+++ source/cli/fdscli/services/tenant_service.py
@@ -0,0 +1,51 @@
+from abstract_service import AbstractService
+from utils.tenant_converter import TenantConverter
+
+
+class TenantService( AbstractService):
+    '''
+    Created on Apr 23, 2015
+    
+    @author: nate
+    '''
+
+    def list_tenants(self):
+        '''
+        Get a list of all tenants in the system
+        '''
+        
+        url = "{}{}".format( self.get_url_preamble(), "/api/system/tenants")
+        response = self.rest_helper.get( self.session, url )
+        
+        tenants = []
+        
+        for j_tenant in response:
+            tenant = TenantConverter.build_tenant_from_json(j_tenant)
+            tenants.append(tenant)
+            
+        return tenants
+    
+    def create_tenant(self, tenant_name):
+        '''
+        Create a new tenancy in the system
+        '''
+        
+        url = "{}{}{}".format( self.get_url_preamble(), "/api/system/tenants/", tenant_name)
+        return self.rest_helper.post( self.session, url )
+    
+    def assign_user_to_tenant(self, tenant_id, user_id):
+        '''
+        assign a user to a tenancy
+        '''
+        
+        url = "{}{}{}{}{}".format( self.get_url_preamble(), "/api/system/tenants/", tenant_id, "/", user_id)
+        return self.rest_helper.put( self.session, url )
+    
+    def remove_user_from_tenant(self, tenant_id, user_id):
+        '''
+        remove the specified user from the specified tenancy
+        '''
+        
+        url = "{}{}{}{}{}".format( self.get_url_preamble(), "/api/system/tenants/", tenant_id, "/", user_id )
+        return self.rest_helper.delete( self.session, url )
+        
