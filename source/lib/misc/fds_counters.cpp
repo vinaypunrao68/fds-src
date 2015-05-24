3f7ec3aecf34872cbe9814b1398e28bbec183bfd
Brian Szmyd <szmyd@formationds.com>
2015-05-24 15:48:24 -0600

    Revert "Revert "Merge pull request #1903 from FDS-Dev/misc_qos_cleanup""
    This reverts commit 3d6805a608c11ceda3664d18a22682ca04dfde0d.

--- source/lib/misc/fds_counters.cpp
+++ source/lib/misc/fds_counters.cpp
@@ -400,9 +400,9 @@ NumericCounter::NumericCounter(const std::string &id, FdsCounters *export_parent
 NumericCounter::NumericCounter(const NumericCounter& c)
 : FdsBaseCounter(c)
 {
-    val_ = c.val_.load();
-    min_value_ = c.min_value_.load();
-    max_value_ = c.max_value_.load();
+    val_ = c.val_.load(std::memory_order_relaxed);
+    min_value_ = c.min_value_.load(std::memory_order_relaxed);
+    max_value_ = c.max_value_.load(std::memory_order_relaxed);
 }
 
 /**
@@ -415,7 +415,7 @@ NumericCounter::NumericCounter() {}
  */
 uint64_t NumericCounter::value() const
 {
-    return val_.load();
+    return val_.load(std::memory_order_relaxed);
 }
 
 /**
@@ -423,20 +423,15 @@ uint64_t NumericCounter::value() const
  */
 void NumericCounter::reset()
 {
-    val_ = 0;
-    min_value_ = std::numeric_limits<uint64_t>::max();
-    max_value_ = 0;
+    val_.store(0, std::memory_order_relaxed);
+    min_value_.store(std::numeric_limits<uint64_t>::max(), std::memory_order_relaxed);
+    max_value_.store(0, std::memory_order_relaxed);;
 }
 /**
  *
  */
 void NumericCounter::incr() {
-    val_++;
-    uint64_t val = val_.load();
-    uint64_t max = max_value_.load();
-    /* min_ stays the same*/
-    if (val > max)
-        max_value_ = val;
+    incr(1);
 }
 
 /**
@@ -444,24 +439,17 @@ void NumericCounter::incr() {
  * @param v
  */
 void NumericCounter::incr(const uint64_t v) {
-    val_ += v;
-    uint64_t val = val_.load();
-    uint64_t max = max_value_.load();
-    /* min_ stays the same*/
-    if (val > max)
-        max_value_ = val;
+    auto val = val_.fetch_add(v, std::memory_order_relaxed) + v;
+    auto old_max = max_value_.load(std::memory_order_consume);
+    if (old_max < val)
+        { max_value_.store(val, std::memory_order_release); }
 }
 
 /**
  *
  */
 void NumericCounter::decr() {
-    val_--;
-    uint64_t val = val_.load();
-    uint64_t min = min_value_.load();
-    /* max_ stays the same*/
-    if (val < min)
-        min_value_ = val;
+    decr(1);
 }
 
 /**
@@ -469,12 +457,10 @@ void NumericCounter::decr() {
  * @param v
  */
 void NumericCounter::decr(const uint64_t v) {
-    val_ -= v;
-    uint64_t val = val_.load();
-    uint64_t min = min_value_.load();
-    /* max_ stays the same*/
-    if (val < min)
-        min_value_ = val;
+    auto val = val_.fetch_sub(v, std::memory_order_relaxed) - v;
+    auto old_min = min_value_.load(std::memory_order_consume);
+    if (old_min > val)
+        { min_value_.store(val, std::memory_order_release); }
 }
 
 /**
