49f87abc759655e49d73a56af5f3751c8e7194de
Aaron Nichols <aaron@formationds.com>
2015-06-08 08:42:22 -0700

    bump version to 2015.06.05

--- omnibus/omnibus-fds-deps/config/projects/fds-deps.rb
+++ omnibus/omnibus-fds-deps/config/projects/fds-deps.rb
@@ -16,7 +16,7 @@ raise "FDS_SRC_DIR must be set'" unless fds_src_dir
 install_dir "#{default_root}/#{name}"
 
 #build_version Omnibus::BuildVersion.semver
-build_version '2015.05.22'
+build_version '2015.06.05'
 build_iteration 1
 
 # Creates required build directories
