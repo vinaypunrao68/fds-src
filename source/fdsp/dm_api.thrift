8aa8ef54501af56d83a55942c34ab782f5b1fd83
James Larkby-Lahet <jlarkbylahet@formationds.com>
2015-06-16 16:44:55 -0700

    forgot scoping of thrift types

--- source/fdsp/dm_api.thrift
+++ source/fdsp/dm_api.thrift
@@ -577,9 +577,8 @@ struct ReloadVolumeRspMsg {
 struct InitialBlobFilterSetMsg {
   // the volume in question
   1: i64                          volume_id;
-
   // the list of blobs held and the sequence id of the most recent write to each blob
-  2: list<BlobFilterSetEntry>           blob_filter_set;
+  2: list<dm_types.BlobFilterSetEntry>           blob_filter_set;
 }
 
 /* ------------------------------------------------------------
