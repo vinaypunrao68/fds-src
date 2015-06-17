cad6a87e8f81ba6c3c18e67be825af4a86002d2d
Andrew Leung <andrew@formationds.com>
2015-06-17 16:41:28 -0700

    FS-2134 Review feedback.

--- source/access-mgr/amlib/AmDispatcher.cpp
+++ source/access-mgr/amlib/AmDispatcher.cpp
@@ -41,6 +41,8 @@ extern std::string logString(const FDS_ProtocolInterface::GetObjectResp &getObj)
 extern std::string logString(const FDS_ProtocolInterface::PutObjectMsg& putObj);
 extern std::string logString(const FDS_ProtocolInterface::PutObjectRspMsg& putObj);
 
+static constexpr uint32_t DmDefaultPrimaryCnt { 2 };
+
 AmDispatcher::AmDispatcher(CommonModuleProviderIf *modProvider) 
         : HasModuleProvider(modProvider)
 {
@@ -81,7 +83,7 @@ AmDispatcher::start() {
 	    StatsCollector::singleton()->startStreaming(nullptr, nullptr);
 	}
     }
-    numDmPrimaries = conf.get_abs<fds_uint32_t>("fds.dm.number_of_primary", 2);
+    numDmPrimaries = conf.get_abs<fds_uint32_t>("fds.dm.number_of_primary", DmDefaultPrimaryCnt);
 }
 
 Error
@@ -213,7 +215,7 @@ AmDispatcher::createFailoverRequest(fds_volid_t const& volId,
     // Only issue reads from primaries.
     DmtColumnPtr dmsForVol = dmtMgr->getCommittedNodeGroup(volId);
     DmtColumnPtr dmPrimariesForVol = boost::make_shared<DmtColumn>(numDmPrimaries);
-    int numPrimaries = (dmsForVol->getLength() < numDmPrimaries ? dmsForVol->getLength() : numDmPrimaries);
+    uint32_t numPrimaries = (dmsForVol->getLength() < numDmPrimaries ? dmsForVol->getLength() : numDmPrimaries);
     for (uint32_t i = 0; i < numPrimaries; ++i) {
         dmPrimariesForVol->set(i, dmsForVol->get(i));
     }
