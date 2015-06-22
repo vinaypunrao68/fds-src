b397c38b9b1dae59d57e66678813903b444c9826
philipperibeiro <philippe@formationds.com>
2015-06-22 13:27:19 -0700

    Test volume bound added
    * New test checks if up to 3000 block/s3 volumes can be created
    * current bound is set at 1023
    * we want to push it further

--- source/test/scale-framework/testsets/testcases/test_volume_bound.py
+++ source/test/scale-framework/testsets/testcases/test_volume_bound.py
@@ -1,64 +1,50 @@
-#!/usr/bin/python
-#
-# Copyright 2014 by Formation Data Systems, Inc.
-
-import s3
-import utils
-import config
+import boto
+import os
+import random
+import shutil
+import sys
 import unittest
-import requests
 import json
+import requests
+
+import config
+import s3
+import samples
+import utils
 import testsets.testcase as testcase
 import block_volumes
 
-from boto.s3.key import Key
-from boto.s3.bucket import Bucket
-
-class TestCreateThreehundredObjectVolumes(testcase.FDSTestCase):
-
+class TestVolumeBound(testcase.FDSTestCase):
     '''
-    Test that 300 object volumes can successfully be created via the REST api
+    Finds the upper bound in the number of volumes
+    that can be created
     '''
-
     def __init__(self, parameters=None, config_file=None, om_ip_address=None):
-        super(TestCreateThreehundredObjectVolumes, self).__init__(parameters=parameters,
+        super(TestVolumeBound, self).__init__(parameters=parameters,
                 config_file=config_file, om_ip_address=om_ip_address)
-        
+
         self.block_volume = block_volumes.BlockVolumes(om_ip_address)
         self.buckets = []
 
-    '''
-    Do it.
-    '''
     def runTest(self):
         test_passed = False
         r = None
         port = config.FDS_REST_PORT
-
         try:
-
-            #Get the user token
+            # Get the user token
             userToken = str(utils.get_user_token("admin", "admin",
-                                                 self.om_ip_address, port, 0, 1))
-            self.log.info("userToken = %s", userToken)
-
-            #Setup for the request
+                                                self.om_ip_address, port, 0, 1))
+            self.log.info("UserToken = %s", userToken)
+            # setup for the request
             url = "http://" + self.om_ip_address + ":" + `port` + "/api/config/volumes"
             self.log.info("url = %s", url)
             header = {'FDS-Auth' : userToken}
             self.log.info("header = %s", header)
 
-            #Get the s3 connection from the testcase parameters
-            # conn = self.parameters['s3'].get_s3_connection()
-
-            #Get number of volumes currently?
-
-            for i in range(0, 100):
-
-                #bucket name
+            for i in xrange(0, 3000):
+                # bucket name
                 bucket_name = "test-" + str(i).zfill(3) + "_300"
                 self.buckets.append(bucket_name)
-                #prep data
                 data = {"sla":0,"limit":0,"priority":10,"snapshotPolicies":[],
                         "timelinePolicies":[{"retention":604800,"recurrenceRule":{"FREQ":"DAILY"}},
                         {"retention":1209600,"recurrenceRule":{"FREQ":"WEEKLY"}},{"retention":5184000,
@@ -66,10 +52,9 @@ class TestCreateThreehundredObjectVolumes(testcase.FDSTestCase):
                         "recurrenceRule":{"FREQ":"YEARLY"}}],"commit_log_retention":86400,
                         "data_connector":{"type":"OBJECT","api":"S3, Swift"},"name":bucket_name,
                         "mediaPolicy":"HDD_ONLY"}
-
                 json_data = json.dumps(data)
 
-                #create volume
+                # create volume
                 r = requests.post(url, data=json_data, headers=header)
                 if r is None:
                     raise ValueError, "r is None"
@@ -77,10 +62,9 @@ class TestCreateThreehundredObjectVolumes(testcase.FDSTestCase):
                     self.log.info("request = %s", r.request)
                     self.log.info("response = %s", r.json())
                     self.log.info("status = %s", r.status_code)
-    
-                    #Check return code
-                    self.assertTrue(200 == r.status_code)
 
+                    # check return code
+                    self.assertTrue(200 == r.status_code)
             test_passed = True
 
         except Exception, e:
@@ -91,11 +75,7 @@ class TestCreateThreehundredObjectVolumes(testcase.FDSTestCase):
 
     def tearDown(self):
         '''
-        Undo it.
+        Delete all the volumes.
         '''
         for bucket in self.buckets:
             self.block_volume.delete_volume(bucket)
-
-if __name__ == '__main__':
-    TestCase.FDSTestCase.fdsGetCmdLineConfigs(sys.argv)
-    unittest.main()
