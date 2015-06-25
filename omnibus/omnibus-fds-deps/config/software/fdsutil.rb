07aa872136236dfbf0d94abd15a105fcfe74ecd8
Cody Boggs <cody@formationds.com>
2015-06-25 11:26:59 -0700

    add readline to fds-deps

--- omnibus/omnibus-fds-deps/config/software/fdsutil.rb
+++ omnibus/omnibus-fds-deps/config/software/fdsutil.rb
@@ -2,6 +2,7 @@ name "fdsutil"
 default_version "0.2.0"
 
 dependency "pip"
+dependency "python-readline"
 
 build do
   command "#{install_dir}/embedded/bin/pip install --extra-index-url=https://builder:h7qgA3fYr0P3@artifacts.artifactoryonline.com/artifacts/api/pypi/formation-pip/simple -I --build #{project_dir} fdsutil==#{version}"
