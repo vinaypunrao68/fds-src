91a9d468de360ff7a6959ab1e9f52870463c14ee
James Larkby-Lahet <jlarkbylahet@formationds.com>
2015-06-30 16:44:32 -0700

    diffBlobList() method for the Migration Client (source side). returns two lists, blobs to update on the destination and blobs to delete, as in-out parameters

--- source/data-mgr/dm-lib/DmMigrationClient.cpp
+++ source/data-mgr/dm-lib/DmMigrationClient.cpp
@@ -40,51 +40,51 @@ DmMigrationClient::~DmMigrationClient()
  *  Algorithm runtime is linear in the size of the input.
  */
 Error
-DmMigrationClient::diffBlobLists(const std::map<int64_t, sequence_id_t>& dest,
-                                 const std::map<int64_t, sequence_id_t>& source)
+DmMigrationClient::diffBlobLists(const std::map<fds_uint64_t, sequence_id_t>& dest,
+                                 const std::map<fds_uint64_t, sequence_id_t>& source,
+                                 std::vector<fds_uint64_t>& update_list,
+                                 std::vector<fds_uint64_t>& delete_list)
 {
     auto source_it = source.cbegin();
     auto dest_it = dest.cbegin();
 
     while (dest_it != dest.cend() && source_it != source.cend()) {
-        if (dest_it.first == source_it.first){
-            /* NOTE: this assumes we overwrite more recent versions on the Dest
-               switch to < to only overwite older versions */
-            if (dest_it.second != source_it.second) {
-                // XXX: send source's blob here
-
+        if (dest_it->first == source_it->first) {
+            /* NOTE: this assumes we overwrite more recent versions on the Dest.
+               Switch the comparison to '<' to only overwite older versions */
+            if (dest_it->second != source_it->second) {
+                // update blob on dest to source's version
+                update_list.push_back(source_it->first);
             } // otherwise they match, do nothing
 
             ++dest_it;
             ++source_it;
-        }else if (dest_it.first > source_it.first) {
-            // XXX: send source's blob here
-
+        } else if (dest_it->first > source_it->first) {
+            // add blob on dest
+            update_list.push_back(source_it->first);
             ++source_it;
-        }else{
-            // XXX: send delete dest's blob here
-
+        } else {
+            // delete blob from dest
+            delete_list.push_back(dest_it->first);
             ++dest_it;
         }
     }
 
+    // delete the remainder of the Dest list
     while (dest_it != dest.cend()) {
-        // delete the remainder of the Dest list
-
-        // XXX: send delete dest's blob here
-
+        // delete blob from dest
+        delete_list.push_back(dest_it->first);
         ++dest_it;
     }
 
-    if (source_it != source.cend()) {
-        // send the remainder of the Source list
-
-        // XXX: send source's blob here
-
+    // send the remainder of the Source list
+    while (source_it != source.cend()) {
+        // add blob on dest
+        update_list.push_back(source_it->first);
         ++source_it;
     }
 
-    return Error.OK;
+    return ERR_OK;
 }
 
 }  // namespace fds
