baf34b66855109792bf3922df3dff6ea0bdd242e
James LaMar <james@formationds.com>
2015-07-17 17:06:40 -0700

    [FS-2853] hack to enable variable length object paths

--- source/java/fds-project/fds/src/main/java/com/formationds/xdi/s3/S3Endpoint.java
+++ source/java/fds-project/fds/src/main/java/com/formationds/xdi/s3/S3Endpoint.java
@@ -11,18 +11,24 @@ import com.formationds.spike.later.AsyncWebapp;
 import com.formationds.spike.later.HttpContext;
 import com.formationds.spike.later.HttpPath;
 import com.formationds.spike.later.SyncRequestHandler;
+import com.formationds.spike.later.pathtemplate.PathTemplate;
 import com.formationds.web.toolkit.*;
 import com.formationds.xdi.AsyncStreamer;
 import com.formationds.xdi.Xdi;
+import io.netty.util.internal.chmv8.ConcurrentHashMapV8;
 import org.joda.time.DateTime;
 import org.joda.time.format.ISODateTimeFormat;
 
 import javax.crypto.SecretKey;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 public class S3Endpoint {
     private final static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(S3Endpoint.class);
@@ -52,12 +58,6 @@ public class S3Endpoint {
     public void start() {
         setupNotImplementedRoutes();
 
-        syncRoute(new HttpPath(HttpMethod.GET, "/:bucket/:object")
-                        .withUrlParam("uploadId"),
-                (t) -> new MultiPartListParts(xdi, t));
-
-        webApp.route(new HttpPath(HttpMethod.GET, "/:bucket/:object"), ctx ->
-                executeAsync(ctx, new AsyncGetObject(xdi)));
 
         syncRoute(HttpMethod.GET, "/", (t) -> new ListBuckets(xdi, t));
 
@@ -71,46 +71,102 @@ public class S3Endpoint {
         syncRoute(HttpMethod.GET, "/:bucket", (t) -> new ListObjects(xdi, t));
         syncRoute(HttpMethod.HEAD, "/:bucket", (t) -> new HeadBucket(xdi, t));
 
-        syncRoute(new HttpPath(HttpMethod.PUT, "/:bucket/:object")
+        // TODO: this is a hack to handle the fact object paths can have variable length
+        for(int i = 1; i <= 50; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("/:bucket");
+            for(int j = 0; j < i; j++) {
+                sb.append("/:object" + j);
+            }
+
+            HttpPath path = new HttpPath().withPath(sb.toString());
+            setupObjectRoutes(path);
+        }
+
+        // this handles object paths of any length (but more slowly than the above)
+        setupObjectRoutes(new HttpPath().withPredicate(this::isObjectPath));
+
+        webApp.start();
+    }
+
+    // TODO: remove object pathing hack
+    //       our routing system doesn't handle variable length paths gracefully
+    //       so we have a bunch of routes for this, for path lengths from 1-50
+    //       as well as a path
+
+    private boolean isObjectPath(HttpContext ctx) {
+        String[] parts = ctx.getRequestURI().replaceAll("^/", "") .split("/", 2);
+        return parts.length == 2;
+    }
+
+    private void syncObjectRoute(HttpPath path, Function<AuthenticationToken, SyncRequestHandler> f) {
+        syncRoute(path, tkn -> ctx -> f.apply(tkn).handle(mergeObjectPath(ctx)));
+    }
+
+    private HttpContext mergeObjectPath(HttpContext context) {
+        Map<String, String> routeParameters = context.getRouteParameters();
+        Map<String, String> rp = new HashMap<>(routeParameters);
+
+        String p = context.getRequestURI().replaceAll("^/+", "").replaceAll("%2F", "/");
+        String[] parts = p.split("/", 2);
+
+        rp.put("object", parts[1]);
+        rp.put("bucket", parts[0]);
+        return context.withRouteParameters(rp);
+    }
+
+    private void setupObjectRoutes(HttpPath path) {
+
+        syncObjectRoute(path.clone().withMethod(HttpMethod.GET)
+                        .withUrlParam("uploadId"),
+                (t) -> new MultiPartListParts(xdi, t));
+
+        webApp.route(path.clone().withMethod(HttpMethod.GET), ctx ->
+                executeAsync(mergeObjectPath(ctx), new AsyncGetObject(xdi)));
+
+        syncObjectRoute(path.clone().withMethod(HttpMethod.PUT)
                         .withHeader(S3Endpoint.X_AMZ_COPY_SOURCE),
                 (t) -> new PutObject(xdi, t));
 
-        syncRoute(new HttpPath(HttpMethod.PUT, "/:bucket/:object")
+        syncObjectRoute(path.clone().withMethod(HttpMethod.PUT)
                         .withUrlParam("uploadId"),
                 (t) -> new PutObject(xdi, t));
 
-        syncRoute(new HttpPath(HttpMethod.PUT, "/:bucket/:object")
+        syncObjectRoute(path.clone().withMethod(HttpMethod.PUT)
                         .withUrlParam("acl")
                         .withHeader("x-amz-acl"),
                 (t) -> new PutObjectAcl(xdi, t));
 
-        webApp.route(new HttpPath(HttpMethod.PUT, "/:bucket/:object"), ctx ->
-                executeAsync(ctx, new AsyncPutObject(xdi)));
+        webApp.route(path.clone().withMethod(HttpMethod.PUT), ctx ->
+                executeAsync(mergeObjectPath(ctx), new AsyncPutObject(xdi)));
 
-        syncRoute(new HttpPath(HttpMethod.POST, "/:bucket/:object")
+        syncObjectRoute(path.clone().withMethod(HttpMethod.POST)
                 .withUrlParam("delete"), (t) -> new DeleteMultipleObjects(xdi, t));
 
-        syncRoute(new HttpPath(HttpMethod.POST, "/:bucket/:object")
+        syncObjectRoute(path.clone().withMethod(HttpMethod.POST)
                 .withUrlParam("uploads"), (t) -> new MultiPartUploadInitiate(xdi, t));
 
-        syncRoute(new HttpPath(HttpMethod.POST, "/:bucket/:object")
+        syncObjectRoute(path.clone().withMethod(HttpMethod.POST)
                 .withUrlParam("uploadId"), (t) -> new MultiPartUploadComplete(xdi, t));
 
-        syncRoute(HttpMethod.POST, "/:bucket/:object", (t) -> new PostObjectUpload(xdi, t));
+        syncObjectRoute(path.clone().withMethod(HttpMethod.POST), (t) -> new PostObjectUpload(xdi, t));
 
-        syncRoute(HttpMethod.HEAD, "/:bucket/:object", (t) -> new HeadObject(xdi, t));
-        syncRoute(HttpMethod.DELETE, "/:bucket/:object", (t) -> new DeleteObject(xdi, t));
+        syncObjectRoute(path.clone().withMethod(HttpMethod.HEAD), (t) -> new HeadObject(xdi, t));
+        syncObjectRoute(path.clone().withMethod(HttpMethod.DELETE), (t) -> new DeleteObject(xdi, t));
 
-        webApp.start();
-    }
+        // unimplemented
+        notImplementedObjectSubResource("cors", path.clone(), HttpMethod.OPTIONS);
+        notImplementedObjectSubResource("lifecycle", path.clone().withUrlParam("restore"), HttpMethod.POST);
+
+zHop    }
+
+    // end object pathing hack
 
     private void setupNotImplementedRoutes() {
         notImplementedBucketSubResources("cors", "lifecycle", "policy", "replication", "tagging", "website", "location",
                 "notification", "versions", "versioning", "logging", "requestPayment");
 
         notImplementedBucketSubResource("bucket uploads", p -> p.withUrlParam("uploads"), HttpMethod.GET);
-        notImplementedObjectSubResource("cors", p -> p, HttpMethod.OPTIONS);
-        notImplementedObjectSubResource("lifecycle", p -> p.withUrlParam("restore"), HttpMethod.POST);
     }
 
     private void notImplementedBucketSubResources(String ... subresourceStrings) {
@@ -122,9 +178,9 @@ public class S3Endpoint {
         notImplementedBucketSubResource(feature, path, HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.GET);
     }
 
-    private void notImplementedObjectSubResource(String feature, Function<HttpPath, HttpPath> path, HttpMethod...methods) {
+    private void notImplementedObjectSubResource(String feature, HttpPath path, HttpMethod...methods) {
         for(HttpMethod method : methods)
-            webApp.route(path.apply(new HttpPath(method, "/:bucket/:object")), ctx -> notImplementedHandler(ctx, feature));
+            webApp.route(path.withMethod(method), ctx -> notImplementedHandler(ctx, feature));
     }
 
     private void notImplementedBucketSubResource(String feature, Function<HttpPath, HttpPath> path, HttpMethod...methods) {
