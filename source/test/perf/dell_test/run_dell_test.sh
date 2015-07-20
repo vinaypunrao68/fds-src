00c02f4e93c1055fd231a5300bc263729330bb0e
root <root@perf2-node1.formationds.com>
2015-07-20 16:05:31 -0700

    Adding multi-volume support

--- source/test/perf/dell_test/run_dell_test.sh
+++ source/test/perf/dell_test/run_dell_test.sh
@@ -5,19 +5,12 @@
 function volume_setup {
     local vol=$1
     pushd ../../../cli
+    echo "Creating: $vol"
     ./fds volume create -name $vol -type block -block_size 128 -block_size_unit KB -media_policy HDD
     popd
     sleep 10
 }
 
-function volume_attach {
-    local node=$1
-    local vol=$2
-    echo $node $vol
-    pushd ../../../cli
-    disks[$m]=`../../../cinder/nbdadm.py attach $node $vol`
-    popd
-}
 function volume_detach {
     local vol=$1
     ../../../cinder/nbdadm.py detach $vol
@@ -31,6 +24,7 @@ function process_results {
     local iodepth=$5
     local disksize=$6
     local machine=$7
+    local vol=$8
 
     iops=`grep iops $f | sed -e 's/[ ,=:]/ /g' | awk '{e+=$7}END{print e}'`
     latency=`grep clat $f | grep avg| awk -F '[,=:()]' '{print ($2 == "msec") ? $9*1000 : $9}' | awk '{i+=1; e+=$1}END{print e/i/1000}'`
@@ -44,6 +38,7 @@ function process_results {
     echo iops=$iops >> .data
     echo latency=$latency >> .data
     echo machine=$machine >> .data
+    echo vol=$i >> .data
     ../common/push_to_influxdb.py dell_test .data
 }
 
@@ -52,8 +47,9 @@ function process_results {
 outdir=$1
 node=$2
 machines=$3
+nvols=$4
 
-size=50g
+size=1g
 
 # Dell test specs:
 # bsizes="512 4096 8192 65536 524288"
@@ -70,36 +66,51 @@ workloads="randread randwrite read write"
 declare -A disks
 
 for m in $machines ; do
-    volume_setup volume_block_$m
+    for i in `seq $nvols` ; do
+    	volume_setup volume_block_$m\_$i
 
-    # disks[$m]=`../../../cinder/nbdadm.py attach $m  volume_block_$m`
-    disks[$m]=`ssh $m "cd /fds/sbin && ./nbdadm.py attach $m  volume_block_$m"`
+    	# disks[$m]=`../../../cinder/nbdadm.py attach $m  volume_block_$m`
+    	disks[$m:$i]=`ssh $m "cd /fds/sbin && ./nbdadm.py attach $m  volume_block_$m\_$i"`
 
-    echo "nbd disk: ${disks[$m]}"
-    if [ "${disks[$m]}" = "" ]; then
-        echo "Volume setup failed"
-        exit 1;
-    fi
-    ssh $m "fio --name=write --rw=write --filename=${disks[$m]} --bs=512k --numjobs=4 --iodepth=64 --ioengine=libaio --direct=1 --size=$size
+    	echo "nbd disk: ${disks[$m:$i]}"
+    	if [ "${disks[$m:$i]}" = "" ]; then
+    	    echo "Volume setup failed"
+    	    exit 1;
+    	fi
+    	ssh $m "fio --name=write --rw=write --filename=${disks[$m:$i]} --bs=512k --numjobs=4 --iodepth=64 --ioengine=libaio --direct=1 --size=$size"
+    done
 done
 
 
-
+declare -A pids
 for bs in $bsizes ; do
     for worker in $workers ; do
         for workload in $workloads ; do
                 for d in $iodepths ; do
-                #sync
-                #echo 3 > /proc/sys/vm/drop_caches
-                for m in $machines ; do
-                    outfile=$outdir/out.numjobs=$worker.workload=$workload.bs=$bs.iodepth=$d.disksize=$size.machine=$m
-                    ssh $m "fio --name=test --rw=$workload --filename=$nbd_disk --bs=$bs --numjobs=$worker --iodepth=$d --ioengine=libaio --direct=1 --size=$size --time_based --runtime=60 | tee $outfile"
-                    process_results $outfile $worker $workload $bs $d $size $m
-                done
+                	#sync
+                	#echo 3 > /proc/sys/vm/drop_caches
+                	for m in $machines ; do
+    			    for i in `seq $nvols` ; do
+                	    	outfile=$outdir/out.numjobs=$worker.workload=$workload.bs=$bs.iodepth=$d.disksize=$size.machine=$m.vol=$i
+                	    	ssh $m "fio --name=test --rw=$workload --filename=${disks[$m:$i]} --bs=$bs --numjobs=$worker --iodepth=$d --ioengine=libaio --direct=1 --size=$size --time_based --runtime=60" | tee $outfile &
+			    	pids[$m:$i]=$!
+                            done
+			done
+                	for m in $machines ; do
+    			    for i in `seq $nvols` ; do
+			    	echo "Waiting for $m ${pids[$m:$i]}"
+			    	wait ${pids[$m:$i]}
+			    	echo "Processing results for $m ${pids[$m:$i]}"
+                	    	process_results $outfile $worker $workload $bs $d $size $m $i
+			    	pids[$m:$i]=""
+                           done
+                	done
                 done
             done
         done
 done
 for m in $machines ; do
-       volume_detach volume_block_$m
+    for i in `seq $nvols` ; do
+        volume_detach volume_block_$m\_$i
+    done
 done
