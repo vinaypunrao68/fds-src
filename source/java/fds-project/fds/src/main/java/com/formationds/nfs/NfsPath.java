f4f3a99501f6dcb1e0742fb4de67e08db9432df5
Fabrice Medio <fmedio@gmail.com>
2015-07-23 15:46:27 -0700

    [FS-2686] NFS: work around DM's pattern matching behavior

--- source/java/fds-project/fds/src/main/java/com/formationds/nfs/NfsPath.java
+++ source/java/fds-project/fds/src/main/java/com/formationds/nfs/NfsPath.java
@@ -1,5 +1,6 @@
 package com.formationds.nfs;
 
+import com.formationds.protocol.BlobDescriptor;
 import org.apache.hadoop.fs.Path;
 import org.dcache.nfs.vfs.FileHandle;
 import org.dcache.nfs.vfs.Inode;
@@ -33,6 +34,13 @@ public class NfsPath {
         path = new Path(sb.toString());
     }
 
+    // The blob name is prefixed with a '/', otherwise DM's regex filter doesn't seem to work.
+    // Specifically, its regex matcher only seems to kick in after the first character.
+    public NfsPath(String volume, BlobDescriptor bd) {
+        this.volume = volume;
+        this.path = new Path(bd.getName().replaceAll("^/", ""));
+    }
+
     public NfsPath(String volume, String path) {
         this.volume = volume;
         this.path = new Path(path);
@@ -61,8 +69,9 @@ public class NfsPath {
         return volume == null && path.toString().equals("/");
     }
 
+    // Same workardound for DM regex matcher issue.
     public String blobName() {
-        return path.toString();
+        return "/" + path.toString();
     }
 
     public long deviceId(ExportResolver resolver) {
@@ -78,7 +87,7 @@ public class NfsPath {
         int exportId = 0;
         if (volume != null) {
             sb.append(volume);
-            sb.append(blobName());
+            sb.append(path.toString());
             exportId = resolver.exportId(volume);
         }
         return new Inode(new FileHandle(0, exportId, type.toMode(), sb.toString().getBytes()));
