ef8a1ccbda5f976cfb83020c25647ce3f5a023de
Sanjay Nayak <sanjay@formationds.com>
2015-07-24 23:26:03 -0700

    expunge object queue- check the volume queue  status

--- source/stor-mgr/smlib/SMSvcHandler.cpp
+++ source/stor-mgr/smlib/SMSvcHandler.cpp
@@ -697,6 +697,10 @@ void SMSvcHandler::deleteObject(boost::shared_ptr<fpi::AsyncHdr>& asyncHdr,
     Error err(ERR_OK);
     ObjectID objId(deleteObjMsg->objId.digest);
     auto delReq = new SmIoDeleteObjectReq(deleteObjMsg);
+    StorMgrVolume * vol = objStorMgr->getVol(delReq->getVolId());
+    fds_assert(vol != NULL);
+
+     fds_volid_t queueId = vol->getQueue()->getVolUuid();
 
     // Set the client's ID to use to serialization
     delReq->setClientSvcId(asyncHdr->msg_src_uuid);
@@ -736,8 +740,11 @@ void SMSvcHandler::deleteObject(boost::shared_ptr<fpi::AsyncHdr>& asyncHdr,
     // SM volume  queues are deleted  as part of the volume delete before expunge is complete. 
     // hence moved the delete queue to system queue. we will hav to  revisit this
 
-    // err = objStorMgr->enqueueMsg(delReq->getVolId(), delReq);
-    err = objStorMgr->enqueueMsg(FdsSysTaskQueueId, delReq);
+    if (objStorMgr->getQueue(queueId)) {
+       err = objStorMgr->enqueueMsg(delReq->getVolId(), delReq);
+    } else  {
+       err = objStorMgr->enqueueMsg(FdsSysTaskQueueId, delReq);
+    }
     if (err != fds::ERR_OK) {
         LOGERROR << "Failed to enqueue to SmIoDeleteObjectReq to StorMgr.  Error: "
                  << err;
