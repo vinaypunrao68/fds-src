709a04b6779e6f590c9ce62985385e7f7f48e564
Matteo Monchiero <matteo@formationds.com>
2015-07-29 12:09:06 -0700

    Ec2 performance test scripts

--- source/test/perf/ec2/install_sw.sh
+++ source/test/perf/ec2/install_sw.sh
@@ -2,7 +2,10 @@
 
 node=$1
 
-SSH="sshpass -p passwd ssh -o StrictHostKeyChecking=no -y root@$node"
+SSH="sshpass -p passwd ssh -o StrictHostKeyChecking=no root@$node"
 $SSH DEBIAN_FRONTEND=noninteractive apt-get install -y python-dev nbd-client fio python-pip
+$SSH pip install influxdb==0.1.12
 $SSH pip install psutil
+$SSH ethtool -K eth0 tso off
+$SSH ethtool -k eth0|grep tcp-segmentation-offload
 
