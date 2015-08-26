1fdacd8199931ac9d091037d09d011d9e49ca719
poojashahuraj <pooja@formationds.com>
2015-08-26 14:06:59 -0600

    not sure if we need this file

--- source/test/testsuites/testcases/-d.py
+++ source/test/testsuites/testcases/-d.py
@@ -15,4 +15,3 @@ idx = sys._getframe(2).f_locals['names'].index('-d')
 # Remove '-d' and its argument.
 sys._getframe(2).f_locals['names'].pop(sys._getframe(2).f_locals['names'].__len__()-1)
 sys._getframe(2).f_locals['names'].pop(sys._getframe(2).f_locals['names'].__len__()-1)
-
