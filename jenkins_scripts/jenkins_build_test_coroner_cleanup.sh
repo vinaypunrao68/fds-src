9ec6e9b899a90fc0712e2917287ad7c029e20678
Neil Huang <neil@formationds.com>
2015-08-27 09:37:15 -0700

    Re-enable ActiveIORestartTest

--- jenkins_scripts/jenkins_build_test_coroner_cleanup.sh
+++ jenkins_scripts/jenkins_build_test_coroner_cleanup.sh
@@ -11,8 +11,8 @@ PYTHON_COVERAGE_TEST_RUNNER_DIRECTORIES="source/platform/python/tests"
 PYTHON_UNITTEST_DISCOVERY_DIRECTORIES="source/tools"
 
 # The list of system test scenarios, do not include the .ini"
-SYSTEM_TEST_SCENARIO_LIST="BuildSmokeTest_onpr StaticMigration ActiveMigration RestartDataPersistence ActiveIOKillTest ActiveIORndKillTest MultiAMVolOpsTest QosTest"
-DISABLED_SYSTEM_TEST_SCENARIO_LIST="ActiveIORestartTest RestartClusterKillServices"   ## This should be deleted when fs-2473 fs-2478 are resolved
+SYSTEM_TEST_SCENARIO_LIST="BuildSmokeTest_onpr StaticMigration ActiveMigration RestartDataPersistence ActiveIOKillTest ActiveIORndKillTest ActiveIORestartTest MultiAMVolOpsTest QosTest"
+DISABLED_SYSTEM_TEST_SCENARIO_LIST="RestartClusterKillServices"   ## This should be deleted when fs-2473 fs-2478 are resolved
 
 function performance_report
 {
