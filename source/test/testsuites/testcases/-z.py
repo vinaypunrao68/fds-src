4407ebdc03fe85829f3dd2404eb1718c6ae2f52e
poojashahuraj <pooja@formationds.com>
2015-08-31 10:43:29 -0600

    corrected comments

--- source/test/testsuites/testcases/-z.py
+++ source/test/testsuites/testcases/-z.py
@@ -1,10 +1,10 @@
 __author__ = 'pooja'
 import sys
 
-# Where is '-q'? We know it's there or we wouldn't be here.
+# Where is '-z'? We know it's there or we wouldn't be here.
 idx = sys._getframe(2).f_locals['names'].index('-z')
 
-# Remove '-q' and its argument.
+# Remove '-z' and its argument.
 sys._getframe(2).f_locals['names'].pop(sys._getframe(2).f_locals['names'].__len__()-1)
 sys._getframe(2).f_locals['names'].pop(sys._getframe(2).f_locals['names'].__len__()-1)
 
