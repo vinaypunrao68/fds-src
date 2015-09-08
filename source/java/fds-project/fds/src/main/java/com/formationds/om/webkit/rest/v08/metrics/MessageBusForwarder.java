1a27d97bd34e8432297bcc5062d2c1fb9cc584fb
Nate Bever <nate@formationds.com>
2015-09-08 16:54:36 -0600

    handling no content

--- source/java/fds-project/fds/src/main/java/com/formationds/om/webkit/rest/v08/metrics/MessageBusForwarder.java
+++ source/java/fds-project/fds/src/main/java/com/formationds/om/webkit/rest/v08/metrics/MessageBusForwarder.java
@@ -55,13 +55,15 @@ public class MessageBusForwarder implements RequestHandler {
 		method.setRequestHeader( "Content-Type", "application/json" );
 		
 		int statusCode = client.executeMethod( method );
-		
+		String body = "";
+				
 		// our code doesn't see a 204 as a success ... but for the message bus it is.
 		if ( statusCode == 204 ){
 			statusCode = 200;
 		}
-		
-		String body = new String( method.getResponseBody() );
+		else {
+			body = method.getResponseBodyAsString();
+		}
 		
 		TextResource resource = new TextResource( statusCode, body );
 		
