4db41a60e9c2b9ae9333210659de23794e541191
Nate Bever <nate@formationds.com>
2015-09-09 10:54:09 -0600

    adding in the starts of the debug page

--- source/java/admin-webapp/src/scripts/debug/debug_fds.js
+++ source/java/admin-webapp/src/scripts/debug/debug_fds.js
@@ -2,26 +2,121 @@ angular.module( 'debug' ).controller( 'debugController', ['$scope', '$http', '$b
     
     $scope.stats = [];
     
-    $scope.getStat = function(){
+    $scope.listVolColors = [ 'blue' ];
+    $scope.listVolLineColor = [ 'black' ];
+    $scope.listVolLabels = [ '2 hours ago', 'Now' ];
+    $scope.listVolStats = [];
+    
+    var pollId = -1;
+    var myQueue = '';
+    
+    var makeBusCall = function( method, route, data ){
         
-        var authStr = $base64.encode( 'admin:admin' );
-        var route = '/api/queues';
-        route = encodeURI( route );
+        var realRoute = encodeURIComponent( route );
         
         var req = {
-            url: '/fds/config/v08/mb/' + route,
+            url: '/fds/config/v08/mb/' + realRoute,
             headers: {
-                Authorization: 'Basic ' + authStr
+                'Content-Type': 'application/json'
             },
-            method: 'GET'
+            method: method,
+            data: data
         };
         
-        $http( req )
-            .then( function( response ){
+        return $http( req );
+    };
+    
+    $scope.start = function(){
+        
+        if ( myQueue !== '' ){
+            return;
+        }
+        
+        myQueue = 'ui-queue-' + (new Date()).getTime();
+        
+        // create my queue
+        var route = 'api/queues/%2F/' + myQueue;
+        var data = {
+            'auto_delete': true,
+            'durable': false,
+            'arguments': {},
+            'node': "rabbit"
+        };
+        
+        // setup a binding
+        var bindToExchange = function( response ){
             
-                alert( response );
-            });
+            var route = 'api/bindings/%2F/e/admin/q/' + myQueue;
+            var data = {
+                'routing_key': 'stats.VOLUME.#',
+                'arguments': []
+            };
+            
+            makeBusCall( 'POST', route, data )
+                .then( 
+                    function( response ){ 
+                        alert( 'Successfully bound.' );
+                        
+                        pollId = $interval( $scope.getMessages, 2 );
+    
+                    },
+                    function( response ){
+                        alert( 'Failed to bind to a subscription' );
+                    });
+        };
+            
+        var declareExchange = function( resopnse ){
+            
+            var route = 'api/exchanges/%2F/admin';
+            var data = {
+                'type': 'topic',
+                'auto_delete': false,
+                'durable': false,
+                'arguments': []
+            };
+            
+            makeBusCall( 'PUT', route, data )
+                .then( bindToExchange,
+                      function( response ){
+                        alert( 'Failed to declare exchange' );
+                    });
+        };
+        
+        makeBusCall( 'PUT', route, data )
+            .then( declareExchange, 
+                  function( response )
+                  { 
+                    alert( 'Failed to establish queue' );
+                  });
         
     };
+    
+    // stop the poller for stats.
+    $scope.stop = function(){
+        $interval.cancel( pollId );
+        makeBusCall( 'DELETE', 'api/queueus/%2F/' + myQueue );
+    };
+    
+    // take the messages and put them in the right stat arrays
+    var handleStats = function( response ){
+        
+        for ( var i = 0; i < response.data.length; i++ ){
+            alert( 'Msg: ' + i );
+        }
+    };
+    
+    $scope.getMessages = function(){
+        
+        var route = 'api/queues/%2F/' + myQueue + '/get';
+        var data = {
+            count: 1000,
+            requeue: false,
+            encoding: 'auto'
+        };
+        
+        makeBusCall( 'POST', route, data ).then( handleStats );
+    };
+    
+    $scope.$on("$destroy", $scope.stop );
     
 }]);
