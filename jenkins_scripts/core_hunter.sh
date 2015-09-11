d3eba39cec75e19b6a746dafc117f49806a66e98
Dave Setzke <dave@formationds.com>
2015-09-11 12:20:27 -0600

    review comments - add {} to bash var refs

--- jenkins_scripts/core_hunter.sh
+++ jenkins_scripts/core_hunter.sh
@@ -9,18 +9,18 @@ function core_hunter
     for d in /corefiles /fds; do 
         for c in core hprof; do 
         
-            find $d -type f -name "*.$c" |grep -e ".*" > /dev/null
+            find ${d} -type f -name "*.${c}" |grep -e ".*" > /dev/null
             return_code=$?
             
-            if [ $return_code -eq 0 ]; then
+            if [ ${return_code} -eq 0 ]; then
                 return ${return_code}
             fi
         done    
 
-        find $d -type f -name "*hs_err_pid*.log" |grep -e ".*" > /dev/null
+        find ${d} -type f -name "*hs_err_pid*.log" |grep -e ".*" > /dev/null
         return_code=$?
         
-        if [ $return_code -eq 0 ]; then
+        if [ ${return_code} -eq 0 ]; then
             return ${return_code}
         fi
 
