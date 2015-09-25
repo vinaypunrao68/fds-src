0afc371133f83e6cd78df709460087ce030903ac
heanglim <heang@formationds.com>
2015-09-25 13:08:44 -0700

    Create a simple shell script to run System test against AWS nodes

---
+++ source/test/testsuites/LongSystemTest.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PASSWD="pairing"
+INVENTORYFILE="s3-ec2-nodes"
+LOGFILE="/tmp/LongSystemTest.log"
+
+#ADD NEW SYSTEM TEST HERE IN THE ARRAY
+declare -a SCENARIOLIST=(
+SnapshotTest
+AWS_KillServiceTest
+AWS_StartStopServiceTest
+AWS_AddRemoveServiceTest
+AWS_StartShutDownNodeTest
+AWS_AddRemoveNodeTest
+)
+
+
+STARTTIME=$(date)
+echo "LONG SYSTEM TEST START TIME:  $STARTTIME"
+
+for SCENARIO in "${SCENARIOLIST[@]}"
+do
+	echo "======================================================================================================="
+	echo "$SCENARIO START TIME:  $(date)"
+	echo "Running Scenario $SCENARIO test....."
+	sudo ./ScenarioDriverSuite.py -q ./${SCENARIO}.ini -d $PASSWD --verbose -l $LOGGING --install -z $INVENTORYFILE
+	[[ $? -ne 0 ]] && echo "LONG SYSTEM TEST:  $SCENARIO FAILED" 
+	echo "$SCENARIO END TIME:  $(date)"
+	echo "*******************************************************************************************************"
+done
+
+ENDTIME=$(date)
+echo "LONG SYSTEM TEST END TIME: $ENDTIME"
