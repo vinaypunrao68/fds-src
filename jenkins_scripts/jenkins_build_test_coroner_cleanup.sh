91a8a1a0033900a34699504cdf13ddbbcee57b7e
Neil Huang <neil@formationds.com>
2015-10-12 13:35:28 -0700

    To be reverted - temporary enable just failover test

--- jenkins_scripts/jenkins_build_test_coroner_cleanup.sh
+++ jenkins_scripts/jenkins_build_test_coroner_cleanup.sh
@@ -11,8 +11,10 @@ PYTHON_COVERAGE_TEST_RUNNER_DIRECTORIES="source/platform/python/tests"
 PYTHON_UNITTEST_DISCOVERY_DIRECTORIES="source/tools"
 
 # The list of system test scenarios, do not include the .ini"
-SYSTEM_TEST_SCENARIO_LIST="BuildSmokeTest_onpr ActiveIORestartTest RestartDataPersistence ActiveIOKillTest ActiveIORndKillTest MultiAMVolOpsTest QosTest RestartClusterKillServices ActiveMigration DmMigrationFailover"
-DISABLED_SYSTEM_TEST_SCENARIO_LIST="StaticMigration"
+#SYSTEM_TEST_SCENARIO_LIST="BuildSmokeTest_onpr ActiveIORestartTest RestartDataPersistence ActiveIOKillTest ActiveIORndKillTest MultiAMVolOpsTest QosTest RestartClusterKillServices ActiveMigration DmMigrationFailover"
+#DISABLED_SYSTEM_TEST_SCENARIO_LIST="StaticMigration"
+DISABLED_SYSTEM_TEST_SCENARIO_LIST="BuildSmokeTest_onpr ActiveIORestartTest RestartDataPersistence ActiveIOKillTest ActiveIORndKillTest MultiAMVolOpsTest QosTest RestartClusterKillServices ActiveMigration DmMigrationFailover"
+SYSTEM_TEST_SCENARIO_LIST="DmMigrationFailover"
 
 function performance_report
 {
