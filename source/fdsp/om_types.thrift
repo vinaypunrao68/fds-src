3b99766a85647fe16b889a907ec6d3777474d91f
Meenakshi Mani <meena@formationds.com>
2015-10-26 10:14:25 -0600

    Address PR failure

--- source/fdsp/om_types.thrift
+++ source/fdsp/om_types.thrift
@@ -1,14 +1,13 @@
 /*
  * Copyright 2014 by Formation Data Systems, Inc.
  */
+namespace cpp FDS_ProtocolInterface
+namespace java com.formationds.protocol
+namespace java com.formationds.protocol.om.types
+namespace java com.formationds.protocol.pm.types
 
 include "common.thrift"
 include "pm_types.thrift"
-
-namespace cpp FDS_ProtocolInterface
-namespace java com.formationds.protocol.om.types
-namespace java com.formationds.protocol.pm.types
-namespace java com.formationds.protocol
 
 exception OmRegisterException {
 }
@@ -19,9 +18,9 @@ exception SvcLookupException {
 
 struct SvcChangeReqInfo {
   /* Type of the service for which state change req came in */
-  1: required common.FDSP_MgrIdType svcType;
+  1: common.FDSP_MgrIdType svcType;
   /* This value(0/1) will indicate (no-op/op success) */
-  2: required pm_types.ActionCode actionCode;
+  2: pm_types.ActionCode actionCode;
 }
 
 typedef list<SvcChangeReqInfo> SvcChangeInfoList
