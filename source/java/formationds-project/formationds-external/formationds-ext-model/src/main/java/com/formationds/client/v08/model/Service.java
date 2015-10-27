28f282adcf1394d62560e807bf7de64000c464f0
Meenakshi Mani <meena@formationds.com>
2015-10-27 16:55:20 -0600

    cleanup before review

--- source/java/formationds-project/formationds-external/formationds-ext-model/src/main/java/com/formationds/client/v08/model/Service.java
+++ source/java/formationds-project/formationds-external/formationds-ext-model/src/main/java/com/formationds/client/v08/model/Service.java
@@ -19,7 +19,7 @@ public class Service extends AbstractResource<Long> {
         INITIALIZING,
         SHUTTING_DOWN,
         STANDBY,         // To reflect PM state when the node is down
-    	DISCOVERED;      // State of PM after node has been removed
+        DISCOVERED;      // State of PM after node has been removed
     }
     
     public static class ServiceStatus{
