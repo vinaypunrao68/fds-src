2b53cf6ab7f66dba88b7d3174f70195f4827a377
paul-tinius <paul@formationds.com>
2015-11-02 12:13:20 -0700

    FS-3416: iSCSI external model
    Removed Incoming/Outgoing Users from LUN class.

--- source/java/formationds-project/formationds-external/formationds-ext-model/src/main/java/com/formationds/client/v08/model/iscsi/LUN.java
+++ source/java/formationds-project/formationds-external/formationds-ext-model/src/main/java/com/formationds/client/v08/model/iscsi/LUN.java
@@ -4,9 +4,6 @@
 
 package com.formationds.client.v08.model.iscsi;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * @author ptinius
  */
@@ -17,13 +14,9 @@ public class LUN
     public static class Builder {
         private AccessType accessType;
         private String lunName;
-        private Set<IncomingUser> incomingUsers = new HashSet<>( );
-        private Set<OutgoingUser> outgoingUsers = new HashSet<>( );
 
         protected AccessType getAccessType( ) { return accessType; }
         protected String getLunName( ) { return lunName; }
-        protected Set<IncomingUser> getIncomingUsers() { return incomingUsers; }
-        protected Set<OutgoingUser> getOutgoingUsers() { return outgoingUsers; }
 
         public Builder withAccessType( final AccessType accessType )
         {
@@ -37,42 +30,23 @@ public class LUN
             return this;
         }
 
-        public Builder withIncomingUser( final IncomingUser incomingUser )
-        {
-            incomingUsers.add( incomingUser );
-            return this;
-        }
-
-        public Builder withOutgoingUser( final OutgoingUser outgoingUser )
-        {
-            outgoingUsers.add( outgoingUser );
-            return this;
-        }
-
         public LUN build() {
-            return new LUN( getLunName(), getAccessType(), getIncomingUsers(), getOutgoingUsers() );
+            return new LUN( getLunName(), getAccessType() );
         }
     }
 
     private final AccessType accessType;
     private final String lunName;
 
-    private final Set<IncomingUser> incomingUsers = new HashSet<>( );
-    private final Set<OutgoingUser> outgoingUsers = new HashSet<>( );
-
     /**
      * @param lunName the name of the logical unit number
      * @param accessType the access control
      */
     protected LUN( final String lunName,
-                   final AccessType accessType,
-                   final Set<IncomingUser> incomingUsers,
-                   final Set<OutgoingUser> outgoingUsers )
+                   final AccessType accessType )
     {
         this.lunName = lunName;
         this.accessType = accessType;
-        this.incomingUsers.addAll( incomingUsers );
-        this.outgoingUsers.addAll( outgoingUsers );
     }
 
     /**
@@ -89,22 +63,6 @@ public class LUN
     public String getLunName( )
     {
         return lunName;
-    }
-
-    /**
-     * @return Returns the incoming users for this Logical Unit Number
-     */
-    public Set<IncomingUser> getIncomingUsers( )
-    {
-        return incomingUsers;
-    }
-
-    /**
-     * @return Returns the outgoing users for this Logical Unit Number
-     */
-    public Set<OutgoingUser> getOutgoingUsers( )
-    {
-        return outgoingUsers;
     }
 
     @Override
