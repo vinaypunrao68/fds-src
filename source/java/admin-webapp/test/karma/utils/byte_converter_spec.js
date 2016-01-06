d96513a556db6cc19341e677ded67459d3d2697b
Nate Bever <nate@formationds.com>
2016-01-06 12:28:03 -0700

    putting the capacity breakdown in the node screen

---
+++ source/java/admin-webapp/test/karma/utils/byte_converter_spec.js
@@ -0,0 +1,17 @@
+describe( 'Testing our byte converter utility', function(){
+    
+    var byte_converter;
+    
+    beforeEach( module( 'base' ) );
+    
+    beforeEach( inject( function( _$byte_converter_ ){
+        byte_converter = _$byte_converter_;
+    }));
+    
+    it ( 'should convert from bytes to a string', function(){
+    });
+    
+    it ( 'should convert from some given unit to a readable string', function(){
+    });
+    
+});
