993185b12f2329c8f89fe072fc4b3fc10c649ed4
Phillip Peralez <phillip@formationds.com>
2016-01-20 08:43:56 -0800

    Adding a timer task that flushes fdslogs of an fds process every 5 seconds. This will prevent logs from being held in a boost log buffer indefinately if there is little activity being logged.

--- source/umod-lib/common/fds_process.cpp
+++ source/umod-lib/common/fds_process.cpp
@@ -16,6 +16,7 @@
 #include <unistd.h>
 #include <syslog.h>
 #include <execinfo.h>
+#include <chrono>
 
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -261,6 +262,11 @@ void FdsProcess::init(int argc, char *argv[],
     /* Set the logger level */
     g_fdslog->setSeverityFilter(
         fds_log::getLevelFromName(conf_helper_.get<std::string>("log_severity","NORMAL")));
+
+    /* Adding a timer task to periodically flush all buffered log data to files
+     * */
+    timer_servicePtr_->scheduledFunctionRepeated(std::chrono::seconds(5),
+                                                 []() { g_fdslog->flush(); });
 
     const libconfig::Setting& fdsSettings = conf_helper_.get_fds_config()->getConfig().getRoot();
     LOGNORMAL << "Configurations as modified by the command line:";
