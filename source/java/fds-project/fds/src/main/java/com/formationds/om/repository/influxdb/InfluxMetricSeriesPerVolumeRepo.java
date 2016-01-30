1fa2e3833f673a74870fd214a06791a01748cdaf
Dave Setzke <dave@formationds.com>
2016-01-29 22:04:19 -0700

    Change series name format to k=v with & separator
    This format will be easier to parse and regexes on series names will be
    easier to look at and understand since it won't require all the escaping

--- source/java/fds-project/fds/src/main/java/com/formationds/om/repository/influxdb/InfluxMetricSeriesPerVolumeRepo.java
+++ source/java/fds-project/fds/src/main/java/com/formationds/om/repository/influxdb/InfluxMetricSeriesPerVolumeRepo.java
@@ -29,17 +29,23 @@ import com.formationds.om.repository.query.QueryCriteria;
  */
 public class InfluxMetricSeriesPerVolumeRepo extends InfluxMetricRepository {
 
+    private static final String TAG_SEP = "&";
+    private static final String DOMAIN_TOKEN = "domain=";
+    private static final String VOLID_TOKEN = "&volid=";
+    private static final String VOLNAME_TOKEN = "&volname=";
+    private static final String METRICS_TOKEN = "&metrics";
+
     /**
      * domain.${volumeDomain}_volname.${volumeName}_volid.${volumeId}_metrics
      */
-    public static final String SERIES_NAME_FMT = "domain.%s_volname.%s_volid.%d_metrics";
+    public static final String SERIES_NAME_FMT = "domain=%s&volname=%s&volid=%d&metrics";
 
-    public static final String SERIES_QUERY_FMT                   = "domain.%s_volname.%s_volid.%d_metrics";
-    public static final String SERIES_QUERY_BY_VOLNAME_DOMAIN_FMT = "/domain.%s_volname.%s_volid.\\.*_metrics/";
-    public static final String SERIES_QUERY_BY_VOLNAME_FMT        = "/domain.\\.*_volname.%s_volid.\\.*_metrics/";
-    public static final String SERIES_QUERY_BY_VOLID_FMT          = "/domain.\\.*_volname.\\.*_volid.%d_metrics/";
-    public static final String SERIES_QUERY_ALL_FOR_VOLDOMAIN_FMT = "/domain.%s_volname.\\.*_volid.\\.*_metrics/";
-    public static final String SERIES_QUERY_ALL_VOL_FMT           = "/domain.\\.*_volname.\\.*_volid.\\.*_metrics/";
+    public static final String SERIES_QUERY_FMT                   = "domain=%s&volname=%s&volid=%d&metrics";
+    public static final String SERIES_QUERY_BY_VOLNAME_DOMAIN_FMT = "/domain=%s&volname=%s&volid=.*&metrics/";
+    public static final String SERIES_QUERY_BY_VOLNAME_FMT        = "/domain=.*&volname=%s&volid=.*&metrics/";
+    public static final String SERIES_QUERY_BY_VOLID_FMT          = "/domain=.*&volname=.*&volid=%d&metrics/";
+    public static final String SERIES_QUERY_ALL_FOR_VOLDOMAIN_FMT = "/domain=%s&volname=.*&volid=.*&metrics/";
+    public static final String SERIES_QUERY_ALL_VOL_FMT           = "/domain=.*&volname=.*&volid=.*&metrics/";
 
     // TODO: tenant id would be useful to have here too!  It is not in IVolumeDatapoint
     // or VolumeDatapoint so either need to add it there or look it up from cache...
@@ -223,20 +229,18 @@ public class InfluxMetricSeriesPerVolumeRepo extends InfluxMetricRepository {
         return sb.toString();
     }
 
-    private static final String DOMAIN_TOKEN = "domain.";
-    private static final String VOLID_TOKEN = "_volid.";
-    private static final String VOLNAME_TOKEN = "_volname.";
-    private static final String METRICS_TOKEN = "_metrics";
-
     /**
      * Extract Volume metadata from the series name.
      *
      * @param s the series
      * @return VolumeInfo containing the domain name, volume name and volume id
      */
-    // "domain.%s_volname.%s_volid.%d_metrics"
+    // "domain=%s&volname=%s&volid=%d&metrics"
     private VolumeInfo extractVolumeMetadata(Serie s) {
 
+        // Note: since format was changed to be key=value[&key2=value2[&...]]
+        // we could change this to use split( '&' ) and then support additional "tags"
+        // in the series name
         String sname = s.getName();
 
         int d = sname.indexOf( DOMAIN_TOKEN );
