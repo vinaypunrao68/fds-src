34c67ecf6c2b6acafbf3af948dd7b653688199e6
paul-tinius <paul@formationds.com>
2016-02-11 09:41:26 -0700

    FS-4877: removed all of the fds_verify on line 1594

--- source/orch-mgr/om-dlt/OmResources.cpp
+++ source/orch-mgr/om-dlt/OmResources.cpp
@@ -1590,8 +1590,6 @@ OM_NodeDomainMod::om_load_state(kvstore::ConfigDB* _configDB)
                      << "persistent state and restart cluster again";
             return err;
         }
-
-        fds_verify( ( sm_services.size() == 0 ) && ( dm_services.size() == 0 ) );
     }
 
     if (( sm_services.size() > 0) || (dm_services.size() > 0)) {
