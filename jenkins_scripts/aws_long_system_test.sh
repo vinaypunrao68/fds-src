7b84813bad61318f1a8e4af96c6a1bea562d5f36
poojashahuraj <pooja@formationds.com>
2016-02-25 07:48:20 -0700

    take inventory name as parameter

--- jenkins_scripts/aws_long_system_test.sh
+++ jenkins_scripts/aws_long_system_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -l
-
+# This script is used for running system tests on AWS nodes for jenkins
+# parameter1: first argument passed is always inventory
+# parameter2: second argument onwards is a list of the tests to run. Length on the this list can be varied for every run
 . jenkins_scripts/jenkins_system_test.lib
 
 error_trap_enabled
@@ -18,9 +20,6 @@ then
     run_coroner 1
 fi
 
-AWS_LST_TEST_SCENARIO_LIST = ${1}
-error_trap_enabled
-
 startup
 clean_up_environment
 configure_cache
@@ -35,6 +34,6 @@ cache_report
 configure_console_access      # Must be complted after the build
 run_python_unit_tests
 run_cpp_unit_tests
-run_aws_test_scenarios AWS_LST_TEST_SCENARIO_LIST
+run_aws_test_scenarios $@
 
 run_node_cleanup 0            # Completed successfully, cleanup and exit with a 0
