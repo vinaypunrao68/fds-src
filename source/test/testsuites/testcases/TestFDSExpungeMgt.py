88d01d8c7a04b86c2c65da38ba1e161ba8c30987
poojashahuraj <pooja@formationds.com>
2016-03-29 16:25:36 -0600

    not ExpungeTest can run against remote/real nodes too

--- source/test/testsuites/testcases/TestFDSExpungeMgt.py
+++ source/test/testsuites/testcases/TestFDSExpungeMgt.py
@@ -2,10 +2,10 @@ from collections import defaultdict
 import TestCase
 from fabric.api import local
 import time
-from fdslib.TestUtils import disconnect_fabric
+from fdslib.TestUtils import execute_command_with_fabric
 
-
-debug_tool_path = '../../Build/linux-x86_64.debug/tools/fds.debug'
+debug_tool_path_local = '../../Build/linux-x86_64.debug/tools/fds.debug'
+debug_tool_path_remote = '/fds/sbin/fds.debug'
 
 
 class TestRunGarbageCollector(TestCase.FDSTestCase):
@@ -16,10 +16,11 @@ class TestRunGarbageCollector(TestCase.FDSTestCase):
                                              "Run garbage collector and wait untill it's done")
 
     def test_RunGC(self):
+        om_ip = self.parameters["fdscfg"].rt_om_node.nd_conf_dict['ip']
         assert self.gc_not_running() is True
-        pre_gc_dms_refscan = get_dm_service_counters()
-        cmd = debug_tool_path + " " + "gc start"
-        op = local(cmd, capture=True)
+        pre_gc_dms_refscan = self.get_dm_service_counters()
+
+        op = run_fds_debug("gc start",self.parameters["ansible_install_done"], om_ip)
 
         self.log.info("Sleep for a minute. Remove this sleep once gc info view is changed and we do get # of previous gc runs")
         # TODO pooja: remove this sleep once we have count of gc runs in gc info
@@ -34,9 +35,8 @@ class TestRunGarbageCollector(TestCase.FDSTestCase):
                 time.sleep(30)
                 continue
             else:
-                assert verify_counters_timestamp(self.log, pre_gc_dms_refscan) is True
+                assert self.verify_counters_timestamp(pre_gc_dms_refscan) is True
                 self.log.info("GC finished. Pre GC dm service counter last run timestapms are greater than post GC")
-                disconnect_fabric()
                 return True
         return False
 
@@ -44,8 +44,8 @@ class TestRunGarbageCollector(TestCase.FDSTestCase):
     # Parses output or fds.debug tool's "gc info" command
     def gc_not_running(self):
         flag = False
-        cmd = debug_tool_path + " " + "gc info"
-        op = local(cmd, capture=True)
+        om_ip = self.parameters["fdscfg"].rt_om_node.nd_conf_dict['ip']
+        op = run_fds_debug("gc info", self.parameters['ansible_install_done'], om_ip)
         dict_a = parse_gc_output(op)
 
         # To confirm gc is not running, all each item in check_keys list should be zero
@@ -53,6 +53,33 @@ class TestRunGarbageCollector(TestCase.FDSTestCase):
         testval = ['0']
         flag = all(dict_a[key] == testval for key in check_keys)
         return flag
+
+    # @param pre_gc_dms_refscan = list of timestamps for pre gc run
+    # If pre_gc_dms_refscan timestamps are greater than respective post_gc_dms_refscan then retrun True else False
+    def verify_counters_timestamp(self, pre_gc_dms_refscan):
+        post_gc_run = self.get_dm_service_counters()
+
+        # When gc runs first time, pre_gc_dms_ref_scan is empty list
+        if len(pre_gc_dms_refscan) != 0:
+            assert len(pre_gc_dms_refscan) == len(post_gc_run)
+            for idx, value in enumerate(pre_gc_dms_refscan):
+                if not int(post_gc_run[idx]) > int(pre_gc_dms_refscan[idx]):
+                    self.log.error("Failed: dm.refscan timestamp {0} not greater than previous run {1}".
+                              format(post_gc_run[idx], pre_gc_dms_refscan[idx]))
+                    self.log.info("dm.refscan timestamps. previous run:{}, Current run {}".
+                             format(pre_gc_dms_refscan, post_gc_run))
+                    return False
+        else:
+            self.log.info("This is First GC run so skip dm.refscan.lastrun.timestamp verification")
+        return True
+
+    # This method parses output of fds.debug tool's `service counters dm` command
+    # returns dictionary as key value pair
+    def get_dm_service_counters(self):
+        om_ip = self.parameters["fdscfg"].rt_om_node.nd_conf_dict['ip']
+        op = run_fds_debug("service counters dm", self.parameters['ansible_install_done'], om_ip)
+        counters_dict = parse_counters_output(op)
+        return counters_dict['dm.refscan.lastrun.timestamp']
 
 
 # This class contains the attributes and methods to test
@@ -77,8 +104,8 @@ class TestVerifySmDeletion(TestCase.FDSTestCase):
         Test Case:
         Check if GC deleted data from SM
         """
-        cmd = debug_tool_path + " " + "gc info"
-        op = local(cmd, capture=True)
+        om_ip = self.parameters["fdscfg"].rt_om_node.nd_conf_dict['ip']
+        op = run_fds_debug("gc info", self.parameters['ansible_install_done'], om_ip)
         key_val_dict = parse_gc_output(op)
         objects_deleted = int(key_val_dict['sm.objects.deleted'][0])
 
@@ -97,37 +124,7 @@ class TestVerifySmDeletion(TestCase.FDSTestCase):
                 return False
 
         self.log.info("OK: GC deleted {} SM objects".format(objects_deleted))
-        disconnect_fabric()
         return True
-
-
-# This method parses output of fds.debug tool's `service counters dm` command
-# returns dictionary as key value pair
-def get_dm_service_counters():
-    cmd = debug_tool_path + " " + "service counters dm"
-    op = local(cmd, capture=True)
-    counters_dict = parse_counters_output(op)
-    return counters_dict['dm.refscan.lastrun.timestamp']
-
-
-# @param pre_gc_dms_refscan = list of timestamps for pre gc run
-# If pre_gc_dms_refscan timestamps are greater than resepctive post_gc_dms_refscan then retrun True else False
-def verify_counters_timestamp(log, pre_gc_dms_refscan):
-    post_gc_run = get_dm_service_counters()
-
-    # When gc runs first time, pre_gc_dms_ref_scan is empty list
-    if len(pre_gc_dms_refscan) != 0:
-        assert len(pre_gc_dms_refscan) == len(post_gc_run)
-        for idx, value in enumerate(pre_gc_dms_refscan):
-            if not int(post_gc_run[idx]) > int(pre_gc_dms_refscan[idx]):
-                log.error("Failed: dm.refscan timestamp {0} not greater than previous run {1}".
-                          format(post_gc_run[idx], pre_gc_dms_refscan[idx]))
-                log.info("dm.refscan timestamps. previous run:{}, Current run {}".
-                         format(pre_gc_dms_refscan, post_gc_run))
-                return False
-    else:
-        log.info("This is First GC run so skip dm.refscan.lastrun.timestamp verification")
-    return True
 
 
 # This method parses output of `gc info` or `gc start`.
@@ -142,7 +139,7 @@ def parse_gc_output(op):
         while '' in a:
             a.remove('')
         if len(a) == 2:
-            op_dict[a[0]].append(a[1])
+            op_dict[a[0]].append(a[1].rstrip())
     return op_dict
 
 
@@ -160,3 +157,17 @@ def parse_counters_output(op):
         if len(a) == 2:
             op_dict[a[1]].append(a[0])
     return op_dict
+
+
+# This method runs given command with tool fds.debug from terminal
+# @param cmd : command to run from terminal
+# @param remote_env: if remote env then run command of remote node else on local machine
+# @param om_ip: fds.debug runs on om node only
+def run_fds_debug(cmd, remote_env, om_ip):
+    if remote_env:
+        cmd = debug_tool_path_remote + " "+cmd
+        op = execute_command_with_fabric(cmd,remote_env=remote_env,node_ip=om_ip)
+    else:
+        cmd = debug_tool_path_local + " "+cmd
+        op = execute_command_with_fabric(cmd,remote_env=remote_env,node_ip=om_ip)
+    return op
